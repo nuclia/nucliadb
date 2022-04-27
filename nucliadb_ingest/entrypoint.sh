6f6443c957e9e15876120d43b331dbdcef5a27da
Fer Pizzabiocche <58727056+ferpizza@users.noreply.github.com>
2022-04-27 16:09:40 +0200

    ingest | determine ingest-replicas form helm values instead of k8s cluster (#68)

--- nucliadb_ingest/entrypoint.sh
+++ nucliadb_ingest/entrypoint.sh
@@ -7,18 +7,4 @@ export SWIM_PEERS_ADDR=$(echo $SWIM_PEERS_ADDR | sed -re "s|\"$HOSTNAME\.([a-zA-
 # parse partition number from hostname
 export REPLICA_NUMBER=$(echo $HOSTNAME | grep -oE '[0-9]+$')
 
-# Get number of replicas from StatefulSet configuration
-## Point to the internal API server hostname
-APISERVER=https://kubernetes.default.svc
-## Path to ServiceAccount token
-SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
-## Read this Pod's namespace
-NAMESPACE=$(cat ${SERVICEACCOUNT}/namespace)
-## Read the ServiceAccount bearer token
-TOKEN=$(cat ${SERVICEACCOUNT}/token)
-## Reference the internal certificate authority (CA)
-CACERT=${SERVICEACCOUNT}/ca.crt
-## Explore the API with TOKEN
-export TOTAL_REPLICAS=$(curl --silent --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/apis/apps/v1/namespaces/${NAMESPACE}/statefulsets/ingest | jq '.status.replicas')
-
 exec "$@"
