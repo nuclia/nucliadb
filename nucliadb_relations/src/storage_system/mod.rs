c692910e7b4de7403fd0b2f599e2d7ab96df50d8
Herme <44120086+hermeGarcia@users.noreply.github.com>
2022-05-27 15:44:19 +0200

    create should create the stamp (#114)
    * create should create the stamp
    
    * license

--- nucliadb_relations/src/storage_system/mod.rs
+++ nucliadb_relations/src/storage_system/mod.rs
@@ -1,3 +1,23 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
 use std::path::Path;
 
 use heed::flags::Flags;
@@ -191,6 +211,7 @@ impl StorageSystem {
             let colaborators = env.create_database(Some(COLABORATORS_DB)).unwrap();
             let edges = env.create_database(Some(EDGE_DB)).unwrap();
             let state = env.create_database(Some(STATE_DB)).unwrap();
+            std::fs::File::create(path.join(STAMP)).unwrap();
             StorageSystem {
                 env,
                 keys,
@@ -436,6 +457,15 @@ mod tests {
         let dir = tempfile::tempdir().unwrap();
         StorageSystem::create(dir.path())
     }
+
+    #[test]
+    fn open_and_create() {
+        let dir = tempfile::tempdir().unwrap();
+        StorageSystem::create(dir.path());
+        StorageSystem::open(dir.path());
+        assert!(true);
+    } 
+
     #[test]
     fn add_resource() {
         let system = initialize_storage_system();
