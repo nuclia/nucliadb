d27dc4ad4b949a5dcba998334419c62e57dafbc0
herme garcia <hermegarcianavarro@gmail.com>
2022-06-10 19:33:49 +0200

    refactor

--- nucliadb_vectors/src/service/writer.rs
+++ nucliadb_vectors/src/service/writer.rs
@@ -57,7 +57,7 @@ impl WriterChild for VectorWriterService {
         self.index
             .write()
             .unwrap()
-            .delete_document(resource_id.shard_id.clone());
+            .delete_document(resource_id.uuid.clone());
         self.index.write().unwrap().commit();
         info!("Delete resource in vector ends");
         Ok(())
@@ -177,7 +177,7 @@ mod tests {
         };
         let resource_id = ResourceId {
             shard_id: "DOC".to_string(),
-            uuid: "".to_string(),
+            uuid: "DOC/KEY".to_string(),
         };
         // insert - delete - insert sequence
         let mut writer = VectorWriterService::start(&vsc).await.unwrap();
