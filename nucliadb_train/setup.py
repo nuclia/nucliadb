120f3397211515c3c70ea1aa50a99d1b55803456
Ramon Navarro Bosch <ramon@nuclia.io>
2022-06-17 11:42:00 +0200

    Initial nucliadb_train package

---
+++ nucliadb_train/setup.py
@@ -0,0 +1,55 @@
+import re
+
+from setuptools import find_packages, setup  # type: ignore
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("VERSION") as f:
+    version = f.read().strip()
+
+
+def load_reqs(filename):
+    with open(filename) as reqs_file:
+        return [
+            line
+            for line in reqs_file.readlines()
+            if not (
+                re.match(r"\s*#", line)  # noqa
+                or re.match("-e", line)
+                or re.match("-r", line)
+            )
+        ]
+
+
+requirements = load_reqs("requirements.txt")
+
+setup(
+    name="nucliadb_train",
+    version=version,
+    author="nucliadb Authors",
+    author_email="nucliadb@nuclia.com",
+    description="NucliaDB Reader Python process",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/nuclia/nucliadb",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: AGPL License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires="~=3.9",
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "extract-openapi = nucliadb_reader.openapi:command_extract_openapi",
+            "reader-metrics = nucliadb_reader.run:run_with_metrics",
+        ]
+    },
+    install_requires=requirements,
+)
