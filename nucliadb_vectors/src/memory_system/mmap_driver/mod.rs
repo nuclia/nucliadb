feeeb34d7ad6cd5592e504e26aa5167305806d66
Herme <44120086+hermeGarcia@users.noreply.github.com>
2022-06-28 22:35:32 +0200

    seeking in mmap_driver (#163)
    * seeking in mmap_driver
    
    * unnecessary cursor operations

--- nucliadb_vectors/src/memory_system/mmap_driver/mod.rs
+++ nucliadb_vectors/src/memory_system/mmap_driver/mod.rs
@@ -21,10 +21,8 @@ use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
-// use std::sync::atomic::{AtomicBool, Ordering};
-// use std::sync::Arc;
 use fs2::FileExt;
-use memmap2::{Mmap, MmapMut};
+use memmap2::Mmap;
 
 use crate::memory_system::elements::{ByteRpr, FileSegment, FixedByteLen};
 
@@ -77,13 +75,9 @@ impl DiskStack {
                 let mut buffer = vec![0u8; FileSegment::segment_len()];
                 stack.read_exact(&mut buffer).unwrap();
                 stack.set_len(new_len).unwrap();
-                stack.rewind().unwrap();
                 Some(FileSegment::from_byte_rpr(&buffer))
             }
-            Err(_) => {
-                stack.rewind().unwrap();
-                None
-            }
+            Err(_) => None,
         }
     }
     pub fn clear(&self) {
@@ -200,18 +194,15 @@ impl Storage {
     }
     fn update_segment(&mut self, segment: FileSegment, bytes: &[u8]) -> FileSegment {
         self.lock.lock_exclusive().unwrap();
-        let range = (segment.start as usize)..(segment.end as usize);
-        let file = OpenOptions::new()
+        let mut file = OpenOptions::new()
             .read(true)
-            .append(true)
+            .write(true)
             .open(&self.path_storage)
             .unwrap();
-        let mut mmap_mut = unsafe { MmapMut::map_mut(&file).unwrap() };
-        if let Some(slice) = mmap_mut.get_mut(range.clone()) {
-            slice.clone_from_slice(bytes);
-            mmap_mut.flush_range(range.start, range.len()).unwrap();
-            self.storage = unsafe { Mmap::map(&file).unwrap() };
-        }
+        file.seek(SeekFrom::Start(segment.start)).unwrap();
+        file.write_all(bytes).unwrap();
+        file.flush().unwrap();
+        self.storage = unsafe { Mmap::map(&file).unwrap() };
         self.lock.unlock().unwrap();
         segment
     }
@@ -219,7 +210,7 @@ impl Storage {
         self.lock.lock_exclusive().unwrap();
         let mut file = OpenOptions::new()
             .read(true)
-            .append(true)
+            .write(true)
             .open(&self.path_storage)
             .unwrap();
         let metadata = file.metadata().unwrap();
@@ -227,6 +218,7 @@ impl Storage {
             start: metadata.len(),
             end: metadata.len() + (bytes.len() as u64),
         };
+        file.seek(SeekFrom::End(0)).unwrap();
         file.write_all(bytes).unwrap();
         file.flush().unwrap();
         self.storage = unsafe { Mmap::map(&file).unwrap() };
