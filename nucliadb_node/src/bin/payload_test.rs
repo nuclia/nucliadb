9693669acd0b6f5214ad4803031772cf3f2ea3c9
Herme <44120086+hermeGarcia@users.noreply.github.com>
2022-07-05 11:57:25 +0200

    payload test binary (#170)
    * payload test binary
    
    * licenses

---
+++ nucliadb_node/src/bin/payload_test.rs
@@ -0,0 +1,53 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use std::io::Cursor;
+
+use nucliadb_node::reader::NodeReaderService;
+use nucliadb_node::writer::NodeWriterService;
+use nucliadb_protos::*;
+use prost::Message;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut writer = NodeWriterService::new();
+    let mut reader = NodeReaderService::new();
+
+    let resources_dir = std::path::Path::new("/path/to/data");
+    let new_shard = writer.new_shard().await;
+    let shard_id = ShardId {
+        id: new_shard.id.clone(),
+    };
+    assert!(resources_dir.exists());
+    for file_path in std::fs::read_dir(&resources_dir).unwrap() {
+        let file_path = file_path.unwrap().path();
+        println!("processing {file_path:?}");
+        let content = std::fs::read(&file_path).unwrap();
+        let resource = Resource::decode(&mut Cursor::new(content)).unwrap();
+        println!("Adding resource {}", file_path.display());
+        let res = writer.set_resource(&shard_id, &resource).await.unwrap();
+        assert!(res.is_ok());
+        println!("Resource added: {}", res.unwrap());
+        let info = reader.get_shard(&shard_id).await.unwrap().get_info().await;
+        println!("Sentences {}", info.sentences);
+        println!("Paragraphs {}", info.paragraphs);
+        println!("resources {}", info.resources);
+    }
+
+    Ok(())
+}
