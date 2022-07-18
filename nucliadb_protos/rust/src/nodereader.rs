47dc6edbc2a813270fb7015fa4f49697aa0a168b
Herme <44120086+hermeGarcia@users.noreply.github.com>
2022-07-18 14:08:40 +0100

    rebase (#198)

--- nucliadb_protos/rust/src/nodereader.rs
+++ nucliadb_protos/rust/src/nodereader.rs
@@ -278,7 +278,7 @@ pub mod node_reader_client {
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
         T::Error: Into<StdError>,
-        T::ResponseBody: Default + Body<Data = Bytes> + Send + 'static,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
         pub fn new(inner: T) -> Self {
@@ -291,6 +291,7 @@ pub mod node_reader_client {
         ) -> NodeReaderClient<InterceptedService<T, F>>
         where
             F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
             T: tonic::codegen::Service<
                 http::Request<tonic::body::BoxBody>,
                 Response = http::Response<
@@ -341,9 +342,9 @@ pub mod node_reader_client {
             &mut self,
             request: impl tonic::IntoRequest<super::super::noderesources::EmptyQuery>,
         ) -> Result<
-                tonic::Response<super::super::noderesources::ShardList>,
-                tonic::Status,
-            > {
+            tonic::Response<super::super::noderesources::ShardList>,
+            tonic::Status,
+        > {
             self.inner
                 .ready()
                 .await
@@ -547,9 +548,9 @@ pub mod node_reader_server {
             &self,
             request: tonic::Request<super::super::noderesources::EmptyQuery>,
         ) -> Result<
-                tonic::Response<super::super::noderesources::ShardList>,
-                tonic::Status,
-            >;
+            tonic::Response<super::super::noderesources::ShardList>,
+            tonic::Status,
+        >;
         async fn document_search(
             &self,
             request: tonic::Request<super::DocumentSearchRequest>,
