0a6ca103bfef205e3c7ef2cb3663da84dd38a7ba
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-07-26 12:49:29 +0200

    Implement and test first simple version of collect_garbage

--- nucliadb_vectors2/src/index/garbage_collector.rs
+++ nucliadb_vectors2/src/index/garbage_collector.rs
@@ -1,21 +1,52 @@
-use super::TransactionLog;
 use crate::disk_structure::{DiskResult, DiskStructure};
-use std::path::Path;
 
-pub trait GCTransactionLog {
-    type Iter: Iterator<Item = (usize, bool)>; // (txn_id, is_up)
-    fn txns(&self) -> Self::Iter;
-    fn get_txn(&self, txn_id: usize) -> bool; // true = txn_id is up, false otherwise
+/// Remove from disk all transactions that are no longer up.
+/// Return an error if any of the disk operations fail
+pub fn collect_garbage<'a, T>(disk: &'a DiskStructure<'a>, txn_log: T) -> DiskResult<()>
+where
+    T: Iterator<Item = &'a (usize, bool)>, // (txn_id, is_up)
+{
+    for (txn_id, is_up) in txn_log {
+        if !is_up {
+            disk.delete_txn(*txn_id)?;
+        }
+    }
+
+    // todo!("Delete also delete logs from database");
+
+    Ok(())
 }
 
-/*
-    Given a DiskStructure and a value of type T (which implements GCTransactionLog) this function will:
-        -> remove from disk all the transactions that are no longer up.
-    This function should return an error if any of the disk operations fail.
-*/
-fn collect_garbage<'a, T, I>(disk: &'a DiskStructure<'a>, txn_log: &T) -> DiskResult<()>
-where
-    T: GCTransactionLog<Iter = I>,
-{
-    todo!()
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+
+    use super::*;
+
+    #[test]
+    fn test_collect_garbage() {
+        // Create some transactions to work with.
+        // Use a mock iterator to mark transactions to delete.
+        // Call `collect_garbage` and test the correct transaction
+        // deletion.
+
+        let dir = tempdir().unwrap();
+        let dir_path = dir.path();
+        let disk = DiskStructure::new(dir_path).unwrap();
+
+        disk.create_txn(0).unwrap();
+        disk.create_txn(1).unwrap();
+        disk.create_txn(2).unwrap();
+        disk.create_txn(3).unwrap();
+
+        let fake_txns = vec![(0, true), (1, false), (2, false), (3, true)];
+
+        let result = collect_garbage(&disk, fake_txns.iter());
+        assert!(result.is_ok());
+
+        assert!(disk.txn_exists(0));
+        assert!(!disk.txn_exists(1));
+        assert!(!disk.txn_exists(2));
+        assert!(disk.txn_exists(3));
+    }
 }
