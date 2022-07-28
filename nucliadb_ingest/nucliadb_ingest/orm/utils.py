dcd57288cfcdf7ff2c9fb3d54db8d0259bb35089
Ramon Navarro Bosch <ramon@nuclia.io>
2022-07-28 21:08:33 +0200

    Fix

--- nucliadb_ingest/nucliadb_ingest/orm/utils.py
+++ nucliadb_ingest/nucliadb_ingest/orm/utils.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Optional
 import urllib.parse
+from typing import Optional
 
 from nucliadb_protos.resources_pb2 import Basic, ExtractedTextWrapper, FieldType
 from nucliadb_protos.writer_pb2 import BrokerMessage
