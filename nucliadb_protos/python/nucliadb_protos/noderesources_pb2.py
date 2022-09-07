1af4568bd8e4ef6e2bdb3518b124357280e0b6a0
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-09-07 10:35:27 +0200

    Update protos

--- nucliadb_protos/python/nucliadb_protos/noderesources_pb2.py
+++ nucliadb_protos/python/nucliadb_protos/noderesources_pb2.py
@@ -17,7 +17,7 @@ from nucliadb_protos import utils_pb2 as nucliadb__protos_dot_utils__pb2
 
 from nucliadb_protos.utils_pb2 import *
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#nucliadb_protos/noderesources.proto\x12\rnoderesources\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1bnucliadb_protos/utils.proto\"/\n\x0fTextInformation\x12\x0c\n\x04text\x18\x01 \x01(\t\x12\x0e\n\x06labels\x18\x02 \x03(\t\"j\n\rIndexMetadata\x12,\n\x08modified\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12+\n\x07\x63reated\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"\x15\n\x07ShardId\x12\n\n\x02id\x18\x01 \x01(\t\"/\n\x08ShardIds\x12#\n\x03ids\x18\x01 \x03(\x0b\x32\x16.noderesources.ShardId\"\xc2\x03\n\x0cShardCreated\x12\n\n\x02id\x18\x01 \x01(\t\x12\x45\n\x10\x64ocument_service\x18\x02 \x01(\x0e\x32+.noderesources.ShardCreated.DocumentService\x12G\n\x11paragraph_service\x18\x03 \x01(\x0e\x32,.noderesources.ShardCreated.ParagraphService\x12\x41\n\x0evector_service\x18\x04 \x01(\x0e\x32).noderesources.ShardCreated.VectorService\x12\x45\n\x10relation_service\x18\x05 \x01(\x0e\x32+.noderesources.ShardCreated.RelationService\"\"\n\x0f\x44ocumentService\x12\x0f\n\x0b\x44OCUMENT_V0\x10\x00\"$\n\x10ParagraphService\x12\x10\n\x0cPARAGRAPH_V0\x10\x00\"\x1e\n\rVectorService\x12\r\n\tVECTOR_V0\x10\x00\"\"\n\x0fRelationService\x12\x0f\n\x0bRELATION_V0\x10\x00\",\n\nResourceID\x12\x10\n\x08shard_id\x18\x01 \x01(\t\x12\x0c\n\x04uuid\x18\x02 \x01(\t\"S\n\x05Shard\x12\x10\n\x08shard_id\x18\x01 \x01(\t\x12\x11\n\tresources\x18\x02 \x01(\x04\x12\x12\n\nparagraphs\x18\x03 \x01(\x04\x12\x11\n\tsentences\x18\x04 \x01(\x04\"1\n\tShardList\x12$\n\x06shards\x18\x01 \x03(\x0b\x32\x14.noderesources.Shard\"\x0f\n\rEmptyResponse\"\x0c\n\nEmptyQuery\" \n\x0eVectorSentence\x12\x0e\n\x06vector\x18\x01 \x03(\x02\"\xfb\x01\n\x0eIndexParagraph\x12\r\n\x05start\x18\x01 \x01(\x05\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x05\x12\x0e\n\x06labels\x18\x03 \x03(\t\x12?\n\tsentences\x18\x04 \x03(\x0b\x32,.noderesources.IndexParagraph.SentencesEntry\x12\r\n\x05\x66ield\x18\x05 \x01(\t\x12\r\n\x05split\x18\x06 \x01(\t\x12\r\n\x05index\x18\x07 \x01(\x04\x1aO\n\x0eSentencesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12,\n\x05value\x18\x02 \x01(\x0b\x32\x1d.noderesources.VectorSentence:\x02\x38\x01\"\xa7\x01\n\x0fIndexParagraphs\x12\x42\n\nparagraphs\x18\x01 \x03(\x0b\x32..noderesources.IndexParagraphs.ParagraphsEntry\x1aP\n\x0fParagraphsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12,\n\x05value\x18\x02 \x01(\x0b\x32\x1d.noderesources.IndexParagraph:\x02\x38\x01\"\xaf\x05\n\x08Resource\x12+\n\x08resource\x18\x01 \x01(\x0b\x32\x19.noderesources.ResourceID\x12.\n\x08metadata\x18\x02 \x01(\x0b\x32\x1c.noderesources.IndexMetadata\x12\x31\n\x05texts\x18\x03 \x03(\x0b\x32\".noderesources.Resource.TextsEntry\x12\x0e\n\x06labels\x18\x04 \x03(\t\x12\x36\n\x06status\x18\x05 \x01(\x0e\x32&.noderesources.Resource.ResourceStatus\x12;\n\nparagraphs\x18\x06 \x03(\x0b\x32\'.noderesources.Resource.ParagraphsEntry\x12\x1c\n\x14paragraphs_to_delete\x18\x07 \x03(\t\x12\x1b\n\x13sentences_to_delete\x18\x08 \x03(\t\x12\"\n\trelations\x18\t \x03(\x0b\x32\x0f.utils.Relation\x12,\n\x13relations_to_delete\x18\n \x03(\x0b\x32\x0f.utils.Relation\x12\x10\n\x08shard_id\x18\x0b \x01(\t\x1aL\n\nTextsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12-\n\x05value\x18\x02 \x01(\x0b\x32\x1e.noderesources.TextInformation:\x02\x38\x01\x1aQ\n\x0fParagraphsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12-\n\x05value\x18\x02 \x01(\x0b\x32\x1e.noderesources.IndexParagraphs:\x02\x38\x01\"N\n\x0eResourceStatus\x12\r\n\tPROCESSED\x10\x00\x12\t\n\x05\x45MPTY\x10\x01\x12\t\n\x05\x45RROR\x10\x02\x12\n\n\x06\x44\x45LETE\x10\x03\x12\x0b\n\x07PENDING\x10\x04P\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#nucliadb_protos/noderesources.proto\x12\rnoderesources\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1bnucliadb_protos/utils.proto\"/\n\x0fTextInformation\x12\x0c\n\x04text\x18\x01 \x01(\t\x12\x0e\n\x06labels\x18\x02 \x03(\t\"j\n\rIndexMetadata\x12,\n\x08modified\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12+\n\x07\x63reated\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"\x15\n\x07ShardId\x12\n\n\x02id\x18\x01 \x01(\t\"/\n\x08ShardIds\x12#\n\x03ids\x18\x01 \x03(\x0b\x32\x16.noderesources.ShardId\"\x85\x04\n\x0cShardCreated\x12\n\n\x02id\x18\x01 \x01(\t\x12\x45\n\x10\x64ocument_service\x18\x02 \x01(\x0e\x32+.noderesources.ShardCreated.DocumentService\x12G\n\x11paragraph_service\x18\x03 \x01(\x0e\x32,.noderesources.ShardCreated.ParagraphService\x12\x41\n\x0evector_service\x18\x04 \x01(\x0e\x32).noderesources.ShardCreated.VectorService\x12\x45\n\x10relation_service\x18\x05 \x01(\x0e\x32+.noderesources.ShardCreated.RelationService\"3\n\x0f\x44ocumentService\x12\x0f\n\x0b\x44OCUMENT_V0\x10\x00\x12\x0f\n\x0b\x44OCUMENT_V1\x10\x01\"6\n\x10ParagraphService\x12\x10\n\x0cPARAGRAPH_V0\x10\x00\x12\x10\n\x0cPARAGRAPH_V1\x10\x01\"-\n\rVectorService\x12\r\n\tVECTOR_V0\x10\x00\x12\r\n\tVECTOR_V1\x10\x01\"3\n\x0fRelationService\x12\x0f\n\x0bRELATION_V0\x10\x00\x12\x0f\n\x0bRELATION_V1\x10\x01\",\n\nResourceID\x12\x10\n\x08shard_id\x18\x01 \x01(\t\x12\x0c\n\x04uuid\x18\x02 \x01(\t\"S\n\x05Shard\x12\x10\n\x08shard_id\x18\x01 \x01(\t\x12\x11\n\tresources\x18\x02 \x01(\x04\x12\x12\n\nparagraphs\x18\x03 \x01(\x04\x12\x11\n\tsentences\x18\x04 \x01(\x04\"1\n\tShardList\x12$\n\x06shards\x18\x01 \x03(\x0b\x32\x14.noderesources.Shard\"\x0f\n\rEmptyResponse\"\x0c\n\nEmptyQuery\" \n\x0eVectorSentence\x12\x0e\n\x06vector\x18\x01 \x03(\x02\"\xfb\x01\n\x0eIndexParagraph\x12\r\n\x05start\x18\x01 \x01(\x05\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x05\x12\x0e\n\x06labels\x18\x03 \x03(\t\x12?\n\tsentences\x18\x04 \x03(\x0b\x32,.noderesources.IndexParagraph.SentencesEntry\x12\r\n\x05\x66ield\x18\x05 \x01(\t\x12\r\n\x05split\x18\x06 \x01(\t\x12\r\n\x05index\x18\x07 \x01(\x04\x1aO\n\x0eSentencesEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12,\n\x05value\x18\x02 \x01(\x0b\x32\x1d.noderesources.VectorSentence:\x02\x38\x01\"\xa7\x01\n\x0fIndexParagraphs\x12\x42\n\nparagraphs\x18\x01 \x03(\x0b\x32..noderesources.IndexParagraphs.ParagraphsEntry\x1aP\n\x0fParagraphsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12,\n\x05value\x18\x02 \x01(\x0b\x32\x1d.noderesources.IndexParagraph:\x02\x38\x01\"\xaf\x05\n\x08Resource\x12+\n\x08resource\x18\x01 \x01(\x0b\x32\x19.noderesources.ResourceID\x12.\n\x08metadata\x18\x02 \x01(\x0b\x32\x1c.noderesources.IndexMetadata\x12\x31\n\x05texts\x18\x03 \x03(\x0b\x32\".noderesources.Resource.TextsEntry\x12\x0e\n\x06labels\x18\x04 \x03(\t\x12\x36\n\x06status\x18\x05 \x01(\x0e\x32&.noderesources.Resource.ResourceStatus\x12;\n\nparagraphs\x18\x06 \x03(\x0b\x32\'.noderesources.Resource.ParagraphsEntry\x12\x1c\n\x14paragraphs_to_delete\x18\x07 \x03(\t\x12\x1b\n\x13sentences_to_delete\x18\x08 \x03(\t\x12\"\n\trelations\x18\t \x03(\x0b\x32\x0f.utils.Relation\x12,\n\x13relations_to_delete\x18\n \x03(\x0b\x32\x0f.utils.Relation\x12\x10\n\x08shard_id\x18\x0b \x01(\t\x1aL\n\nTextsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12-\n\x05value\x18\x02 \x01(\x0b\x32\x1e.noderesources.TextInformation:\x02\x38\x01\x1aQ\n\x0fParagraphsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12-\n\x05value\x18\x02 \x01(\x0b\x32\x1e.noderesources.IndexParagraphs:\x02\x38\x01\"N\n\x0eResourceStatus\x12\r\n\tPROCESSED\x10\x00\x12\t\n\x05\x45MPTY\x10\x01\x12\t\n\x05\x45RROR\x10\x02\x12\n\n\x06\x44\x45LETE\x10\x03\x12\x0b\n\x07PENDING\x10\x04P\x01\x62\x06proto3')
 
 
 
@@ -194,41 +194,41 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _SHARDIDS._serialized_start=296
   _SHARDIDS._serialized_end=343
   _SHARDCREATED._serialized_start=346
-  _SHARDCREATED._serialized_end=796
+  _SHARDCREATED._serialized_end=863
   _SHARDCREATED_DOCUMENTSERVICE._serialized_start=656
-  _SHARDCREATED_DOCUMENTSERVICE._serialized_end=690
-  _SHARDCREATED_PARAGRAPHSERVICE._serialized_start=692
-  _SHARDCREATED_PARAGRAPHSERVICE._serialized_end=728
-  _SHARDCREATED_VECTORSERVICE._serialized_start=730
-  _SHARDCREATED_VECTORSERVICE._serialized_end=760
-  _SHARDCREATED_RELATIONSERVICE._serialized_start=762
-  _SHARDCREATED_RELATIONSERVICE._serialized_end=796
-  _RESOURCEID._serialized_start=798
-  _RESOURCEID._serialized_end=842
-  _SHARD._serialized_start=844
-  _SHARD._serialized_end=927
-  _SHARDLIST._serialized_start=929
-  _SHARDLIST._serialized_end=978
-  _EMPTYRESPONSE._serialized_start=980
-  _EMPTYRESPONSE._serialized_end=995
-  _EMPTYQUERY._serialized_start=997
-  _EMPTYQUERY._serialized_end=1009
-  _VECTORSENTENCE._serialized_start=1011
-  _VECTORSENTENCE._serialized_end=1043
-  _INDEXPARAGRAPH._serialized_start=1046
-  _INDEXPARAGRAPH._serialized_end=1297
-  _INDEXPARAGRAPH_SENTENCESENTRY._serialized_start=1218
-  _INDEXPARAGRAPH_SENTENCESENTRY._serialized_end=1297
-  _INDEXPARAGRAPHS._serialized_start=1300
-  _INDEXPARAGRAPHS._serialized_end=1467
-  _INDEXPARAGRAPHS_PARAGRAPHSENTRY._serialized_start=1387
-  _INDEXPARAGRAPHS_PARAGRAPHSENTRY._serialized_end=1467
-  _RESOURCE._serialized_start=1470
-  _RESOURCE._serialized_end=2157
-  _RESOURCE_TEXTSENTRY._serialized_start=1918
-  _RESOURCE_TEXTSENTRY._serialized_end=1994
-  _RESOURCE_PARAGRAPHSENTRY._serialized_start=1996
-  _RESOURCE_PARAGRAPHSENTRY._serialized_end=2077
-  _RESOURCE_RESOURCESTATUS._serialized_start=2079
-  _RESOURCE_RESOURCESTATUS._serialized_end=2157
+  _SHARDCREATED_DOCUMENTSERVICE._serialized_end=707
+  _SHARDCREATED_PARAGRAPHSERVICE._serialized_start=709
+  _SHARDCREATED_PARAGRAPHSERVICE._serialized_end=763
+  _SHARDCREATED_VECTORSERVICE._serialized_start=765
+  _SHARDCREATED_VECTORSERVICE._serialized_end=810
+  _SHARDCREATED_RELATIONSERVICE._serialized_start=812
+  _SHARDCREATED_RELATIONSERVICE._serialized_end=863
+  _RESOURCEID._serialized_start=865
+  _RESOURCEID._serialized_end=909
+  _SHARD._serialized_start=911
+  _SHARD._serialized_end=994
+  _SHARDLIST._serialized_start=996
+  _SHARDLIST._serialized_end=1045
+  _EMPTYRESPONSE._serialized_start=1047
+  _EMPTYRESPONSE._serialized_end=1062
+  _EMPTYQUERY._serialized_start=1064
+  _EMPTYQUERY._serialized_end=1076
+  _VECTORSENTENCE._serialized_start=1078
+  _VECTORSENTENCE._serialized_end=1110
+  _INDEXPARAGRAPH._serialized_start=1113
+  _INDEXPARAGRAPH._serialized_end=1364
+  _INDEXPARAGRAPH_SENTENCESENTRY._serialized_start=1285
+  _INDEXPARAGRAPH_SENTENCESENTRY._serialized_end=1364
+  _INDEXPARAGRAPHS._serialized_start=1367
+  _INDEXPARAGRAPHS._serialized_end=1534
+  _INDEXPARAGRAPHS_PARAGRAPHSENTRY._serialized_start=1454
+  _INDEXPARAGRAPHS_PARAGRAPHSENTRY._serialized_end=1534
+  _RESOURCE._serialized_start=1537
+  _RESOURCE._serialized_end=2224
+  _RESOURCE_TEXTSENTRY._serialized_start=1985
+  _RESOURCE_TEXTSENTRY._serialized_end=2061
+  _RESOURCE_PARAGRAPHSENTRY._serialized_start=2063
+  _RESOURCE_PARAGRAPHSENTRY._serialized_end=2144
+  _RESOURCE_RESOURCESTATUS._serialized_start=2146
+  _RESOURCE_RESOURCESTATUS._serialized_end=2224
 # @@protoc_insertion_point(module_scope)
