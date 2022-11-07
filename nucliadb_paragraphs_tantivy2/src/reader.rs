16bdab1770bf7d089748eb9d32743d2948fb98a5
Alexis Le Provost <alexis.leprovost@outlook.com>
2022-11-07 16:19:41 +0100

    Simplify expression

--- nucliadb_paragraphs_tantivy2/src/reader.rs
+++ nucliadb_paragraphs_tantivy2/src/reader.rs
@@ -66,7 +66,7 @@ impl ParagraphReader for ParagraphReaderService {
         let topdocs = TopDocs::with_limit(no_results);
         let mut results = searcher.search(&original, &topdocs).unwrap();
         if results.is_empty() {
-            let topdocs = TopDocs::with_limit(no_results - results.len());
+            let topdocs = TopDocs::with_limit(no_results);
             match searcher.search(&fuzzied, &topdocs) {
                 Ok(mut fuzzied) => results.append(&mut fuzzied),
                 Err(err) => error!("{err:?} during suggest"),
@@ -127,7 +127,6 @@ impl ReaderChild for ParagraphReaderService {
         };
         let mut response = searcher.do_search(termc.clone(), original, self);
         if response.results.is_empty() {
-            searcher.results -= response.results.len();
             let fuzzied = searcher.do_search(termc, fuzzied, self);
             let filter = response
                 .results
