c91196dc691b7d69e7ef0589ae4dd5b1ec92a37d
Joan Antoni RE <joanantoni.re16@gmail.com>
2022-11-11 18:31:14 +0100

    Import protobuffers without aliases in ingest test

--- nucliadb/nucliadb/ingest/tests/servicer/test_clean_and_upgrade_index_servicer.py
+++ nucliadb/nucliadb/ingest/tests/servicer/test_clean_and_upgrade_index_servicer.py
@@ -22,80 +22,80 @@ from uuid import uuid4
 
 import pytest
 from nucliadb_protos.noderesources_pb2 import ShardCleaned
-from nucliadb_protos.writer_pb2 import ShardCreated as ReplicaData
-from nucliadb_protos.writer_pb2 import ShardObject as PBShard
-from nucliadb_protos.writer_pb2 import ShardReplica as PBReplica
-from nucliadb_protos.writer_pb2 import Shards as PBShards
-
+from nucliadb_protos.writer_pb2 import ShardCreated
+from nucliadb_protos.writer_pb2 import ShardObject
+from nucliadb_protos.writer_pb2 import ShardReplica
+from nucliadb_protos.writer_pb2 import Shards
+from nucliadb.ingest.tests.fixtures import IngestFixture
 from nucliadb.ingest.service.writer import update_shards_with_updated_replica
 from nucliadb_protos import knowledgebox_pb2, writer_pb2_grpc
 
 
 @pytest.mark.asyncio
-async def test_clean_and_upgrade_kb_index(grpc_servicer):
+async def test_clean_and_upgrade_kb_index(grpc_servicer: IngestFixture):
     stub = writer_pb2_grpc.WriterStub(grpc_servicer.channel)
 
     kb_id = str(uuid4())
     pb = knowledgebox_pb2.KnowledgeBoxNew(slug="test", forceuuid=kb_id)
     pb.config.title = "My Title"
-    result = await stub.NewKnowledgeBox(pb)
+    result = await stub.NewKnowledgeBox(pb)  # type: ignore
     assert result.status == knowledgebox_pb2.KnowledgeBoxResponseStatus.OK
 
     req = knowledgebox_pb2.KnowledgeBoxID(uuid=kb_id)
-    result = await stub.CleanAndUpgradeKnowledgeBoxIndex(req)
+    result = await stub.CleanAndUpgradeKnowledgeBoxIndex(req)  # type: ignore
 
 
 def test_update_shards_pb_replica():
-    shard1_rep1 = PBReplica(
+    shard1_rep1 = ShardReplica(
         node="node1",
-        shard=ReplicaData(
+        shard=ShardCreated(
             id="shard1rep1",
-            document_service=ReplicaData.DOCUMENT_V0,
-            paragraph_service=ReplicaData.PARAGRAPH_V0,
-            relation_service=ReplicaData.RELATION_V0,
-            vector_service=ReplicaData.VECTOR_V0,
+            document_service=ShardCreated.DOCUMENT_V0,
+            paragraph_service=ShardCreated.PARAGRAPH_V0,
+            relation_service=ShardCreated.RELATION_V0,
+            vector_service=ShardCreated.VECTOR_V0,
         ),
     )
-    shard1_rep2 = PBReplica(
+    shard1_rep2 = ShardReplica(
         node="node2",
-        shard=ReplicaData(
+        shard=ShardCreated(
             id="shard1rep2",
-            document_service=ReplicaData.DOCUMENT_V0,
-            paragraph_service=ReplicaData.PARAGRAPH_V0,
-            relation_service=ReplicaData.RELATION_V0,
-            vector_service=ReplicaData.VECTOR_V0,
+            document_service=ShardCreated.DOCUMENT_V0,
+            paragraph_service=ShardCreated.PARAGRAPH_V0,
+            relation_service=ShardCreated.RELATION_V0,
+            vector_service=ShardCreated.VECTOR_V0,
         ),
     )
-    shard2_rep1 = PBReplica(
+    shard2_rep1 = ShardReplica(
         node="node1",
-        shard=ReplicaData(
+        shard=ShardCreated(
             id="shard2rep1",
-            document_service=ReplicaData.DOCUMENT_V0,
-            paragraph_service=ReplicaData.PARAGRAPH_V0,
-            relation_service=ReplicaData.RELATION_V0,
-            vector_service=ReplicaData.VECTOR_V0,
+            document_service=ShardCreated.DOCUMENT_V0,
+            paragraph_service=ShardCreated.PARAGRAPH_V0,
+            relation_service=ShardCreated.RELATION_V0,
+            vector_service=ShardCreated.VECTOR_V0,
         ),
     )
-    shard2_rep2 = PBReplica(
+    shard2_rep2 = ShardReplica(
         node="node2",
-        shard=ReplicaData(
+        shard=ShardCreated(
             id="shard2rep2",
-            document_service=ReplicaData.DOCUMENT_V0,
-            paragraph_service=ReplicaData.PARAGRAPH_V0,
-            relation_service=ReplicaData.RELATION_V0,
-            vector_service=ReplicaData.VECTOR_V0,
+            document_service=ShardCreated.DOCUMENT_V0,
+            paragraph_service=ShardCreated.PARAGRAPH_V0,
+            relation_service=ShardCreated.RELATION_V0,
+            vector_service=ShardCreated.VECTOR_V0,
         ),
     )
-    shard1 = PBShard(shard="shard1", replicas=[shard1_rep1, shard1_rep2])
-    shard2 = PBShard(shard="shard2", replicas=[shard2_rep1, shard2_rep2])
+    shard1 = ShardObject(shard="shard1", replicas=[shard1_rep1, shard1_rep2])
+    shard2 = ShardObject(shard="shard2", replicas=[shard2_rep1, shard2_rep2])
 
-    shards = PBShards(shards=[shard1, shard2])
+    shards = Shards(shards=[shard1, shard2])
 
     new_replica_info = ShardCleaned(
-        document_service=ReplicaData.DOCUMENT_V1,
-        relation_service=ReplicaData.RELATION_V1,
-        vector_service=ReplicaData.VECTOR_V1,
-        paragraph_service=ReplicaData.PARAGRAPH_V1,
+        document_service=ShardCreated.DOCUMENT_V1,
+        relation_service=ShardCreated.RELATION_V1,
+        vector_service=ShardCreated.VECTOR_V1,
+        paragraph_service=ShardCreated.PARAGRAPH_V1,
     )
 
     update_shards_with_updated_replica(shards, "shard1rep1", new_replica_info)
@@ -104,14 +104,14 @@ def test_update_shards_pb_replica():
     for shard in shards.shards:
         for replica in shard.replicas:
             if replica.shard.id == "shard1rep1":
-                assert replica.shard.document_service == ReplicaData.DOCUMENT_V1
-                assert replica.shard.paragraph_service == ReplicaData.PARAGRAPH_V1
-                assert replica.shard.vector_service == ReplicaData.VECTOR_V1
-                assert replica.shard.relation_service == ReplicaData.RELATION_V1
+                assert replica.shard.document_service == ShardCreated.DOCUMENT_V1
+                assert replica.shard.paragraph_service == ShardCreated.PARAGRAPH_V1
+                assert replica.shard.vector_service == ShardCreated.VECTOR_V1
+                assert replica.shard.relation_service == ShardCreated.RELATION_V1
                 found = True
             else:
-                assert replica.shard.document_service == ReplicaData.DOCUMENT_V0
-                assert replica.shard.paragraph_service == ReplicaData.PARAGRAPH_V0
-                assert replica.shard.vector_service == ReplicaData.VECTOR_V0
-                assert replica.shard.relation_service == ReplicaData.RELATION_V0
+                assert replica.shard.document_service == ShardCreated.DOCUMENT_V0
+                assert replica.shard.paragraph_service == ShardCreated.PARAGRAPH_V0
+                assert replica.shard.vector_service == ShardCreated.VECTOR_V0
+                assert replica.shard.relation_service == ShardCreated.RELATION_V0
     assert found
