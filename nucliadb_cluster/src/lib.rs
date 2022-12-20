2b80cccb808fa54e8cc39198bbdd0f734ae85842
Alexis Le Provost <alexis.leprovost@outlook.com>
2022-12-20 16:03:17 +0100

    Remove cluster.rs

--- nucliadb_cluster/src/lib.rs
+++ nucliadb_cluster/src/lib.rs
@@ -1,4 +1,7 @@
-extern crate log;
-
-pub mod cluster;
 pub mod error;
+pub mod node;
+pub mod key;
+
+pub use error::Error;
+pub use node::{Node, NodeType, NodeSnapshot, NodeHandle};
+pub use key::Key;
