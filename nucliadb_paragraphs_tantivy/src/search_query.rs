0460079c191d6c3e6d7d73b62938fc6cb1f7178a
herme garcia <hermegarcianavarro@gmail.com>
2022-12-22 14:16:13 +0000

    or in streaming filters

--- nucliadb_paragraphs_tantivy/src/search_query.rs
+++ nucliadb_paragraphs_tantivy/src/search_query.rs
@@ -461,7 +461,7 @@ pub fn streaming_query(schema: &ParagraphSchema, request: &StreamRequest) -> Box
         .for_each(|facet| {
             let facet_term = Term::from_facet(schema.facets, &facet);
             let facet_term_query = TermQuery::new(facet_term, IndexRecordOption::Basic);
-            queries.push((Occur::Must, Box::new(facet_term_query)));
+            queries.push((Occur::Should, Box::new(facet_term_query)));
         });
     Box::new(BooleanQuery::new(queries))
 }
