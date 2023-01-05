8581045a26b390790b6812aa89fbd7f35f33d10d
Ferran Llamas <llamas.arroniz@gmail.com>
2023-01-05 08:25:14 +0100

    Set icon as content type if provided by the user

--- nucliadb/nucliadb/writer/resource/field.py
+++ nucliadb/nucliadb/writer/resource/field.py
@@ -263,8 +263,9 @@ def parse_external_file_field(
     writer.files[key].file.uri = uri  # type: ignore
     writer.files[key].file.source = resources_pb2.CloudFile.Source.EXTERNAL
     writer.files[key].file.content_type = file_field.file.content_type
-    # if file_field.file.content_type and writer.basic.icon == GENERIC_MIME_TYPE:
-    #     writer.basic.icon = file_field.file.content_type
+    if file_field.file.content_type and writer.basic.icon == GENERIC_MIME_TYPE:
+        writer.basic.icon = file_field.file.content_type
+
     processing = get_processing()
     toprocess.filefield[key] = processing.convert_external_filefield_to_str(file_field)
 
