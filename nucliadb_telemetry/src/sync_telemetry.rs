5be5970e14c28ed00f50c04f555df786b2ad8114
herme garcia <hermegarcianavarro@gmail.com>
2023-01-23 14:32:21 +0100

    licenses

--- nucliadb_telemetry/src/sync_telemetry.rs
+++ nucliadb_telemetry/src/sync_telemetry.rs
@@ -1,3 +1,22 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::Mutex;
 
@@ -29,11 +48,14 @@ impl SyncTelemetryLoop {
 
 lazy_static::lazy_static! {
     static ref SYNC_TELEMETRY: Option<Mutex<Sender<TelemetryEvent>>> = {
-        if !is_telemetry_enabled() { return None; }
-        let (stxn, rtxn) = channel();
-        let Some(telemtry_loop) = SyncTelemetryLoop::new(rtxn) else { return None };
-        std::thread::spawn(move || telemtry_loop.run());
-        Some(Mutex::new(stxn))
+        if is_telemetry_enabled() {
+            let (stxn, rtxn) = channel();
+            let Some(telemtry_loop) = SyncTelemetryLoop::new(rtxn) else { return None };
+            std::thread::spawn(move || telemtry_loop.run());
+            Some(Mutex::new(stxn))
+        } else {
+            None
+        }
     };
 }
 
