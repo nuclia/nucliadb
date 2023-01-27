dbc59cf7baed93a0a94defa91b264812a5dff90a
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-01-27 19:19:10 +0100

    Improve sorted search tests

---
+++ nucliadb/nucliadb/tests/knowledgeboxes/twenty_dummy_resources.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.fixture(scope="function")
+async def twenty_dummy_resources_kb(
+    nucliadb_manager: AsyncClient,
+    nucliadb_reader: AsyncClient,
+    nucliadb_writer: AsyncClient,
+):
+    payloads = [
+        {
+            "slug": f"dummy-resource-{i}",
+            "title": f"Dummy resource {i}",
+            "summary": f"Dummy resource {i} summary",
+        }
+        for i in range(20)
+    ]
+
+    resp = await nucliadb_manager.post("/kbs", json={"slug": "twenty-dummy-resources"})
+    assert resp.status_code == 201
+    kbid = resp.json().get("uuid")
+
+    for payload in payloads:
+        resp = await nucliadb_writer.post(
+            f"/kb/{kbid}/resources",
+            headers={"X-Synchronous": "true"},
+            json=payload,
+        )
+        assert resp.status_code == 201
+
+    resp = await nucliadb_reader.get(
+        f"/kb/{kbid}/resources",
+    )
+    assert resp.status_code == 200
+    body = resp.json()
+    assert len(body["resources"]) == 20
+
+    yield kbid
+
+    resp = await nucliadb_manager.delete(f"/kb/{kbid}")
+    assert resp.status_code == 200
