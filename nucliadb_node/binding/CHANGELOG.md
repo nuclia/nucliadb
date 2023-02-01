3d05382ed436deeacd6c0f9ed47fc46b6d35156b
Herme <44120086+hermeGarcia@users.noreply.github.com>
2023-02-01 14:25:34 +0100

    fix versions.json on reader (#551)
    * fix versions.json on reader
    
    * no hardcoded config

--- nucliadb_node/binding/CHANGELOG.md
+++ nucliadb_node/binding/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+
+## 0.5.4
+
+- versions.json fix
+
 ## 0.5.3
 
 - Node refactor
--- nucliadb_node/src/services/versions.rs
+++ nucliadb_node/src/services/versions.rs
@@ -27,6 +27,7 @@ const VECTORS_VERSION: u32 = 1;
 const PARAGRAPHS_VERSION: u32 = 1;
 const RELATIONS_VERSION: u32 = 1;
 const TEXTS_VERSION: u32 = 1;
+const DEPRECATED_CONFIG: &str = "config.json";
 
 #[derive(Serialize, Deserialize)]
 pub struct Versions {
@@ -41,6 +42,13 @@ pub struct Versions {
 }
 
 impl Versions {
+    fn deprecated_versions_exists(versions_file: &Path) -> bool {
+        versions_file
+            .parent()
+            .map(|v| v.join(DEPRECATED_CONFIG))
+            .map(|v| v.exists())
+            .unwrap_or_default()
+    }
     fn new_from_deprecated() -> Versions {
         Versions {
             version_paragraphs: Some(1),
@@ -172,13 +180,21 @@ impl Versions {
     }
 
     pub fn load(versions_file: &Path) -> NodeResult<Versions> {
-        let versions_json = std::fs::read_to_string(versions_file)?;
-        let mut versions: Versions = serde_json::from_str(&versions_json)?;
-        versions.fill_gaps();
-        Ok(versions)
+        if versions_file.exists() {
+            let versions_json = std::fs::read_to_string(versions_file)?;
+            let mut versions: Versions = serde_json::from_str(&versions_json)?;
+            versions.fill_gaps();
+            Ok(versions)
+        } else if Versions::deprecated_versions_exists(versions_file) {
+            // In this case is an old index, therefore we create the versions file
+            // with the index versions that where available that moment.
+            // The writer will create the file at some point
+            Ok(Versions::new_from_deprecated())
+        } else {
+            Err(node_error!("Versions not found"))
+        }
     }
     pub fn load_or_create(versions_file: &Path) -> NodeResult<Versions> {
-        let deprecated_config = versions_file.parent().map(|v| v.join("config.json"));
         if versions_file.exists() {
             let versions_json = std::fs::read_to_string(versions_file)?;
             let mut versions: Versions = serde_json::from_str(&versions_json)?;
@@ -187,7 +203,7 @@ impl Versions {
                 std::fs::write(versions_file, serialized)?;
             }
             Ok(versions)
-        } else if deprecated_config.map(|v| v.exists()).unwrap_or_default() {
+        } else if Versions::deprecated_versions_exists(versions_file) {
             // In this case is an old index, therefore we create the versions file
             // with the index versions that where available that moment.
             let versions = Versions::new_from_deprecated();
