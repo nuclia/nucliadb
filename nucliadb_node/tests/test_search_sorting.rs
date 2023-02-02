f7769e005ecba34279d168930edaf4471c7b102e
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-02-02 16:37:35 +0100

    Add test for search sorting

---
+++ nucliadb_node/tests/test_search_sorting.rs
@@ -0,0 +1,122 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+mod common;
+
+use std::collections::HashMap;
+use std::time::SystemTime;
+
+use common::{node_services, TestNodeWriter};
+use nucliadb_core::protos::op_status::Status;
+use nucliadb_core::protos::prost_types::Timestamp;
+use nucliadb_core::protos::resource::ResourceStatus;
+use nucliadb_core::protos::{EmptyQuery, IndexMetadata, Resource, ResourceId, SearchRequest};
+use tonic::Request;
+use uuid::Uuid;
+
+async fn create_dummy_resources(total: u8, writer: &mut TestNodeWriter, shard_id: String) {
+    for i in 0..total {
+        let rid = Uuid::new_v4();
+        let field = format!("dummy-{i}");
+
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let timestamp = Timestamp {
+            seconds: now.as_secs() as i64,
+            nanos: 0,
+        };
+
+        let labels = vec![format!("/dummy{i}")];
+        let mut texts = HashMap::new();
+        texts.insert(
+            field,
+            nucliadb_protos::TextInformation {
+                text: format!("Dummy text {i}"),
+                labels: vec![],
+            },
+        );
+        let result = writer
+            .set_resource(Resource {
+                shard_id: shard_id.clone(),
+                resource: Some(ResourceId {
+                    shard_id: shard_id.clone(),
+                    uuid: rid.to_string(),
+                }),
+                status: ResourceStatus::Processed as i32,
+                metadata: Some(IndexMetadata {
+                    created: Some(timestamp.clone()),
+                    modified: Some(timestamp),
+                }),
+                labels,
+                texts,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(result.get_ref().status(), Status::Ok);
+    }
+}
+
+#[tokio::test]
+async fn test_search_sorting() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut reader, mut writer) = node_services().await;
+
+    let new_shard_response = writer.new_shard(Request::new(EmptyQuery {})).await?;
+    let shard_id = &new_shard_response.get_ref().id;
+
+    create_dummy_resources(20, &mut writer, shard_id.clone()).await;
+
+    // --------------------------------------------------------------
+    // Test: sort by creation date in descending order
+    // --------------------------------------------------------------
+
+    let response = reader
+        .search(SearchRequest {
+            shard: shard_id.clone(),
+            order: Some(nucliadb_protos::OrderBy {
+                sort_by: nucliadb_protos::order_by::OrderField::Created.into(),
+                r#type: nucliadb_protos::order_by::OrderType::Desc.into(),
+                ..Default::default()
+            }),
+            document: true,
+            result_per_page: 20,
+            ..Default::default()
+        })
+        .await?;
+
+    let fields = response
+        .get_ref()
+        .document
+        .as_ref()
+        .unwrap()
+        .results
+        .iter()
+        .cloned()
+        .map(|r| r.field)
+        .collect::<Vec<String>>();
+
+    let mut sorted_fields = fields.clone();
+    sorted_fields.sort();
+    assert_eq!(fields, sorted_fields);
+
+    Ok(())
+}
