905389c43776acb8f731e88f03a544ba846f9734
Alexis Le Provost <alexis.leprovost@outlook.com>
2023-02-09 15:22:52 +0100

    Add metrics notification on node writer (#576)
    * Added `paragraph_count` method
    
    * Added metadata attribute on `NodeWriterService`
    
    * Initialized metadata at startup
    
    * Added metadata method
    
    * Applied cargo fmt
    
    * Used channel instead of in-memory node writer metadata
    
    * Fixed shards folder deletion bug
    
    * Fixed shard deletion event bug
    
    * Moved writer tasks into free-functions
    
    * Removed unused function
    
    * Moved notification mechanism into gRPC interface
    
    * Applied cargofmt
    
    * Fixed integration tests

--- nucliadb_node/src/bin/writer.rs
+++ nucliadb_node/src/bin/writer.rs
@@ -1,4 +1,3 @@
-use std::fs;
 // Copyright (C) 2021 Bosutech XXI S.L.
 //
 // nucliadb is offered under the AGPL v3.0 and as commercial software.
@@ -17,21 +16,28 @@ use std::fs;
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+use std::fs;
 use std::net::SocketAddr;
 use std::path::Path;
 use std::str::FromStr;
 use std::time::Instant;
 
 use anyhow::{Context, Result};
-use nucliadb_cluster::{node, Key, Node, NodeType};
+use futures::Stream;
+use nucliadb_cluster::{node, Key, Node, NodeHandle, NodeType};
 use nucliadb_core::protos::node_writer_server::NodeWriterServer;
 use nucliadb_core::protos::GetShardRequest;
 use nucliadb_core::tracing::*;
 use nucliadb_node::env;
 use nucliadb_node::reader::NodeReaderService;
 use nucliadb_node::telemetry::init_telemetry;
-use nucliadb_node::writer::grpc_driver::NodeWriterGRPCDriver;
+use nucliadb_node::writer::grpc_driver::{
+    NodeWriterEvent, NodeWriterGRPCDriver, NodeWriterMetadata,
+};
 use nucliadb_node::writer::NodeWriterService;
+use tokio::sync::mpsc::UnboundedReceiver;
 use tokio_stream::StreamExt;
 use tonic::transport::Server;
 use uuid::Uuid;
@@ -46,6 +52,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let start_bootstrap = Instant::now();
 
+    let node_metadata = get_node_metadata();
     let mut node_writer_service = NodeWriterService::new();
 
     std::fs::create_dir_all(env::shards_path())?;
@@ -53,7 +60,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         node_writer_service.load_shards()?;
     }
 
-    let grpc_driver = NodeWriterGRPCDriver::from(node_writer_service);
+    let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
+    let grpc_driver = NodeWriterGRPCDriver::from(node_writer_service).with_sender(sender);
     let host_key_path = env::host_key_path();
     let public_ip = env::public_ip().await;
     let chitchat_port = env::chitchat_port();
@@ -69,99 +77,99 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .on_local_network(chitchat_addr)
         .with_id(host_key.to_string())
         .with_seed_nodes(seed_nodes)
-        .insert_to_initial_state(LOAD_SCORE_KEY, 0.0)
-        .insert_to_initial_state(SHARD_COUNT_KEY, 0)
+        .insert_to_initial_state(LOAD_SCORE_KEY, node_metadata.paragraph_count as f32)
+        .insert_to_initial_state(SHARD_COUNT_KEY, node_metadata.shard_count)
         .build()?;
 
     let node = node.start().await?;
 
-    let writer_task = tokio::spawn(async move {
-        let addr = env::writer_listen_address();
-        info!("Writer listening for gRPC requests at: {:?}", addr);
-        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-        health_reporter
-            .set_serving::<NodeWriterServer<NodeWriterGRPCDriver>>()
-            .await;
-
-        Server::builder()
-            .add_service(health_service)
-            .add_service(NodeWriterServer::new(grpc_driver))
-            .serve(addr)
-            .await
-            .expect("Error starting gRPC writer");
-    });
+    let writer_task = tokio::spawn(start_grpc_service(grpc_driver));
 
     let telemetry_handle = nucliadb_telemetry::sync::start_telemetry_loop();
-    let mut cluster_watcher = node.cluster_watcher().await;
-    let monitor_task = tokio::task::spawn(async move {
-        loop {
-            debug!("node writer wait updates");
-            if let Some(live_nodes) = cluster_watcher.next().await {
-                let cluster_snapshot = node::cluster_snapshot(live_nodes).await;
 
-                if let Ok(json_update) = serde_json::to_string(&cluster_snapshot) {
-                    info!("Chitchat cluster updated: {json_update}");
-                };
-            } else {
-                error!("Chitchat cluster updated monitor fail");
-            }
-        }
-    });
+    let cluster_watcher = node.cluster_watcher().await;
+    let monitor_task = tokio::task::spawn(monitor_cluster(cluster_watcher));
 
-    let metrics_task = tokio::spawn(async move {
-        info!("Start metrics task");
-
-        let mut node_reader = NodeReaderService::new();
-        let mut interval = tokio::time::interval(env::get_metrics_update_interval());
-
-        loop {
-            interval.tick().await;
-
-            let mut shard_count = 0;
-            let mut paragraph_count = 0;
-
-            let shards = match node_reader.iter_shards() {
-                Ok(shards) => shards,
-                Err(e) => {
-                    error!("Cannot read shards folder: {e}");
-                    continue;
-                }
-            };
-
-            for shard in shards {
-                let shard = match shard {
-                    Ok(shard) => shard,
-                    Err(e) => {
-                        error!("Cannot load shard: {e}");
-                        continue;
-                    }
-                };
-
-                match shard.get_info(&GetShardRequest::default()) {
-                    Ok(count) => {
-                        shard_count += 1;
-                        paragraph_count += count.paragraphs;
-                    }
-                    Err(e) => error!("Cannot get metrics for {}: {e:?}", shard.id),
-                }
-            }
-
-            info!("Update node state: load_score = {paragraph_count}");
-            node.update_state(LOAD_SCORE_KEY, paragraph_count as f32)
-                .await;
-            info!("Update node state: shard_count = {shard_count}");
-            node.update_state(SHARD_COUNT_KEY, shard_count as u64).await;
-        }
-    });
+    let update_task = tokio::spawn(watch_node_update(node, receiver, node_metadata));
 
     info!("Bootstrap complete in: {:?}", start_bootstrap.elapsed());
     eprintln!("Running");
 
-    tokio::try_join!(writer_task, monitor_task, metrics_task)?;
+    tokio::try_join!(writer_task, monitor_task, update_task)?;
     telemetry_handle.terminate_telemetry().await;
     // node_writer_service.shutdown().await;
 
     Ok(())
+}
+
+pub async fn start_grpc_service(grpc_driver: NodeWriterGRPCDriver) {
+    let addr = env::writer_listen_address();
+
+    info!("Listening for gRPC requests at: {:?}", addr);
+
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+    health_reporter
+        .set_serving::<NodeWriterServer<NodeWriterGRPCDriver>>()
+        .await;
+
+    Server::builder()
+        .add_service(health_service)
+        .add_service(NodeWriterServer::new(grpc_driver))
+        .serve(addr)
+        .await
+        .expect("Error starting gRPC service");
+}
+
+pub async fn monitor_cluster(cluster_watcher: impl Stream<Item = Vec<NodeHandle>>) {
+    info!("Start cluster monitoring");
+
+    let mut cluster_watcher = Box::pin(cluster_watcher);
+
+    loop {
+        debug!("Wait for cluster update");
+        if let Some(live_nodes) = cluster_watcher.next().await {
+            let cluster_snapshot = node::cluster_snapshot(live_nodes).await;
+
+            if let Ok(snapshot) = serde_json::to_string(&cluster_snapshot) {
+                info!("Cluster update: {snapshot}");
+            };
+        } else {
+            error!("Cluster snapshot cannot be serialized");
+        }
+    }
+}
+
+pub async fn watch_node_update(
+    node: NodeHandle,
+    mut receiver: UnboundedReceiver<NodeWriterEvent>,
+    mut metadata: NodeWriterMetadata,
+) {
+    info!("Start node update task");
+
+    while let Some(event) = receiver.recv().await {
+        match event {
+            NodeWriterEvent::ShardCreation => {
+                metadata.shard_count = metadata.shard_count.saturating_add(1);
+                info!("Update node state: shard_count = {}", metadata.shard_count);
+                node.update_state(SHARD_COUNT_KEY, metadata.shard_count)
+                    .await;
+            }
+            NodeWriterEvent::ShardDeletion => {
+                metadata.shard_count = metadata.shard_count.saturating_sub(1);
+                info!("Update node state: shard_count = {}", metadata.shard_count);
+                node.update_state(SHARD_COUNT_KEY, metadata.shard_count)
+                    .await;
+            }
+            NodeWriterEvent::ParagraphCount(paragraph_count) => {
+                metadata.paragraph_count = paragraph_count;
+                info!("Update node state: load_score = {paragraph_count}");
+                node.update_state(LOAD_SCORE_KEY, paragraph_count as f32)
+                    .await;
+            }
+        }
+    }
+
+    info!("Node update task stopped");
 }
 
 pub fn read_host_key(host_key_path: &Path) -> Result<Uuid> {
@@ -199,4 +207,39 @@ pub fn read_or_create_host_key(host_key_path: &Path) -> Result<Uuid> {
     }
 
     Ok(host_key)
+}
+
+pub fn get_node_metadata() -> NodeWriterMetadata {
+    let mut node_reader = NodeReaderService::new();
+
+    let shards = match node_reader.iter_shards() {
+        Ok(shards) => shards,
+        Err(e) => {
+            error!("Cannot read shards folder: {e}");
+
+            return NodeWriterMetadata::default();
+        }
+    };
+
+    let mut node_metadata = NodeWriterMetadata::default();
+
+    for shard in shards {
+        let shard = match shard {
+            Ok(shard) => shard,
+            Err(e) => {
+                error!("Cannot load shard: {e}");
+                continue;
+            }
+        };
+
+        match shard.get_info(&GetShardRequest::default()) {
+            Ok(count) => {
+                node_metadata.shard_count += 1;
+                node_metadata.paragraph_count += count.paragraphs as u64;
+            }
+            Err(e) => error!("Cannot get metrics for {}: {e:?}", shard.id),
+        }
+    }
+
+    node_metadata
 }
