2d62f71731955ee22f81594eb5b60c9b2292a74d
Ramon Navarro Bosch <ramon@nuclia.io>
2023-02-15 13:12:27 +0100

    Fix mypy

--- nucliadb_sdk/nucliadb_sdk/client.py
+++ nucliadb_sdk/nucliadb_sdk/client.py
@@ -71,7 +71,7 @@ class NucliaDBClient:
     def __init__(
         self,
         *,
-        environment: Optional[Environment] = Environment.CLOUD,
+        environment: Environment = Environment.CLOUD,
         url: Optional[str] = None,
         api_key: Optional[str] = None,
         writer_host: Optional[str] = None,
