491571e8bebc302cdc70bb0f49a28a48521299e5
Ferran Llamas <llamas.arroniz@gmail.com>
2023-03-24 19:16:18 +0100

    wip

--- nucliadb/nucliadb/writer/api/v1/services.py
+++ nucliadb/nucliadb/writer/api/v1/services.py
@@ -22,17 +22,14 @@ from fastapi_versioning import version
 from nucliadb_protos.knowledgebox_pb2 import KnowledgeBoxID
 from nucliadb_protos.knowledgebox_pb2 import Label as LabelPB
 from nucliadb_protos.knowledgebox_pb2 import LabelSet as LabelSetPB
-from nucliadb_protos.knowledgebox_pb2 import Widget as WidgetPB
 from nucliadb_protos.writer_pb2 import (
     DelEntitiesRequest,
     DelLabelsRequest,
     DelVectorSetRequest,
-    DetWidgetsRequest,
     OpStatusWriter,
     SetEntitiesRequest,
     SetLabelsRequest,
     SetSynonymsRequest,
-    SetWidgetsRequest,
 )
 from starlette.requests import Request
 
@@ -43,7 +40,6 @@ from nucliadb_models.labels import LabelSet
 from nucliadb_models.resource import NucliaDBRoles
 from nucliadb_models.synonyms import KnowledgeBoxSynonyms
 from nucliadb_models.vectors import VectorSet
-from nucliadb_models.widgets import Widget, WidgetMode
 from nucliadb_telemetry.utils import set_info_on_span
 from nucliadb_utils.authentication import requires
 from nucliadb_utils.utilities import get_ingest
@@ -191,87 +187,6 @@ async def delete_labels(request: Request, kbid: str, labelset: str):
     elif status.status == OpStatusWriter.Status.ERROR:
         raise HTTPException(
             status_code=500, detail="Error on deleting labels from a Knowledge box"
-        )
-    return Response(status_code=204)
-
-
-@api.post(
-    f"/{KB_PREFIX}/{{kbid}}/widget/{{widget}}",
-    status_code=200,
-    name="Set Knowledge Box Widgets",
-    tags=["Knowledge Box Services"],
-    openapi_extra={"x-operation_order": 1},
-)
-@requires(NucliaDBRoles.WRITER)
-@version(1)
-async def set_widget(request: Request, kbid: str, widget: str, item: Widget):
-    ingest = get_ingest()
-    pbrequest: SetWidgetsRequest = SetWidgetsRequest()
-    pbrequest.kb.uuid = kbid
-    pbrequest.widget.id = widget
-    set_info_on_span({"nuclia.kbid": kbid})
-    if item.description:
-        pbrequest.widget.description = item.description
-
-    if item.mode:
-        if item.mode == WidgetMode.BUTTON:
-            pbrequest.widget.mode = WidgetPB.WidgetMode.BUTTON
-        elif item.mode == WidgetMode.INPUT:
-            pbrequest.widget.mode = WidgetPB.WidgetMode.INPUT
-        elif item.mode == WidgetMode.FORM:
-            pbrequest.widget.mode = WidgetPB.WidgetMode.FORM
-
-    if item.features:
-        pbrequest.widget.features.useFilters = item.features.useFilters
-        pbrequest.widget.features.suggestEntities = item.features.suggestEntities
-        pbrequest.widget.features.suggestSentences = item.features.suggestSentences
-        pbrequest.widget.features.suggestParagraphs = item.features.suggestParagraphs
-        pbrequest.widget.features.suggestLabels = item.features.suggestLabels
-        pbrequest.widget.features.editLabels = item.features.editLabels
-        pbrequest.widget.features.entityAnnotation = item.features.entityAnnotation
-
-    for filter_input in item.filters:
-        pbrequest.widget.filters.append(filter_input)
-
-    for entity_input in item.topEntities:
-        pbrequest.widget.topEntities.append(entity_input)
-
-    pbrequest.widget.style.update(item.style)
-
-    status: OpStatusWriter = await ingest.SetWidgets(pbrequest)  # type: ignore
-    if status.status == OpStatusWriter.Status.OK:
-        return None
-    elif status.status == OpStatusWriter.Status.NOTFOUND:
-        raise HTTPException(status_code=404, detail="Knowledge Box does not exist")
-    elif status.status == OpStatusWriter.Status.ERROR:
-        raise HTTPException(
-            status_code=500, detail="Error on settings widgets on a Knowledge box"
-        )
-
-
-@api.delete(
-    f"/{KB_PREFIX}/{{kbid}}/widget/{{widget}}",
-    status_code=200,
-    name="Delete Knowledge Box Widget",
-    tags=["Knowledge Box Services"],
-    openapi_extra={"x-operation_order": 3},
-)
-@requires(NucliaDBRoles.WRITER)
-@version(1)
-async def delete_widget(request: Request, kbid: str, widget: str):
-    ingest = get_ingest()
-    pbrequest: DetWidgetsRequest = DetWidgetsRequest()
-    pbrequest.kb.uuid = kbid
-    pbrequest.widget = widget
-    set_info_on_span({"nuclia.kbid": kbid})
-    status: OpStatusWriter = await ingest.DelWidgets(pbrequest)  # type: ignore
-    if status.status == OpStatusWriter.Status.OK:
-        return None
-    elif status.status == OpStatusWriter.Status.NOTFOUND:
-        raise HTTPException(status_code=404, detail="Knowledge Box does not exist")
-    elif status.status == OpStatusWriter.Status.ERROR:
-        raise HTTPException(
-            status_code=500, detail="Error on deleting widget from a Knowledge box"
         )
     return Response(status_code=204)
 
