428ebb71635ead51b24d661b02fb6e571f667090
Ramon Navarro Bosch <ramon@nuclia.com>
2023-03-27 12:59:11 +0200

    Fix feedback (#716)
    * Fix feedback
    
    * lint

--- nucliadb/nucliadb/search/predict.py
+++ nucliadb/nucliadb/search/predict.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import json
 from typing import AsyncIterator, List, Optional, Tuple
 
 import aiohttp
@@ -98,7 +97,7 @@ class PredictEngine:
             # Upload the payload
             resp = await self.session.post(
                 url=f"{self.cluster_url}{PRIVATE_PREDICT}{CHAT}",
-                json=json.dumps(data),
+                json=data,
                 headers={"X-STF-KBID": kbid},
             )
             if resp.status != 200:
@@ -113,7 +112,7 @@ class PredictEngine:
             headers = {"X-STF-NUAKEY": f"Bearer {self.nuclia_service_account}"}
             resp = await self.session.post(
                 url=f"{self.public_url}{PUBLIC_PREDICT}{CHAT}",
-                json=json.dumps(data),
+                json=data,
                 headers=headers,
             )
             if resp.status != 200:
