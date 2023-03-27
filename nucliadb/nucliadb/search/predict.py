1429d23a49f08d504570c66e760da9e07f1ed46d
Ramon Navarro Bosch <ramon@nuclia.io>
2023-03-27 12:42:23 +0200

    lint

--- nucliadb/nucliadb/search/predict.py
+++ nucliadb/nucliadb/search/predict.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import json
 from typing import AsyncIterator, List, Optional, Tuple
 
 import aiohttp
