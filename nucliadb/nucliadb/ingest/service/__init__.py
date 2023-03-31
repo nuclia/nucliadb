d3d4f0442dd47935c00c232d881740f96ff4c353
Ramon Navarro Bosch <ramon@nuclia.io>
2023-03-31 13:40:20 +0200

    Fix health

--- nucliadb/nucliadb/ingest/service/__init__.py
+++ nucliadb/nucliadb/ingest/service/__init__.py
@@ -17,35 +17,33 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import asyncio
 from typing import Optional
 
 from grpc import aio  # type: ignore
-from grpc_health.v1 import health, health_pb2, health_pb2_grpc  # type: ignore
+from grpc_health.v1 import health, health_pb2_grpc  # type: ignore
 
 from nucliadb.ingest import logger
-from nucliadb.ingest.orm import NODES
 from nucliadb.ingest.service.writer import WriterServicer
-from nucliadb.ingest.settings import DriverConfig, settings
+from nucliadb.ingest.settings import settings
 from nucliadb_protos import writer_pb2_grpc
 from nucliadb_telemetry.grpc import OpenTelemetryGRPC
 from nucliadb_telemetry.utils import get_telemetry, init_telemetry
 
-
-async def health_check(health_servicer):
-    while True:
-        if len(NODES) == 0 and settings.driver != DriverConfig.local:
-            await health_servicer.set("", health_pb2.HealthCheckResponse.NOT_SERVING)
-        else:
-            await health_servicer.set("", health_pb2.HealthCheckResponse.SERVING)
-        try:
-            await asyncio.sleep(1)
-        except (
-            asyncio.CancelledError,
-            asyncio.TimeoutError,
-            KeyboardInterrupt,
-        ):
-            return
+# Removed: ingest  should work when NODES are not all defined
+# async def health_check(health_servicer):
+#     while True:
+#         if len(NODES) == 0 and settings.driver != DriverConfig.local:
+#             await health_servicer.set("", health_pb2.HealthCheckResponse.NOT_SERVING)
+#         else:
+#             await health_servicer.set("", health_pb2.HealthCheckResponse.SERVING)
+#         try:
+#             await asyncio.sleep(1)
+#         except (
+#             asyncio.CancelledError,
+#             asyncio.TimeoutError,
+#             KeyboardInterrupt,
+#         ):
+#             return
 
 
 async def start_grpc(service_name: Optional[str] = None):
@@ -77,10 +75,7 @@ async def start_grpc(service_name: Optional[str] = None):
         f"======= Ingest GRPC serving on http://0.0.0.0:{settings.grpc_port}/ ======"
     )
 
-    health_task = asyncio.create_task(health_check(health_servicer))
-
     async def finalizer():
-        health_task.cancel()
         await servicer.finalize()
         await server.stop(grace=False)
 
