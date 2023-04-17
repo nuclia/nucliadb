7ec4807fd32f123fba614d257ba1185952fa46b2
Nathan Van Gheem <vangheem@gmail.com>
2023-04-17 08:42:42 -0400

    remove write sync metrics since they can not work properly (#803)
    * remove write sync metrics since they can not work properly with the number of files we have on a node
    
    * hmmm, where did that come from
    
    * fix

--- nucliadb_utils/nucliadb_utils/featureflagging.py
+++ nucliadb_utils/nucliadb_utils/featureflagging.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import json
-from typing import Optional
+from typing import Any, Optional
 
 import mrflagly
 import pydantic
@@ -31,9 +31,7 @@ class Settings(pydantic.BaseSettings):
     flag_settings_url: Optional[str]
 
 
-DEFAULT_FLAG_DATA = {
-    "nucliadb_node_sync_fs": {"rollout": 0, "variants": {"environment": ["stage"]}},
-}
+DEFAULT_FLAG_DATA: dict[str, Any] = {}
 
 
 class FlagService:
