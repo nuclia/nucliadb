37ffcf6b07242501abb729fe39c70971c953f606
Ramon Navarro Bosch <ramon@nuclia.io>
2023-04-17 12:06:22 +0200

    Fix lint

--- nucliadb/nucliadb/search/predict.py
+++ nucliadb/nucliadb/search/predict.py
@@ -169,7 +169,9 @@ class PredictEngine:
             )
         else:
             if self.nuclia_service_account is None:
-                error = "Nuclia Service account is not defined so could not rephrase query"
+                error = (
+                    "Nuclia Service account is not defined so could not rephrase query"
+                )
                 logger.warning(error)
                 raise SendToPredictError(error)
             # Upload the payload
