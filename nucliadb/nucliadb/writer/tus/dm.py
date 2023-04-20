f7be77f7506598b3f4a3d2c2fbe500f50361f226
Nathan Van Gheem <vangheem@gmail.com>
2023-04-20 07:03:47 -0400

    Revert "do not leak redis connections in file data manager (#808)" (#837)
    This reverts commit dae5ce9f1631f479725bea69a3616f04640fd7c6.

--- nucliadb/nucliadb/writer/tus/dm.py
+++ nucliadb/nucliadb/writer/tus/dm.py
@@ -40,9 +40,6 @@ class FileDataMangaer:
     key = None
     _ttl = 60 * 50 * 5  # 5 minutes should be plenty of time between activity
 
-    async def finalize(self):
-        ...
-
     async def load(self, key):
         # preload data
         self.key = key
@@ -121,9 +118,6 @@ class FileDataMangaer:
 class RedisFileDataManager(FileDataMangaer):
     def __init__(self, redis_url: str):
         self.redis = aioredis.from_url(redis_url)
-
-    async def finalize(self):
-        await self.redis.close(close_connection_pool=True)
 
     async def load(self, key):
         # preload data
