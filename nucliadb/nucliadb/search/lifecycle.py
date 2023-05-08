8aad4dabbe77407f540ff68e7869de26d5e432ff
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-08 16:37:22 +0200

    fix

--- nucliadb/nucliadb/search/lifecycle.py
+++ nucliadb/nucliadb/search/lifecycle.py
@@ -22,7 +22,6 @@ from nucliadb.ingest.orm.nodes_manager import NodesManager
 from nucliadb.ingest.utils import get_driver  # type: ignore
 from nucliadb.ingest.utils import start_ingest, stop_ingest
 from nucliadb.search import SERVICE_NAME
-from nucliadb.search.search import paragraphs
 from nucliadb.search.predict import start_predict_engine
 from nucliadb.search.search import paragraphs
 from nucliadb_telemetry.utils import clean_telemetry, setup_telemetry
