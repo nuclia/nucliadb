a8eed056f475f2062ba947df9e5db5a3182fcd0c
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-15 13:48:35 +0200

    remove unused import

--- nucliadb_utils/nucliadb_utils/transaction.py
+++ nucliadb_utils/nucliadb_utils/transaction.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from __future__ import barry_as_FLUFL
-
 import asyncio
 import uuid
 from asyncio import Event
