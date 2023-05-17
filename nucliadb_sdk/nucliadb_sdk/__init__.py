49b576d9d61989cb86d6ef72a2ba2ed851c977a3
Nathan Van Gheem <vangheem@gmail.com>
2023-05-17 14:57:23 -0400

    SDK v2 (#864)

--- nucliadb_sdk/nucliadb_sdk/__init__.py
+++ nucliadb_sdk/nucliadb_sdk/__init__.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# Old SDK APIs that should eventually be removed
 from nucliadb_sdk.entities import Entity
 from nucliadb_sdk.file import File
 from nucliadb_sdk.knowledgebox import KnowledgeBox
@@ -28,9 +29,15 @@ from nucliadb_sdk.utils import (
     get_or_create,
     list_kbs,
 )
+from nucliadb_sdk.v2 import NucliaSDK, Region, exceptions
 from nucliadb_sdk.vectors import Vector
 
 __all__ = (
+    "NucliaSDK",
+    "Region",
+    "exceptions",
+    # OLD support APIs
+    # Plan on removing these in the future
     "File",
     "KnowledgeBox",
     "Entity",
--- nucliadb_sdk/nucliadb_sdk/v2/__init__.py
+++ nucliadb_sdk/nucliadb_sdk/v2/__init__.py
@@ -16,17 +16,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+from .sdk import NucliaSDK, Region
 
-import pytest
-
-import nucliadb_sdk
-from nucliadb_models.resource import KnowledgeBoxObj
-
-
-def test_create_kb(sdk: nucliadb_sdk.NucliaSDK):
-    kb: KnowledgeBoxObj = sdk.create_knowledge_box(slug="hola")
-    assert sdk.get_knowledge_box(kbid=kb.uuid) is not None
-    assert sdk.get_knowledge_box_by_slug(slug="hola") is not None
-
-    with pytest.raises(nucliadb_sdk.exceptions.ConflictError):
-        sdk.create_knowledge_box(slug="hola")
+__all__ = ("NucliaSDK", "Region")
