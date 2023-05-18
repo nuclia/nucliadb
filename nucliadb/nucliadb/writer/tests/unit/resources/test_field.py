3d4e11d4fb46e3b72a2cdabcc335ce6b8a16a1f0
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-18 06:33:58 +0200

    add unit test

--- nucliadb/nucliadb/writer/tests/unit/resources/test_field.py
+++ nucliadb/nucliadb/writer/tests/unit/resources/test_field.py
@@ -18,34 +18,45 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 from unittest import mock
-from nucliadb_models.common import FileB64
-from nucliadb_models.conversation import InputConversationField, InputMessageContent, InputMessage
 
 import pytest
+from nucliadb_protos.resources_pb2 import CloudFile, MessageContent
 from nucliadb_protos.writer_pb2 import BrokerMessage
 
 from nucliadb.ingest.processing import PushPayload
 from nucliadb.writer.resource.field import parse_conversation_field, parse_file_field
 from nucliadb_models import File, FileField
+from nucliadb_models.common import FileB64
+from nucliadb_models.conversation import (
+    InputConversationField,
+    InputMessage,
+    InputMessageContent,
+    PushMessageFormat,
+)
 
 FIELD_MODULE = "nucliadb.writer.resource.field"
 
 
 @pytest.fixture(scope="function")
 def processing_mock():
-    with mock.patch(f"{FIELD_MODULE}.get_processing") as get_processing_mock:
-        processing = mock.Mock()
-        processing.convert_filefield_to_str = mock.AsyncMock(return_value="internal")
-        processing.convert_external_filefield_to_str.return_value = "external"
-        get_processing_mock.return_value = processing
+    processing = mock.Mock()
+    processing.convert_filefield_to_str = mock.AsyncMock(return_value="internal")
+    processing.convert_external_filefield_to_str.return_value = "external"
+    processing.convert_internal_cf_to_str = mock.AsyncMock(return_value="internal")
+    with mock.patch(f"{FIELD_MODULE}.get_processing", return_value=processing):
         yield processing
 
 
 @pytest.fixture(scope="function")
 def storage_mock():
     cf = CloudFile()
+    cf.bucket_name = "bucket"
+    cf.uri = "fullkey"
+    cf.filename = "payload.pb"
+
     storage = mock.AsyncMock()
     storage.upload_b64file_to_cloudfile = mock.AsyncMock(return_value=cf)
+
     with mock.patch(f"{FIELD_MODULE}.get_storage", return_value=storage):
         yield storage
 
@@ -78,8 +89,10 @@ async def test_parse_file_field_does_not_store_password(processing_mock, file_fi
 
     assert not writer_bm.files[field_key].password
 
+
 def get_file():
     return FileB64(filename="myfile.pdf", payload="foo", md5="bar")
+
 
 def get_message(id, who, to, text, attachments=None):
     content = InputMessageContent(text=text)
@@ -99,12 +112,59 @@ async def test_parse_conversation_field(storage_mock, processing_mock):
     bm = BrokerMessage()
     pp = PushPayload(uuid=uuid, kbid=kbid, partition=1, userid="user")
     attachment = get_file()
-    m1 = get_message(id="m1", who="ramon", to="nucliadb", text="look at this idea", attachments=[attachment])
+    m1 = get_message(
+        id="m1",
+        who="ramon",
+        to="nucliadb",
+        text="look at this idea",
+        attachments=[attachment],
+    )
     m2 = get_message(id="m2", who="nucliadb", to="ramon", text="ok")
 
     conversation_field = InputConversationField(messages=[m1, m2])
 
     await parse_conversation_field(key, conversation_field, bm, pp, kbid, uuid)
 
-    breakpoint()
-    pass
+    # Check push payload
+    assert len(pp.conversationfield[key].messages) == 2
+    ppm1 = pp.conversationfield[key].messages[0]
+    assert ppm1.ident == "m1"
+    assert ppm1.who == "ramon"
+    assert ppm1.to == ["nucliadb"]
+    assert ppm1.content.text == "look at this idea"
+    assert ppm1.content.format == PushMessageFormat.PLAIN
+    assert ppm1.content.attachments[0] == "internal"
+    ppm2 = pp.conversationfield[key].messages[1]
+    assert ppm2.ident == "m2"
+    assert ppm2.who == "nucliadb"
+    assert ppm2.to == ["ramon"]
+    assert ppm2.content.text == "ok"
+    assert ppm2.content.format == PushMessageFormat.PLAIN
+    assert ppm2.content.attachments == []
+
+    # Check broker message
+    bmm1 = bm.conversations[key].messages[0]
+    assert bmm1.ident == "m1"
+    assert bmm1.who == "ramon"
+    assert bmm1.to == ["nucliadb"]
+    assert bmm1.content.text == "look at this idea"
+    assert bmm1.content.format == MessageContent.Format.PLAIN
+    assert len(bmm1.content.attachments) == 1
+    cf = bmm1.content.attachments[0]
+    assert cf.filename == "payload.pb"
+    assert cf.bucket_name == "bucket"
+    assert cf.uri == "fullkey"
+    bmm2 = bm.conversations[key].messages[1]
+    assert bmm2.ident == "m2"
+    assert bmm2.who == "nucliadb"
+    assert bmm2.to == ["ramon"]
+    assert bmm2.content.text == "ok"
+    assert bmm2.content.format == MessageContent.Format.PLAIN
+    assert len(bmm2.content.attachments) == 0
+
+    # Check storage calls
+    assert storage_mock.conversation_field.call_count == 1
+    assert storage_mock.upload_b64file_to_cloudfile.await_count == 1
+
+    # Check processing calls
+    assert processing_mock.convert_internal_cf_to_str.await_count == 1
