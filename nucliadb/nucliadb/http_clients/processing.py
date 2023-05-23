ed44a250bcd372744c9cee2c1faeaabe756ed741
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-23 09:06:22 +0200

    Few fixes

--- nucliadb/nucliadb/http_clients/processing.py
+++ nucliadb/nucliadb/http_clients/processing.py
@@ -89,6 +89,7 @@ class StatusResponse(pydantic.BaseModel):
 class PullResponse(pydantic.BaseModel):
     status: str
     payload: Optional[str] = None
+    msgid: int
 
 
 class ProcessingHTTPClient:
