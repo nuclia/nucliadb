120544313d356bb973e61588825a13e66df6c7aa
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-23 16:51:42 +0200

    Make it optional to be safe

--- nucliadb/nucliadb/http_clients/processing.py
+++ nucliadb/nucliadb/http_clients/processing.py
@@ -89,7 +89,7 @@ class StatusResponse(pydantic.BaseModel):
 class PullResponse(pydantic.BaseModel):
     status: str
     payload: Optional[str] = None
-    msgid: int
+    msgid: Optional[int] = None
 
 
 class ProcessingHTTPClient:
