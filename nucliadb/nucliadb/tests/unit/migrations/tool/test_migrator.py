056f6cd9b14e247ac1145289cd20534b9e2075f1
vangheem <vangheem@gmail.com>
2023-06-16 14:56:40 -0400

    fix

--- nucliadb/nucliadb/tests/unit/migrations/tool/test_migrator.py
+++ nucliadb/nucliadb/tests/unit/migrations/tool/test_migrator.py
@@ -24,4 +24,4 @@ def test_get_migrations():
     migrations = migrator.get_migrations()
     assert len(migrations) == 1
     assert migrations[0].version == 1
-    assert migrations[0].module.__name__ == "nucliadb.migrations.noop_1"
+    assert migrations[0].module.__name__ == "nucliadb.migrations.bootstrap_1"
