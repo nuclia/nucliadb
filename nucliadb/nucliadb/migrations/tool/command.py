104b0ba33ddf0a3a38813f8151b9e01e94ad3903
vangheem <vangheem@gmail.com>
2023-06-16 14:07:05 -0400

    fixes

--- nucliadb/nucliadb/migrations/tool/command.py
+++ nucliadb/nucliadb/migrations/tool/command.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from nucliadb_utils.fastapi.run import serve_metrics
+
 from . import migrator
 from .context import ExecutionContext
 from .settings import Settings
@@ -9,10 +11,12 @@ async def run():
     settings = Settings()
     context = ExecutionContext(settings)
     await context.initialize()
+    metrics_server = await serve_metrics()
     try:
         await migrator.run(context)
     finally:
         await context.finalize()
+        await metrics_server.shutdown()
 
 
 def main():
