ec0cb9c49296d7774e03540e7513871a40b9c30c
Nathan Van Gheem <vangheem@gmail.com>
2023-06-16 22:06:31 -0400

    add bump action and tag stable (#1010)
    * add bump action and tag stable
    
    * revert version

--- bump.py
+++ bump.py
@@ -2,18 +2,38 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Bump version")
 parser.add_argument("--build", type=int, help="build number")
+parser.add_argument("--sem", type=str, help="Semantic version part")
+parser.add_argument("--minor", type=bool, help="Minor")
+parser.add_argument("--bug", type=bool, help="Bug")
 
 
-def run(build_num: int):
+def run(args):
     with open("VERSION", "r") as f:
         version = f.read().strip()
 
-    version = f"{version}-post{build_num}"
+    major, minor, bug = version.split(".")
+    major = int(major)
+    minor = int(minor)
+    bug = int(bug)
+    version_post = ""
 
+    if args.build:
+        version_post = f"-post{args.build}"
+    elif args.sem == "major":
+        major += 1
+        minor = 0
+        bug = 0
+    elif args.sem == "minor":
+        minor += 1
+        bug = 0
+    elif args.sem == "bug":
+        bug += 1
+
+    version = f"{major}.{minor}.{bug}{version_post}"
     with open("VERSION", "w") as f:
         f.write(version)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    run(args.build)
+    run(args)
