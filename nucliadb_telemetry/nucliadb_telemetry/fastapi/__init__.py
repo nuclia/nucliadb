661441cff4e7d0da48b1b9720bbe611e3983b6d4
Ferran Llamas <llamas.arroniz@gmail.com>
2023-06-21 12:33:50 +0200

    Simpler middleware: header is hardcoded

--- nucliadb_telemetry/nucliadb_telemetry/fastapi/__init__.py
+++ nucliadb_telemetry/nucliadb_telemetry/fastapi/__init__.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Iterable, List, Optional
+from typing import Iterable, List
 from urllib.parse import urlparse
 
 import prometheus_client  # type: ignore
