5dbbac1fdff0b4681e8424f98f9128a50a390375
Ferran Llamas <llamas.arroniz@gmail.com>
2023-06-22 09:51:46 +0200

    Do not offer by slug on resource search operations

--- nucliadb/nucliadb/search/api/v1/resource/chat.py
+++ nucliadb/nucliadb/search/api/v1/resource/chat.py
@@ -17,22 +17,18 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from typing import Optional, Union
+from typing import Union
 
 from fastapi import Header, Request, Response
 from fastapi_versioning import version
 from starlette.responses import StreamingResponse
 
-from nucliadb.ingest.serialize import get_resource_uuid_by_slug
 from nucliadb.models.responses import HTTPClientError
 from nucliadb.search.api.v1.find import find
 from nucliadb.search.api.v1.router import KB_PREFIX, api
 from nucliadb.search.predict import SendToPredictError
 from nucliadb.search.search.chat.query import chat, rephrase_query_from_context
-from nucliadb.search.search.exceptions import (
-    IncompleteFindResultsError,
-    ResourceNotFoundError,
-)
+from nucliadb.search.search.exceptions import IncompleteFindResultsError
 from nucliadb_models.resource import NucliaDBRoles
 from nucliadb_models.search import (
     ChatRequest,
@@ -54,7 +50,7 @@ from nucliadb_utils.exceptions import LimitsExceededError
 )
 @requires(NucliaDBRoles.READER)
 @version(1)
-async def chat_post_resource_by_id(
+async def resource_chat_endpoint(
     request: Request,
     response: Response,
     kbid: str,
@@ -64,60 +60,14 @@ async def chat_post_resource_by_id(
     x_nucliadb_user: str = Header(""),
     x_forwarded_for: str = Header(""),
 ) -> Union[StreamingResponse, HTTPClientError]:
-    return await chat_on_resource_endpoint(
-        response,
-        kbid,
-        rid=rid,
-        rslug=None,
-        item=item,
-        x_ndb_client=x_ndb_client,
-        x_nucliadb_user=x_nucliadb_user,
-        x_forwarded_for=x_forwarded_for,
-    )
-
-
-@api.post(
-    f"/{KB_PREFIX}/{{kbid}}/s/resource/{{rslug}}/chat",
-    status_code=200,
-    name="Chat with a Resource (by slug)",
-    summary="Chat with a resource",
-    description="Chat with a resource",
-    tags=["Search"],
-)
-@requires(NucliaDBRoles.READER)
-@version(1)
-async def chat_post_resource_by_slug(
-    request: Request,
-    response: Response,
-    kbid: str,
-    rslug: str,
-    item: ChatRequest,
-    x_ndb_client: NucliaDBClientType = Header(NucliaDBClientType.API),
-    x_nucliadb_user: str = Header(""),
-    x_forwarded_for: str = Header(""),
-) -> Union[StreamingResponse, HTTPClientError]:
-    return await chat_on_resource_endpoint(
-        response,
-        kbid,
-        rid=None,
-        rslug=rslug,
-        item=item,
-        x_ndb_client=x_ndb_client,
-        x_nucliadb_user=x_nucliadb_user,
-        x_forwarded_for=x_forwarded_for,
-    )
-
-
-async def chat_on_resource_endpoint(*args, **kwargs):
-    # All chat endpoint / view logic should be here
     try:
-        return await chat_on_resource(*args, **kwargs)
+        return await resource_chat(
+            response, kbid, rid, item, x_ndb_client, x_nucliadb_user, x_forwarded_for
+        )
     except LimitsExceededError as exc:
         return HTTPClientError(status_code=exc.status_code, detail=exc.detail)
     except SendToPredictError:
         return HTTPClientError(status_code=503, detail="Chat service not available")
-    except ResourceNotFoundError:
-        return HTTPClientError(status_code=404, detail="Resource not found")
     except IncompleteFindResultsError:
         return HTTPClientError(
             status_code=529,
@@ -125,25 +75,15 @@ async def chat_on_resource_endpoint(*args, **kwargs):
         )
 
 
-async def chat_on_resource(
+async def resource_chat(
     response: Response,
     kbid: str,
-    rid: Optional[str],
-    rslug: Optional[str],
+    rid: str,
     item: ChatRequest,
     x_ndb_client: NucliaDBClientType,
     x_nucliadb_user: str,
     x_forwarded_for: str,
 ):
-    if all([rid, rslug]) or not any([rid, rslug]):
-        raise ValueError("You must provide either rid or rslug")
-
-    if rid is None:
-        # We need the resource uuid to filter the search results
-        rid = await get_resource_uuid_by_slug(kbid, rslug)  # type: ignore
-        if rid is None:
-            raise ResourceNotFoundError()
-
     new_query = item.query
     if item.context and len(item.context) > 0:
         new_query = await rephrase_query_from_context(
