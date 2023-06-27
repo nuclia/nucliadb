b55ea38d7bf54362cdcfe670659fb785ea568772
vangheem <vangheem@gmail.com>
2023-06-27 13:27:43 -0400

    more tests

--- nucliadb/nucliadb/search/search/chat/prompt.py
+++ nucliadb/nucliadb/search/search/chat/prompt.py
@@ -31,15 +31,15 @@ from nucliadb_utils.utilities import get_storage
 
 GPT_4_ENC = tiktoken.encoding_for_model("gpt-4")
 
-
 # less than real because of prompt text size, understanding api isn't truncating
 # and hard fails when we're over the limit
-MAX_WORDS = 4000 * 0.75
-
+MAX_TOKENS = 4000 * 0.85
 
 # Number of messages to pull after a match in a message
 # The hope here is it will be enough to get the answer to the question.
 CONVERSATION_MESSAGE_CONTEXT_EXPANSION = 15
+
+PROMPT_TEXT_RESULT_SEP = " \n\n "
 
 
 async def get_next_conversation_messages(
@@ -129,7 +129,7 @@ async def format_chat_prompt_content(kbid: str, results: KnowledgeboxFindResults
         for field_path, paragraph in ordered_paras:
             text = paragraph.text.strip()
             words += len(GPT_4_ENC.encode(text))
-            if words >= MAX_WORDS:
+            if words >= MAX_TOKENS:
                 break
 
             output[paragraph.id] = text
@@ -142,9 +142,9 @@ async def format_chat_prompt_content(kbid: str, results: KnowledgeboxFindResults
                 for msg in expanded_msgs:
                     text = msg.content.text.strip()
                     words += len(GPT_4_ENC.encode(text))
-                    if words >= MAX_WORDS:
+                    if words >= MAX_TOKENS:
                         break
                     pid = f"{rid}/{field_type}/{field_id}/{msg.ident}/0-{len(msg.content.text) + 1}"
                     output[pid] = text
 
-    return " \n\n ".join(output.values())
+    return PROMPT_TEXT_RESULT_SEP.join(output.values())
