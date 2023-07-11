ba61597aa1e386b83255bb9b37bf17ee813a243e
Ferran Llamas <llamas.arroniz@gmail.com>
2023-07-11 20:05:04 +0200

    Fix score find results

--- nucliadb/nucliadb/search/tests/unit/test_find_merge.py
+++ nucliadb/nucliadb/search/tests/unit/test_find_merge.py
@@ -20,7 +20,10 @@
 
 import random
 
-from nucliadb.search.search.find_merge import Orderer
+from nucliadb_protos.nodereader_pb2 import DocumentScored, ParagraphResult
+
+from nucliadb.search.search.find_merge import Orderer, merge_paragraphs_vectors
+from nucliadb_models.search import SCORE_TYPE
 
 
 def test_orderer():
@@ -57,3 +60,53 @@ def test_orderer_handles_duplicate_insertions():
     orderer.add_boosted("c")
     orderer.add("a")
     assert list(orderer.sorted_by_insertion()) == ["a", "b", "c"]
+
+
+def test_merge_paragraphs_vectors():
+    paragraphs = []
+    for i in range(5):
+        pr = ParagraphResult()
+        pr.uuid = "foo"
+        pr.score.bm25 = i
+        pr.score.booster = 0
+        pr.paragraph = f"id/text/paragraph/{i}/0-10"
+        pr.start = 0
+        pr.end = 10
+        pr.field = "/a/title"
+        paragraphs.append(pr)
+
+    vectors = []
+    for i in range(5):
+        score = max(5 / float(i + 1), 1)
+        vr = DocumentScored()
+        vr.doc_id.id = f"id/vector/paragraph/{i}/0-2"
+        vr.score = score
+        vr.metadata.position.start = 0
+        vr.metadata.position.start = 2
+        vectors.append(vr)
+
+    paragraphs, next_page = merge_paragraphs_vectors(
+        [paragraphs], [vectors], 20, 0, min_score=1
+    )
+    assert not next_page
+    assert len(paragraphs) == 10
+
+    vector_scores = set()
+    for index, score_type in [
+        (0, SCORE_TYPE.BM25),
+        (1, SCORE_TYPE.VECTOR),
+        (2, SCORE_TYPE.BM25),
+        (3, SCORE_TYPE.BM25),
+        (4, SCORE_TYPE.VECTOR),
+        (5, SCORE_TYPE.BM25),
+        (6, SCORE_TYPE.BM25),
+        (7, SCORE_TYPE.VECTOR),
+        (8, SCORE_TYPE.VECTOR),
+        (9, SCORE_TYPE.VECTOR),
+    ]:
+        assert paragraphs[index].paragraph.score_type == score_type
+        if score_type == SCORE_TYPE.VECTOR:
+            vector_scores.add(paragraphs[index].paragraph.score)
+
+    # Check that the vector scores are different
+    assert len(vector_scores) == 5
