50d85901992a6a02b196339ab678a8429b02d047
vangheem <vangheem@gmail.com>
2023-07-14 08:43:53 -0400

    async version

--- nucliadb/nucliadb/common/cluster/discovery/k8s.py
+++ nucliadb/nucliadb/common/cluster/discovery/k8s.py
@@ -23,7 +23,9 @@ import logging
 import os
 from dataclasses import dataclass
 
-import kubernetes
+import kubernetes_asyncio.client
+import kubernetes_asyncio.config
+import kubernetes_asyncio.watch
 from nucliadb_protos.noderesources_pb2 import EmptyQuery
 
 from nucliadb.common.cluster import manager
@@ -49,17 +51,14 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
     """
 
     cluster_task: asyncio.Task
-    watch_update_queue_task: asyncio.Task
     update_node_data_cache_task: asyncio.Task
-    k8s_watch: kubernetes.watch.Watch
+    k8s_watch: kubernetes_asyncio.watch.Watch
 
     def __init__(self, settings: Settings) -> None:
         super().__init__(settings)
-        self.queue = asyncio.Queue()
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-        self.k8s_watch = kubernetes.watch.Watch()
+        self.k8s_watch = kubernetes_asyncio.watch.Watch()
         self.node_id_cache: dict[str, NodeData] = {}
-        self.exit = False
 
     async def get_node_metadata(self, sts_name: str) -> nodewriter_pb2.NodeMetadata:
         address = f"{sts_name}.node.nucliadb.svc.cluster.local"
@@ -68,8 +67,8 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
         stub = nodewriter_pb2_grpc.NodeWriterStub(channel)
         return await stub.GetMetadata(EmptyQuery())  # type: ignore
 
-    async def get_node_data(self, sts_name: str) -> NodeData:
-        if sts_name not in self.node_id_cache:
+    async def get_node_data(self, sts_name: str, force=False) -> NodeData:
+        if force or sts_name not in self.node_id_cache:
             metadata = await self.get_node_metadata(sts_name)
             self.node_id_cache[sts_name] = NodeData(
                 node_id=metadata.node_id,
@@ -80,12 +79,15 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
 
     async def update_node(self, event):
         ready = event["object"].status.container_statuses is not None
-        for status in event["object"].status.container_statuses or []:
-            if status.name not in ("reader", "writer", "sidecar"):
-                continue
-            if not status.ready:
-                ready = False
-                break
+        if event["type"] == "DELETED":
+            ready = False
+        else:
+            for status in event["object"].status.container_statuses or []:
+                if status.name not in ("reader", "writer", "sidecar"):
+                    continue
+                if not status.ready:
+                    ready = False
+                    break
 
         sts_name = event["object"].metadata.name
         node_data = await self.get_node_data(sts_name)
@@ -94,7 +96,7 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
             logger.debug(f"Update node {sts_name}: {node_data.node_id}")
             node = manager.get_index_node(node_data.node_id)
             if node is None:
-                logger.debug(f"Adding node", extra={"node_id": node_data.node_id})
+                logger.info(f"Adding node", extra={"node_id": node_data.node_id})
                 manager.add_index_node(
                     IndexNode(
                         id=node_data.node_id,
@@ -106,49 +108,38 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
                 node.address = address
                 node.shard_count = node_data.shard_count
         else:
-            logger.debug(f"Remove node", extra={"node_id": node_data.node_id})
+            logger.info(f"Remove node", extra={"node_id": node_data.node_id})
             node = manager.get_index_node(node_data.node_id)
             if node is not None:
                 manager.remove_index_node(node_data.node_id)
 
-    async def watch_update_queue(self) -> None:
+    async def watch_k8s_for_updates(self) -> None:
+        if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/token"):
+            await kubernetes_asyncio.config.load_incluster_config()
+        else:
+            await kubernetes_asyncio.config.load_kube_config()
+        v1 = kubernetes_asyncio.client.CoreV1Api()
+
         while True:
             try:
-                event = await self.queue.get()
-                await self.update_node(event)
+                async for event in self.k8s_watch.stream(
+                    v1.list_namespaced_pod,
+                    namespace="nucliadb",
+                    label_selector="app.kubernetes.io/instance=node",
+                    timeout_seconds=30,
+                ):
+                    await self.update_node(event)
             except asyncio.CancelledError:  # pragma: no cover
                 return
             except Exception:  # pragma: no cover
-                logger.exception("Error while processing cluster event.")
-            finally:  # pragma: no cover
-                try:
-                    self.queue.task_done()
-                except ValueError:
-                    ...
-
-    def _initialize_cluster(self) -> None:
-        if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/token"):
-            kubernetes.config.load_incluster_config()
-        else:
-            kubernetes.config.load_kube_config()
-        v1 = kubernetes.client.CoreV1Api()
-
-        while not self.exit:
-            for event in self.k8s_watch.stream(
-                v1.list_namespaced_pod,
-                namespace="nucliadb",
-                label_selector="app.kubernetes.io/instance=node",
-                timeout_seconds=5,
-            ):
-                self.queue.put_nowait(event)
+                logger.exception("Error while watching kubernetes.")
 
     async def update_node_data_cache(self) -> None:
         while True:
             await asyncio.sleep(60)
             try:
                 for sts_name in list(self.node_id_cache.keys()):
-                    node_data = await self.get_node_data(sts_name)
-                    self.node_id_cache[sts_name] = node_data
+                    node_data = await self.get_node_data(sts_name, force=True)
                     node = manager.get_index_node(node_data.node_id)
                     if node is not None:
                         node.shard_count = node_data.shard_count
@@ -158,17 +149,13 @@ class KubernetesDiscovery(AbstractClusterDiscovery):
                 logger.exception("Error while updating shard info.")
 
     async def initialize(self) -> None:
-        self.cluster_task = asyncio.get_event_loop().run_in_executor(
-            self.executor, self._initialize_cluster
-        )
-        self.watch_update_queue_task = asyncio.create_task(self.watch_update_queue())
+        self.cluster_task = asyncio.create_task(self.watch_k8s_for_updates())
         self.update_node_data_cache_task = asyncio.create_task(
             self.update_node_data_cache()
         )
 
     async def finalize(self) -> None:
-        self.exit = True
         self.k8s_watch.stop()
+        self.k8s_watch.close()
         self.cluster_task.cancel()
-        self.watch_update_queue_task.cancel()
         self.update_node_data_cache_task.cancel()
