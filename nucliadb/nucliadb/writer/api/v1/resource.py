0cd9593d66ae288864b00b7b6b15067f899642c1
Ferran Llamas <llamas.arroniz@gmail.com>
2023-08-09 10:39:00 +0200

    Add slug on conflict error message

--- nucliadb/nucliadb/writer/api/v1/resource.py
+++ nucliadb/nucliadb/writer/api/v1/resource.py
@@ -126,7 +126,9 @@ async def create_resource(
 
     if item.slug:
         if await resource_slug_exists(kbid, item.slug):
-            raise HTTPException(status_code=409, detail="Resource slug already exists")
+            raise HTTPException(
+                status_code=409, detail=f"Resource slug {item.slug} already exists"
+            )
         writer.slug = item.slug
         toprocess.slug = item.slug
 
