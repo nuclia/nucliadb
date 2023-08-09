eb30db76dfc189533f09b2b96fce6033a2309c7e
Tarek Ziade <tarek@ziade.org>
2023-08-09 15:42:28 +0200

    Adding coverage to `nucliadb_node_binding` (#1173)
    * reorganized the test in the standard layout [sc-6677]
    
    * make the new action run manually for now
    
    * reactivate lints
    
    * fix the name

---
+++ nucliadb_node_binding/cov.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+cargo llvm-cov show-env --export-prefix >.env
+cargo llvm-cov clean --workspace
+rm -rf ../target/wheels/nucliadb_node_binding*
+source .env
+
+maturin build
+pip install ../target/wheels/nucliadb_node_binding*.whl
+
+pytest -rfE --cov=nucliadb_node_binding --cov-config=../.coveragerc -s --tb=native -v --cov-report term-missing:skip-covered --cov-report xml tests
+
+# generating coverage for the CI
+cargo llvm-cov report --ignore-filename-regex="^(.*/nucliadb_vectors/).*|^(.*/nucliadb_texts/).*|^(.*/nucliadb_telemetry/).*|^(.*/nucliadb_relations/).*|^(.*/nucliadb_core/).*|^(.*/nucliadb_node/).*|^(.*/nucliadb_paragraphs/).*|^(.*/nucliadb_protos/)|^(.*/nucliadb/nucliadb/)" --lcov --output-path coverage.lcov
+
+# coverage for the console
+cargo llvm-cov report --ignore-filename-regex="^(.*/nucliadb_vectors/).*|^(.*/nucliadb_texts/).*|^(.*/nucliadb_telemetry/).*|^(.*/nucliadb_relations/).*|^(.*/nucliadb_core/).*|^(.*/nucliadb_node/).*|^(.*/nucliadb_paragraphs/).*|^(.*/nucliadb_protos/)|^(.*/nucliadb/nucliadb/)"
