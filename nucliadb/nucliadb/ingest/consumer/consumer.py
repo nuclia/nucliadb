f6b63bc6c252e9473ca59891af4557b18eb2513d
vangheem <vangheem@gmail.com>
2023-08-29 08:43:08 -0400

    explicit nak on error in ingest

--- nucliadb/nucliadb/ingest/consumer/consumer.py
+++ nucliadb/nucliadb/ingest/consumer/consumer.py
@@ -206,6 +206,7 @@ class IngestConsumer:
                     f"Check sentry for more details: {str(e)}"
                 )
                 await asyncio.sleep(2)
+                await msg.nack()
                 raise e
             else:
                 # Successful processing
