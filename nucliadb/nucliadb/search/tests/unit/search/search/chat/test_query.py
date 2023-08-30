af64163a67b8d8d84ca37668fc711ceaf3a15e82
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-08-30 18:23:13 +0200

    Fix test

--- nucliadb/nucliadb/search/tests/unit/search/search/chat/test_query.py
+++ nucliadb/nucliadb/search/tests/unit/search/search/chat/test_query.py
@@ -67,13 +67,13 @@ async def test_chat_does_not_call_predict_if_no_find_results(
 async def test_async_gen_lookahead():
     async def gen(n):
         for i in range(n):
-            yield i
+            yield f"{i}".encode()
 
     assert [item async for item in async_gen_lookahead(gen(0))] == []
-    assert [item async for item in async_gen_lookahead(gen(1))] == [(0, True)]
+    assert [item async for item in async_gen_lookahead(gen(1))] == [(b"0", True)]
     assert [item async for item in async_gen_lookahead(gen(2))] == [
-        (0, False),
-        (1, True),
+        (b"0", False),
+        (b"1", True),
     ]
 
 
