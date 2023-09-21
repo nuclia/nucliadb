8a76b9aabdc0ccf016597b3fa41672bb8fc5cb4b
Nathan Van Gheem <vangheem@gmail.com>
2023-09-21 08:40:22 -0500

    add vm e2e test (#1358)
    * add vm e2e test
    
    * exe
    
    * another
    
    * tweak
    
    * try
    
    * try
    
    * try
    
    * tweak
    
    * tweak address
    
    * add host key path config
    
    * fix
    
    * tweak
    
    * try
    
    * blah
    
    * .
    
    * regular node key
    
    * fix
    
    * rev
    
    * pod ip
    i

--- nucliadb/nucliadb/common/cluster/standalone/index_node.py
+++ nucliadb/nucliadb/common/cluster/standalone/index_node.py
@@ -98,9 +98,11 @@ class ProxyCallerWrapper:
         self._address = address
         self._type = type
         self._original_type = original_type
-        self._channel = get_traced_grpc_channel(
-            f"{address}:{cluster_settings.standalone_node_port}", "standalone_proxy"
-        )
+        if ":" not in address:
+            grpc_address = f"{address}:{cluster_settings.standalone_node_port}"
+        else:
+            grpc_address = address
+        self._channel = get_traced_grpc_channel(grpc_address, "standalone_proxy")
         self._stub = standalone_pb2_grpc.StandaloneClusterServiceStub(self._channel)
 
     def __getattr__(self, name):
