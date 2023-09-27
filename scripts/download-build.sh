4ca9630573fc44882db9876375a1f4641ec11c5c
vangheem <vangheem@gmail.com>
2023-09-26 21:24:49 -0400

    maturin build

--- scripts/download-build.sh
+++ scripts/download-build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-BUILD_SERVER_URL="https://ci.stashify.tech/rust-build-server"
-# BUILD_SERVER_URL="http://localhost:8000"
+# BUILD_SERVER_URL="https://ci.stashify.tech/rust-build-server"
+BUILD_SERVER_URL="http://localhost:8000"
 
 if [ -z "$BRANCH" ]; then
     echo "BRANCH is not set"
@@ -19,6 +19,7 @@ if [ -z "$secret_key" ]; then
 fi
 
 TEST=${TEST:-false}
+MATURIN=${MATURIN:-false}
 
 echo "Building: $BRANCH $COMMIT_HASH -- test: $TEST"
 
@@ -27,7 +28,7 @@ json_data=$(curl -f "$BUILD_SERVER_URL/build" \
     --retry 3 \
     -H "X-Secret-Key:$secret_key" \
     -H 'content-type: application/json' \
-    --data "{\"git_url\": \"https://github.com/nuclia/nucliadb.git\",\"branch\": \"$BRANCH\",\"commit_hash\": \"$COMMIT_HASH\",\"release\": false, \"test\": $TEST}")
+    --data "{\"git_url\": \"https://github.com/nuclia/nucliadb.git\",\"branch\": \"$BRANCH\",\"commit_hash\": \"$COMMIT_HASH\",\"release\": false, \"test\": $TEST, \"maturin\": $MATURIN, \"cargo_file\": \"nucliadb_node_binding/Cargo.toml\"}")
 
 echo "JSON data:"
 echo $json_data
@@ -35,9 +36,19 @@ echo $json_data
 # Parse JSON and download files
 binaries=$(echo "$json_data" | jq -r '.binaries[]')
 test_binaries=$(echo "$json_data" | jq -r '.test_binaries[]')
+wheels=$(echo "$json_data" | jq -r '.wheels[]')
 base_url="$BUILD_SERVER_URL/download-binary/$COMMIT_HASH"
 
 mkdir -p http-builds/tests
+mkdir -p http-builds/wheels
+
+if [ "$MATURIN" = "true" ]; then
+    for wheel in $wheels; do
+        url="${base_url}/${wheel}"
+        echo "Downloading $wheel from $url"
+        curl -f --retry 1 -o "http-builds/wheels/$wheel" "$url" -H "X-Secret-Key:$secret_key"
+    done
+fi
 
 for binary in $binaries; do
     url="${base_url}/${binary}"
