b072ac330cdacd5e48bfe620f33c5b33a7704f3b
vangheem <vangheem@gmail.com>
2023-09-27 12:50:56 -0400

    tweaks

--- scripts/download-build.sh
+++ scripts/download-build.sh
@@ -13,12 +13,13 @@ if [ -z "$COMMIT_HASH" ]; then
     echo "COMMIT_HASH is not set"
     exit 1
 fi
-if [ -z "$secret_key" ]; then
-    echo "secret_key is not set"
+if [ -z "$SECRET_KEY" ]; then
+    echo "SECRET_KEY is not set"
     exit 1
 fi
 
 TEST=${TEST:-false}
+RELEASE=${RELEASE:-false}
 MATURIN=${MATURIN:-false}
 
 echo "Building: $BRANCH $COMMIT_HASH -- test: $TEST"
@@ -26,9 +27,9 @@ echo "Building: $BRANCH $COMMIT_HASH -- test: $TEST"
 # JSON data
 json_data=$(curl -f "$BUILD_SERVER_URL/build" \
     --retry 3 \
-    -H "X-Secret-Key:$secret_key" \
+    -H "X-Secret-Key:$SECRET_KEY" \
     -H 'content-type: application/json' \
-    --data "{\"git_url\": \"https://github.com/nuclia/nucliadb.git\",\"branch\": \"$BRANCH\",\"commit_hash\": \"$COMMIT_HASH\",\"release\": false, \"test\": $TEST, \"maturin\": $MATURIN, \"cargo_file\": \"nucliadb_node_binding/Cargo.toml\"}")
+    --data "{\"git_url\": \"https://github.com/nuclia/nucliadb.git\",\"branch\": \"$BRANCH\",\"commit_hash\": \"$COMMIT_HASH\",\"release\": $RELEASE, \"test\": $TEST, \"maturin\": $MATURIN, \"cargo_file\": \"nucliadb_node_binding/Cargo.toml\"}")
 
 echo "JSON data:"
 echo $json_data
@@ -39,27 +40,27 @@ test_binaries=$(echo "$json_data" | jq -r '.test_binaries[]')
 wheels=$(echo "$json_data" | jq -r '.wheels[]')
 base_url="$BUILD_SERVER_URL/download-binary/$COMMIT_HASH"
 
-mkdir -p http-builds/tests
-mkdir -p http-builds/wheels
+mkdir -p builds/tests
+mkdir -p builds/wheels
 
 if [ "$MATURIN" = "true" ]; then
     for wheel in $wheels; do
         url="${base_url}/${wheel}"
         echo "Downloading $wheel from $url"
-        curl -f --retry 1 -o "http-builds/wheels/$wheel" "$url" -H "X-Secret-Key:$secret_key"
+        curl -f --retry 1 -o "builds/wheels/$wheel" "$url" -H "X-Secret-Key:$SECRET_KEY"
     done
 fi
 
 for binary in $binaries; do
     url="${base_url}/${binary}"
     echo "Downloading $binary from $url"
-    curl -f --retry 1 -o "http-builds/$binary" "$url" -H "X-Secret-Key:$secret_key"
+    curl -f --retry 1 -o "builds/$binary" "$url" -H "X-Secret-Key:$SECRET_KEY"
 done
 
 if [ "$TEST" = "true" ]; then
     for binary in $test_binaries; do
         url="${base_url}/${binary}"
         echo "Downloading $binary from $url"
-        curl -f --retry 1 -o "http-builds/tests/$binary" "$url" -H "X-Secret-Key:$secret_key"
+        curl -f --retry 1 -o "builds/tests/$binary" "$url" -H "X-Secret-Key:$SECRET_KEY"
     done
 fi
