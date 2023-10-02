3abef958f4cf77af85a0bbff2fd9dbafb00b95a0
Nathan Van Gheem <vangheem@gmail.com>
2023-10-02 10:58:56 -0500

    enable forking standalone process (#1282)
    * enable forking standalone process
    
    * naming
    
    * fix default
    
    * hmmm
    
    * fix

--- nucliadb/nucliadb/standalone/settings.py
+++ nucliadb/nucliadb/standalone/settings.py
@@ -111,3 +111,5 @@ Examples:
     )
 
     cluster_discovery_mode: StandaloneDiscoveryMode = StandaloneDiscoveryMode.DEFAULT
+
+    fork: bool = pydantic.Field(default=False, description="Fork process on startup")
