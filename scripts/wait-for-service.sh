dc7dcd0737a45a051187aba9c93e5e3444c9e5ec
Tarek Ziade <tarek@ziade.org>
2023-10-04 11:49:57 +0200

    Retry tikv install 3 times
    this step has been failing intermittently

--- scripts/wait-for-service.sh
+++ scripts/wait-for-service.sh
@@ -5,20 +5,20 @@ COMMAND="nc -zv 127.0.0.1 $1"
 
 start_time=$(date +%s)
 while true; do
-    $COMMAND
-    exit_code=$?
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
+	$COMMAND
+	exit_code=$?
+	current_time=$(date +%s)
+	elapsed_time=$((current_time - start_time))
 
-    if [ $exit_code -eq 0 ]; then
-        echo "Running on 127.0.0.1 $1."
-        break
-    elif [ $elapsed_time -ge $MAX_TIMEOUT ]; then
-        echo "Not runing within the maximum timeout of $MAX_TIMEOUT seconds."
-        exit 1
-        break
-    else
-        echo "Waiting to come up..."
-        sleep 1  # You can adjust the sleep time as needed
-    fi
+	if [ $exit_code -eq 0 ]; then
+		echo "Running on 127.0.0.1 $1."
+		break
+	elif [ $elapsed_time -ge $MAX_TIMEOUT ]; then
+		echo "Not runing within the maximum timeout of $MAX_TIMEOUT seconds."
+		exit 1
+		break
+	else
+		echo "Waiting to come up..."
+		sleep 1 # You can adjust the sleep time as needed
+	fi
 done
