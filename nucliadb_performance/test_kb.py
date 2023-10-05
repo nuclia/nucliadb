b0237a9e35ef25bc263324f833f79e3e898230c9
Ferran Llamas <llamas.arroniz@gmail.com>
2023-10-05 12:34:44 +0200

    wip

--- nucliadb_performance/test_kb.py
+++ nucliadb_performance/test_kb.py
@@ -1,3 +1,4 @@
+from email.mime import base
 from molotov import scenario
 import nucliadb_sdk
 from faker import Faker
@@ -5,10 +6,11 @@ from faker import Faker
 fake = Faker()
 REGION = "europe-1"
 ENV = "stashify.cloud"
-URL = None
+LOCAL_NDB = True
+URL = "http://localhost:8080/api/v1"
 
 def get_base_url():
-    if URL is not None:
+    if LOCAL_NDB and URL:
         return URL
     return f"https://{REGION}.{ENV}/api/v1"
 
@@ -17,11 +19,22 @@ def get_url(path):
     return get_base_url() + path
 
 
-kbid = "13c0f7ee-cd27-4739-a41b-898653b5885b"
+kbid = "b27f6902-c2b8-4399-8310-528eacd9642f"
+
+
+class Client:
+    def __init__(self, session, base_url):
+        self.session = session
+        self.base_url = base_url
+
+    async def make_request(self, method: str, path: str, *args, **kwargs):
+        url = self.base_url + path
+        func = getattr(self.session, method.lower())
+        async with func(url, *args, **kwargs) as resp:
+            assert resp.status == 200, resp.status
 
 
 @scenario(weight=50)
 async def test_find_default(session):
-    find = get_url(f"/kb/{kbid}/find?query={fake.sentence()}")
-    async with session.get(find) as resp:
-        assert resp.status == 200, resp.status
+    client = Client(session, get_base_url())
+    await client.make_request(f"/kb/{kbid}/find", params={"query": fake.sentence()})
