d8dffc6a72e557fefb05a6c6f2d562119deac37f
Ferran Llamas <llamas.arroniz@gmail.com>
2023-10-17 13:47:00 +0200

    Fix more memory leaks on search

--- nucliadb/nucliadb/search/search/merge.py
+++ nucliadb/nucliadb/search/search/merge.py
@@ -201,51 +201,57 @@ async def merge_suggest_paragraph_results(
     if len(suggest_responses) > 1:
         sort_results_by_score(raw_paragraph_list)
 
-    result_paragraph_list: List[Paragraph] = []
+    rcache = get_resource_cache(clear=True)
     etcache = ExtractedTextCache()
-    for result in raw_paragraph_list[:10]:
-        _, field_type, field = result.field.split("/")
-        text = await get_paragraph_text(
-            kbid=kbid,
-            rid=result.uuid,
-            field=result.field,
-            start=result.start,
-            end=result.end,
-            split=result.split,
-            highlight=highlight,
-            ematches=ematches,  # type: ignore
-            matches=result.matches,  # type: ignore
-            extracted_text_cache=etcache,
-        )
-        labels = await get_labels_paragraph(result, kbid)
-        new_paragraph = Paragraph(
-            score=result.score.bm25,
-            rid=result.uuid,
-            field_type=field_type,
-            field=field,
-            text=text,
-            labels=labels,
-            position=TextPosition(
-                index=result.metadata.position.index,
-                start=result.metadata.position.start,
-                end=result.metadata.position.end,
-                page_number=result.metadata.position.page_number,
-            ),
-        )
-        if len(result.metadata.position.start_seconds) or len(
-            result.metadata.position.end_seconds
-        ):
-            new_paragraph.start_seconds = list(result.metadata.position.start_seconds)
-            new_paragraph.end_seconds = list(result.metadata.position.end_seconds)
-        else:
-            # TODO: Remove once we are sure all data has been migrated!
-            seconds_positions = await get_seconds_paragraph(result, kbid)
-            if seconds_positions is not None:
-                new_paragraph.start_seconds = seconds_positions[0]
-                new_paragraph.end_seconds = seconds_positions[1]
-        result_paragraph_list.append(new_paragraph)
-    etcache.clear()
-    return Paragraphs(results=result_paragraph_list, query=query)
+    try:
+        result_paragraph_list: List[Paragraph] = []
+        for result in raw_paragraph_list[:10]:
+            _, field_type, field = result.field.split("/")
+            text = await get_paragraph_text(
+                kbid=kbid,
+                rid=result.uuid,
+                field=result.field,
+                start=result.start,
+                end=result.end,
+                split=result.split,
+                highlight=highlight,
+                ematches=ematches,  # type: ignore
+                matches=result.matches,  # type: ignore
+                extracted_text_cache=etcache,
+            )
+            labels = await get_labels_paragraph(result, kbid)
+            new_paragraph = Paragraph(
+                score=result.score.bm25,
+                rid=result.uuid,
+                field_type=field_type,
+                field=field,
+                text=text,
+                labels=labels,
+                position=TextPosition(
+                    index=result.metadata.position.index,
+                    start=result.metadata.position.start,
+                    end=result.metadata.position.end,
+                    page_number=result.metadata.position.page_number,
+                ),
+            )
+            if len(result.metadata.position.start_seconds) or len(
+                result.metadata.position.end_seconds
+            ):
+                new_paragraph.start_seconds = list(
+                    result.metadata.position.start_seconds
+                )
+                new_paragraph.end_seconds = list(result.metadata.position.end_seconds)
+            else:
+                # TODO: Remove once we are sure all data has been migrated!
+                seconds_positions = await get_seconds_paragraph(result, kbid)
+                if seconds_positions is not None:
+                    new_paragraph.start_seconds = seconds_positions[0]
+                    new_paragraph.end_seconds = seconds_positions[1]
+            result_paragraph_list.append(new_paragraph)
+        return Paragraphs(results=result_paragraph_list, query=query)
+    finally:
+        etcache.clear()
+        rcache.clear()
 
 
 async def merge_vectors_results(
@@ -374,60 +380,64 @@ async def merge_paragraph_results(
 
     result_paragraph_list: List[Paragraph] = []
     etcache = ExtractedTextCache()
-    for result, _ in raw_paragraph_list[min(skip, length) : min(end, length)]:
-        _, field_type, field = result.field.split("/")
-        text = await get_paragraph_text(
-            kbid=kbid,
-            rid=result.uuid,
-            field=result.field,
-            start=result.start,
-            end=result.end,
-            split=result.split,
-            highlight=highlight,
-            ematches=ematches,
-            matches=result.matches,  # type: ignore
-            extracted_text_cache=etcache,
-        )
-        labels = await get_labels_paragraph(result, kbid)
-        new_paragraph = Paragraph(
-            score=result.score.bm25,
-            rid=result.uuid,
-            field_type=field_type,
-            field=field,
-            text=text,
-            labels=labels,
-            position=TextPosition(
-                index=result.metadata.position.index,
-                start=result.metadata.position.start,
-                end=result.metadata.position.end,
-                page_number=result.metadata.position.page_number,
-            ),
-        )
-        if len(result.metadata.position.start_seconds) or len(
-            result.metadata.position.end_seconds
-        ):
-            new_paragraph.start_seconds = list(result.metadata.position.start_seconds)
-            new_paragraph.end_seconds = list(result.metadata.position.end_seconds)
-        else:
-            # TODO: Remove once we are sure all data has been migrated!
-            seconds_positions = await get_seconds_paragraph(result, kbid)
-            if seconds_positions is not None:
-                new_paragraph.start_seconds = seconds_positions[0]
-                new_paragraph.end_seconds = seconds_positions[1]
+    try:
+        for result, _ in raw_paragraph_list[min(skip, length) : min(end, length)]:
+            _, field_type, field = result.field.split("/")
+            text = await get_paragraph_text(
+                kbid=kbid,
+                rid=result.uuid,
+                field=result.field,
+                start=result.start,
+                end=result.end,
+                split=result.split,
+                highlight=highlight,
+                ematches=ematches,
+                matches=result.matches,  # type: ignore
+                extracted_text_cache=etcache,
+            )
+            labels = await get_labels_paragraph(result, kbid)
+            new_paragraph = Paragraph(
+                score=result.score.bm25,
+                rid=result.uuid,
+                field_type=field_type,
+                field=field,
+                text=text,
+                labels=labels,
+                position=TextPosition(
+                    index=result.metadata.position.index,
+                    start=result.metadata.position.start,
+                    end=result.metadata.position.end,
+                    page_number=result.metadata.position.page_number,
+                ),
+            )
+            if len(result.metadata.position.start_seconds) or len(
+                result.metadata.position.end_seconds
+            ):
+                new_paragraph.start_seconds = list(
+                    result.metadata.position.start_seconds
+                )
+                new_paragraph.end_seconds = list(result.metadata.position.end_seconds)
+            else:
+                # TODO: Remove once we are sure all data has been migrated!
+                seconds_positions = await get_seconds_paragraph(result, kbid)
+                if seconds_positions is not None:
+                    new_paragraph.start_seconds = seconds_positions[0]
+                    new_paragraph.end_seconds = seconds_positions[1]
 
-        result_paragraph_list.append(new_paragraph)
-        if new_paragraph.rid not in resources:
-            resources.append(new_paragraph.rid)
-    etcache.clear()
-    return Paragraphs(
-        results=result_paragraph_list,
-        facets=facets,
-        query=query,
-        total=total,
-        page_number=page,
-        page_size=count,
-        next_page=next_page,
-    )
+            result_paragraph_list.append(new_paragraph)
+            if new_paragraph.rid not in resources:
+                resources.append(new_paragraph.rid)
+        return Paragraphs(
+            results=result_paragraph_list,
+            facets=facets,
+            query=query,
+            total=total,
+            page_number=page,
+            page_size=count,
+            next_page=next_page,
+        )
+    finally:
+        etcache.clear()
 
 
 @merge_observer.wrap({"type": "merge_relations"})
@@ -507,7 +517,6 @@ async def merge_results(
     api_results = KnowledgeboxSearchResults()
 
     rcache = get_resource_cache(clear=True)
-
     try:
         resources: List[str] = list()
         api_results.fulltext = await merge_documents_results(
