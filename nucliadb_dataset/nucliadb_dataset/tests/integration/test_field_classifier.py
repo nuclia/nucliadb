5fc34f987425654940baddc538451232520f8def
Joan Antoni RE <joanantoni.re16@gmail.com>
2023-10-20 12:02:19 +0200

    Explicit test for dataset exporters

--- nucliadb_dataset/nucliadb_dataset/tests/integration/test_field_classifier.py
+++ nucliadb_dataset/nucliadb_dataset/tests/integration/test_field_classifier.py
@@ -17,75 +17,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import re
 import tempfile
-from uuid import uuid4
 
 import pyarrow as pa  # type: ignore
 from nucliadb_protos.dataset_pb2 import TaskType, TrainSet
 
 from nucliadb_dataset.dataset import NucliaDBDataset, download_all_partitions
-from nucliadb_dataset.export import FileSystemExport, NucliaDatasetsExport
 from nucliadb_sdk.knowledgebox import KnowledgeBox
-
-
-def test_filesystem(knowledgebox: KnowledgeBox, upload_data_field_classification):
-    trainset = TrainSet()
-    trainset.type = TaskType.FIELD_CLASSIFICATION
-    trainset.batch_size = 2
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        fse = FileSystemExport(
-            knowledgebox.client,
-            trainset=trainset,
-            store_path=tmpdirname,
-        )
-        fse.export()
-        files = os.listdir(tmpdirname)
-        for filename in files:
-            with pa.memory_map(f"{tmpdirname}/{filename}", "rb") as source:
-                loaded_array = pa.ipc.open_stream(source).read_all()
-                # We multiply by two due to auto-generated title field
-                assert len(loaded_array) == 2 * 2
-
-
-def run_dataset_export(requests_mock, knowledgebox, trainset):
-    mock_dataset_id = str(uuid4())
-    requests_mock.real_http = True
-    requests_mock.register_uri(
-        "POST",
-        "http://datasets.service/datasets",
-        status_code=201,
-        json={"id": mock_dataset_id},
-    )
-    requests_mock.register_uri(
-        "PUT",
-        re.compile(rf"^http://datasets.service/dataset/{mock_dataset_id}/partition/.*"),
-        status_code=204,
-        content=b"",
-    )
-
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        fse = NucliaDatasetsExport(
-            client=knowledgebox.client,
-            datasets_url="http://datasets.service",
-            trainset=trainset,
-            cache_path=tmpdirname,
-            apikey="i_am_a_nuakey",
-        )
-        fse.export()
-
-
-def test_nucliadb_export_fields(
-    knowledgebox: KnowledgeBox, upload_data_field_classification, requests_mock
-):
-    trainset = TrainSet()
-    trainset.type = TaskType.FIELD_CLASSIFICATION
-    trainset.filter.labels.append("labelset1")
-    trainset.batch_size = 2
-
-    run_dataset_export(requests_mock, knowledgebox, trainset)
 
 
 def test_live_field_classification(
