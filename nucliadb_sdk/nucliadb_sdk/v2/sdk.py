da2489c67a6b8ccb5b24bad649a886ac25be89c5
Nathan Van Gheem <vangheem@gmail.com>
2023-11-07 11:44:50 -0500

    Revert "changing config needs a PATCH not a POST (#1556)"
    This reverts commit ef3922fac35324635f995f6d575542ddd4fa4c28.

--- nucliadb_sdk/nucliadb_sdk/v2/sdk.py
+++ nucliadb_sdk/nucliadb_sdk/v2/sdk.py
@@ -468,7 +468,7 @@ class _NucliaDBBase:
     set_configuration = _request_builder(
         name="set_configuration",
         path_template="/v1/kb/{kbid}/configuration",
-        method="PATCH",
+        method="POST",
         path_params=("kbid",),
         request_type=KBConfiguration,
         response_type=None,
