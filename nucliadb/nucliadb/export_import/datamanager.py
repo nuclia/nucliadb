2b3e7fc5478327cc97188b31bc5b5709e48e1c24
Ferran Llamas <llamas.arroniz@gmail.com>
2023-11-10 11:45:46 +0100

    Give progress for export downloads too

--- nucliadb/nucliadb/export_import/datamanager.py
+++ nucliadb/nucliadb/export_import/datamanager.py
@@ -88,7 +88,7 @@ class ExportImportDataManager:
         export_bytes: AsyncGenerator[bytes, None],
         kbid: str,
         export_id: str,
-    ):
+    ) -> int:
         key = STORAGE_EXPORT_KEY.format(export_id=export_id)
         cf = resources_pb2.CloudFile()
         cf.bucket_name = self.storage.get_bucket_name(kbid)
@@ -97,6 +97,7 @@ class ExportImportDataManager:
         field: StorageField = self._get_storage_field(kbid, key, cf)
         iterator = iterate_storage_compatible(export_bytes, self.storage, cf)
         await self.storage.uploaditerator(iterator, field, cf)
+        return cf.size
 
     async def download_export(
         self, kbid: str, export_id: str
