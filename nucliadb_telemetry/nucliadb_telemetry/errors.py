c56b6a9f32d3267605cbc0a3854e8981422c71cd
Carles Bruguera Artero <carlesba@gmail.com>
2023-11-15 14:44:08 +0100

    Set zone on sentry as tag (#1527)
    * Set zone on sentry as tag
    remove europe-1 on chart values
    
    * Revert default value

--- nucliadb_telemetry/nucliadb_telemetry/errors.py
+++ nucliadb_telemetry/nucliadb_telemetry/errors.py
@@ -76,6 +76,7 @@ def push_scope(**kwargs: Any) -> ContextManager[Scope]:
 
 
 class ErrorHandlingSettings(BaseSettings):
+    zone: str = pydantic.Field(default="local", env=["NUCLIA_ZONE"])
     sentry_url: Optional[str] = None
     environment: str = pydantic.Field(
         default="local", env=["environment", "running_environment"]
@@ -99,3 +100,4 @@ def setup_error_handling(version: str) -> None:
             integrations=[],
             default_integrations=False,
         )
+        sentry_sdk.set_tag("zone", settings.zone)
