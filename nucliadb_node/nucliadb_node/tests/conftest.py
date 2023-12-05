5ab741848067f022314447b7e262932d29b117b2
Ferran Llamas <llamas.arroniz@gmail.com>
2023-12-05 12:43:46 +0100

    add plugin

--- nucliadb_node/nucliadb_node/tests/conftest.py
+++ nucliadb_node/nucliadb_node/tests/conftest.py
@@ -20,6 +20,7 @@
 pytest_plugins = [
     "pytest_docker_fixtures",
     "nucliadb_utils.tests.nats",
+    "nucliadb_utils.tests.conftest",
     "nucliadb_utils.tests.gcs",
     "nucliadb_utils.tests.s3",
     "nucliadb_utils.tests.indexing",
