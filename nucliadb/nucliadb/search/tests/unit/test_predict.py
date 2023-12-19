590abe70cb0d60a0e2318687c79ad7572fd01204
Ferran Llamas <llamas.arroniz@gmail.com>
2023-12-19 11:20:17 +0100

    Add chat extra context option to chat endpoint

--- nucliadb/nucliadb/search/tests/unit/test_predict.py
+++ nucliadb/nucliadb/search/tests/unit/test_predict.py
@@ -355,7 +355,9 @@ async def test_rephrase():
         "POST", 200, json="rephrased", context_manager=False
     )
 
-    item = RephraseModel(question="question", chat_history=[], user_id="foo")
+    item = RephraseModel(
+        question="question", chat_history=[], user_id="foo", user_context=["foo"]
+    )
     rephrased_query = await pe.rephrase_query("kbid", item)
     # The rephrase query should not be wrapped in quotes, otherwise it will trigger an exact match query to the index
     assert rephrased_query.strip('"') == rephrased_query
