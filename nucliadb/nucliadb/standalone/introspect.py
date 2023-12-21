e1f079ba8b0c4a77d43a6d3da4377f5b497fe43f
Ferran Llamas <llamas.arroniz@gmail.com>
2023-12-21 10:40:58 +0100

    Add introspection endpoint

---
+++ nucliadb/nucliadb/standalone/introspect.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+from collections.abc import AsyncGenerator
+
+import pkg_resources
+from fastapi import FastAPI
+
+from nucliadb.standalone.settings import Settings
+from nucliadb_telemetry.settings import LogSettings
+
+MB = 1024 * 1024
+CHUNK_SIZE = 2 * MB
+
+
+async def stream_tar(app: FastAPI) -> AsyncGenerator[bytes, None]:
+    import tarfile
+    import tempfile
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # Create tar file
+        tar_file = os.path.join(tmpdirname, "introspect.tar.gz")
+        with tarfile.open(tar_file, mode="w:gz") as tar:
+            # Add pip dependencies
+            with open(os.path.join(tmpdirname, "dependencies.txt"), "w") as f:
+                installed_packages = [pkg for pkg in pkg_resources.working_set]
+                lines = []
+                for pkg in sorted(installed_packages, key=lambda p: p.key):
+                    lines.append(f"{pkg.key}=={pkg.version}\n")
+                f.writelines(lines)
+            tar.add(
+                os.path.join(tmpdirname, "dependencies.txt"), arcname="dependencies.txt"
+            )
+
+            # Add standalone settings
+            if not hasattr(app, "settings"):
+                return
+
+            settings: Settings = app.settings.copy()
+            # Remove sensitive data
+            settings.nua_api_key = None
+            settings.jwk_key = None
+            settings.gcs_base64_creds = None
+            settings.s3_client_secret = None
+            with open(os.path.join(tmpdirname, "settings.json"), "w") as f:
+                f.write(settings.json(indent=4))
+            tar.add(os.path.join(tmpdirname, "settings.json"), arcname="settings.json")
+
+            # Add log files
+            if settings.log_output_type == "file":
+                log_settings = LogSettings()
+                access_log = os.path.realpath(log_settings.access_log)
+                tar.add(access_log, arcname="logs/access.log")
+                error_log = os.path.realpath(log_settings.error_log)
+                tar.add(error_log, arcname="logs/error.log")
+                info_log = os.path.realpath(log_settings.info_log)
+                tar.add(info_log, arcname="logs/info.log")
+
+        # Stream out tar file
+        with open(tar_file, "rb") as f:
+            chunk = f.read(CHUNK_SIZE)
+            while chunk:
+                yield chunk
+                chunk = f.read(CHUNK_SIZE)
---
+++ nucliadb/nucliadb/standalone/tests/integration/test_introspect.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import tarfile
+import tempfile
+
+import pytest
+
+from nucliadb.standalone.settings import Settings
+
+
+@pytest.mark.asyncio
+async def test_labelsets_service(nucliadb_manager) -> None:
+    # Generate some traffic to have some logs
+    await nucliadb_manager.post("/not/found")
+    await nucliadb_manager.delete("/kb/foobar")
+
+    resp = await nucliadb_manager.get("/introspect")
+    assert resp.status_code == 200
+
+    with tempfile.TemporaryDirectory() as root_dir:
+        # Save the tar to a file
+        introspect_tar_file = os.path.join(root_dir, "introspect.tar.gz")
+        with open(introspect_tar_file, "wb") as f:
+            f.write(resp.content)
+
+        # Extract the tar
+        extracted_tar = os.path.join(root_dir, "introspect")
+        with tarfile.open(introspect_tar_file, "r:gz") as tar:
+            tar.extractall(extracted_tar)
+
+        # Check dependencies
+        assert os.path.exists(os.path.join(extracted_tar, "dependencies.txt"))
+        with open(os.path.join(extracted_tar, "dependencies.txt")) as f:
+            dependencies = f.read()
+            assert "nucliadb" in dependencies
+            assert "nucliadb_models" in dependencies
+
+        # Check settings
+        assert os.path.exists(os.path.join(extracted_tar, "settings.json"))
+        introspect_settings = Settings.parse_file(
+            os.path.join(extracted_tar, "settings.json")
+        )
+        assert introspect_settings.nua_api_key is None
+        assert introspect_settings.jwk_key is None
+        assert introspect_settings.gcs_base64_creds is None
+        assert introspect_settings.s3_client_secret is None
+
+        # Check logs
+        assert os.path.exists(os.path.join(extracted_tar, "logs/info.log"))
+        assert os.path.exists(os.path.join(extracted_tar, "logs/error.log"))
+        assert os.path.exists(os.path.join(extracted_tar, "logs/access.log"))
+        assert os.path.getsize(os.path.join(extracted_tar, "logs/access.log")) > 0
