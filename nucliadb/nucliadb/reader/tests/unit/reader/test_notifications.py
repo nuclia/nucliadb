7cdbe7306131d01cb5c6aa937970aa4f9d03bef3
Ferran Llamas <llamas.arroniz@gmail.com>
2024-01-16 22:42:37 +0100

    better tests

--- nucliadb/nucliadb/reader/tests/unit/reader/test_notifications.py
+++ nucliadb/nucliadb/reader/tests/unit/reader/test_notifications.py
@@ -20,25 +20,64 @@
 import asyncio
 from unittest import mock
 
+import pytest
+
 from nucliadb.reader.reader.notifications import kb_notifications_stream
 from nucliadb_protos import writer_pb2
+
+MODULE = "nucliadb.reader.reader.notifications"
+
+
+@pytest.fixture(scope="function", autouse=True)
+def timeout():
+    with mock.patch(f"{MODULE}.NOTIFICATIONS_TIMEOUT_S", 1):
+        yield
 
 
 async def test_kb_notifications_stream_timeout_gracefully():
     event = asyncio.Event()
+    cancelled_event = asyncio.Event()
 
-    async def mocked_long_kb_notifications(kbid):
-        await asyncio.sleep(2)
-        yield writer_pb2.Notification()
-        event.set()
+    async def mocked_kb_notifications(kbid):
+        # Wait longer than the timeout to yield a notification
+        try:
+            await asyncio.sleep(2)
+            yield writer_pb2.Notification()
+            event.set()
+        except asyncio.CancelledError:
+            cancelled_event.set()
 
-    module = "nucliadb.reader.reader.notifications"
-    with mock.patch(f"{module}.NOTIFICATIONS_TIMEOUT_S", 1), mock.patch(
-        f"{module}.kb_notifications", new=mocked_long_kb_notifications
-    ):
-        # Check that the generator returns after NOTIFICATIONS_TIMEOUT_S seconds
+    with mock.patch(f"{MODULE}.kb_notifications", new=mocked_kb_notifications):
 
+        # Check that the generator returns gracefully after NOTIFICATIONS_TIMEOUT_S seconds
         async for _ in kb_notifications_stream("testkb"):
             assert False, "Should not be reached"
 
         assert not event.is_set()
+        assert cancelled_event.is_set()
+
+
+async def test_kb_notifications_stream_timeout_gracefully_while_streaming():
+    cancelled_event = asyncio.Event()
+
+    async def mocked_kb_notifications(kbid):
+        # Only ever yield one notification
+        try:
+            yield writer_pb2.Notification()
+            while True:
+                await asyncio.sleep(0.1)
+        except asyncio.CancelledError:
+            cancelled_event.set()
+
+    with mock.patch(f"{MODULE}.kb_notifications", new=mocked_kb_notifications):
+
+        # Yield a notification first
+        stream = kb_notifications_stream("testkb")
+        assert await stream.__anext__()
+
+        # Since there are no more notifications, the generator will eventually finish due to the timeout
+        with pytest.raises(StopAsyncIteration):
+            assert await stream.__anext__()
+
+        # Check that the kb_notifications generator was cancelled
+        assert cancelled_event.is_set()
