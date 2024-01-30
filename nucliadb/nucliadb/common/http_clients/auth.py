dc5c6b06a94fa71e3fb0bbd6d5503680d690d0b9
Nathan Van Gheem <vangheem@gmail.com>
2024-01-30 09:51:14 -0500

    Fix nua key check for standalone (#1785)

---
+++ nucliadb/nucliadb/common/http_clients/auth.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import logging
+
+import aiohttp
+import pydantic
+
+from nucliadb_utils.settings import nuclia_settings
+
+from .utils import check_status
+
+logger = logging.getLogger(__name__)
+
+
+class AuthInfoUser(pydantic.BaseModel):
+    user_id: str
+    account_id: str
+    account_type: str
+
+
+class AuthInfoResponse(pydantic.BaseModel):
+    auth: str
+    user: AuthInfoUser
+
+
+class NucliaAuthHTTPClient:
+    def __init__(self):
+        self.session = aiohttp.ClientSession()
+        self.base_url = (
+            nuclia_settings.nuclia_public_url.format(zone=nuclia_settings.nuclia_zone)
+            + "/api"
+        )
+        self.headers = {}
+        if nuclia_settings.nuclia_service_account is not None:
+            self.headers[
+                "X-NUCLIA-NUAKEY"
+            ] = f"Bearer {nuclia_settings.nuclia_service_account}"
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *args):
+        await self.close()
+
+    async def close(self):
+        await self.session.close()
+
+    async def status(self) -> AuthInfoResponse:
+        url = self.base_url + "/authorizer/info"
+        async with self.session.get(url, headers=self.headers) as resp:
+            resp_text = await resp.text()
+            check_status(resp, resp_text)
+            return AuthInfoResponse.parse_raw(resp_text)
---
+++ nucliadb/nucliadb/common/http_clients/utils.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import aiohttp
+
+from . import exceptions
+
+
+def check_status(resp: aiohttp.ClientResponse, resp_text: str) -> None:
+    if resp.status < 300:
+        return
+    elif resp.status == 402:
+        raise exceptions.AccountLimitException(f"Account limits exceeded: {resp_text}")
+    elif resp.status == 404:
+        raise exceptions.NotFoundException(f"Resource not found: {resp_text}")
+    elif resp.status in (401, 403):
+        raise exceptions.AuthorizationException(
+            f"Unauthorized to access: {resp.status}"
+        )
+    elif resp.status == 429:
+        raise exceptions.RateLimitException("Rate limited")
+    else:
+        raise exceptions.ClientException(f"Unknown error: {resp.status} - {resp_text}")
