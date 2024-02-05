8b312adfa2c540fd4936d59e3e49c6302124aaad
Ferran Llamas <llamas.arroniz@gmail.com>
2024-02-05 11:39:05 +0100

    removing feature flag for summarization

--- nucliadb_utils/nucliadb_utils/featureflagging.py
+++ nucliadb_utils/nucliadb_utils/featureflagging.py
@@ -51,10 +51,6 @@ DEFAULT_FLAG_DATA: dict[str, Any] = {
         "rollout": 0,
         "variants": {"environment": ["local"]},
     },
-    const.Features.SUMMARIZATION: {
-        "rollout": 0,
-        "variants": {"environment": ["local"]},
-    },
     const.Features.PROCESSING_V2: {
         "rollout": 0,
         "variants": {"environment": ["testing"]},
