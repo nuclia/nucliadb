03fea380e30ed2adcc964f89c578dd4185043dac
Ferran Llamas <llamas.arroniz@gmail.com>
2024-02-13 07:05:04 +0100

    Remove old sdk code

--- nucliadb_sdk/nucliadb_sdk/v2/__init__.py
+++ nucliadb_sdk/nucliadb_sdk/v2/__init__.py
@@ -16,6 +16,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# To have backward compatibility with the previous version of the SDK
+# from nucliadb_sdk.v2 import NucliaDB, Region
+
 from .sdk import NucliaDB, NucliaDBAsync, Region
 
 __all__ = ("NucliaDB", "NucliaDBAsync", "Region")
