e0b7eccd1b59122ffbc9c7e55626e8e1db4519cb
Ferran Llamas <llamas.arroniz@gmail.com>
2024-02-13 07:09:00 +0100

    pin sdk in jupyter notebooks

--- nucliadb_sdk/nucliadb_sdk/v2/__init__.py
+++ nucliadb_sdk/nucliadb_sdk/v2/__init__.py
@@ -17,9 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# To have backward compatibility with the previous version of the SDK
-# from nucliadb_sdk.v2 import NucliaDB, Region
-
 from .sdk import NucliaDB, NucliaDBAsync, Region
 
 __all__ = ("NucliaDB", "NucliaDBAsync", "Region")
