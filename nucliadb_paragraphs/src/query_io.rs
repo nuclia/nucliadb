0c45cca53a62555bf63e0b64f94ebdfd54d9e147
herme garcia <hermegarcianavarro@gmail.com>
2024-02-13 12:06:10 +0100

    paragraphs

---
+++ nucliadb_paragraphs/src/query_io.rs
@@ -0,0 +1,53 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use crate::schema::ParagraphSchema;
+use nucliadb_core::query_language::{BooleanExpression, BooleanOperation, Operator};
+use tantivy::query::{BooleanQuery, Occur, Query, TermQuery};
+use tantivy::schema::{Facet, IndexRecordOption};
+use tantivy::Term;
+
+fn translate_literal(literal: &str, schema: &ParagraphSchema) -> Box<dyn Query> {
+    let facet = Facet::from_text(literal).unwrap();
+    let term = Term::from_facet(schema.facets, &facet);
+    Box::new(TermQuery::new(term, IndexRecordOption::Basic))
+}
+
+fn translate_operation(operation: &BooleanOperation, schema: &ParagraphSchema) -> Box<dyn Query> {
+    let operator = match operation.operator {
+        Operator::Not => Occur::MustNot,
+        Operator::And => Occur::Must,
+        Operator::Or => Occur::Should,
+    };
+
+    let mut operands = Vec::with_capacity(operation.operands.len());
+    for operand in operation.operands.iter() {
+        let subquery = translate_expression(operand, schema);
+        operands.push((operator, subquery));
+    }
+
+    Box::new(BooleanQuery::new(operands))
+}
+
+pub fn translate_expression(expression: &BooleanExpression, schema: &ParagraphSchema) -> Box<dyn Query> {
+    match expression {
+        BooleanExpression::Literal(literal) => translate_literal(literal, schema),
+        BooleanExpression::Operation(operation) => translate_operation(operation, schema),
+    }
+}
