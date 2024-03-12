6cb9a51b2749c539f50115b28143f9012fa70da2
Ferran Llamas <llamas.arroniz@gmail.com>
2024-03-12 12:13:03 +0100

    Add another rollover for speficic kbs

---
+++ nucliadb/migrations/0013_rollover_shards.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+Allow targeted rollover of KBs
+"""
+
+import logging
+import os
+
+from nucliadb.common.cluster.rollover import rollover_kb_shards
+from nucliadb.migrator.context import ExecutionContext
+
+logger = logging.getLogger(__name__)
+
+
+AFFECTED_KBS = [
+    kbid.strip()
+    for kbid in os.environ.get("ROLLOVER_KBS", "").split(",")
+    if kbid.strip()
+]
+
+
+async def migrate(context: ExecutionContext) -> None:
+    ...
+
+
+async def migrate_kb(context: ExecutionContext, kbid: str) -> None:
+    if kbid in AFFECTED_KBS:
+        logger.info(f"Rolling over affected KB: {kbid}")
+        await rollover_kb_shards(context, kbid)
