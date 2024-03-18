3189ea326e69d414ead4c3e02e0b27dc03bf5114
Carles Bruguera <carlesba@gmail.com>
2024-03-18 11:23:53 +0100

    Rollback to accept all methods

--- nucliadb/nucliadb/writer/app.py
+++ nucliadb/nucliadb/writer/app.py
@@ -41,7 +41,7 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=http_settings.cors_origins,
-        allow_methods=["PUT", "DELETE", "POST", "PATCH", "HEAD", "OPTIONS"],
+        allow_methods=["*"],
         # Authorization will be exluded from * in the future, (CORS non-wildcard request-header).
         # Browsers already showing deprecation notices, so it needs to be specified explicitly
         allow_headers=["*", "Authorization"],
