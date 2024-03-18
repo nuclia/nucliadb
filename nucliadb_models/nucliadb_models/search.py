4f0c85bdd38d027911bfd2fd6dd463e3c83fd68f
Ferran Llamas <ferran@nuclia.com>
2024-03-18 16:08:20 +0100

    validate prompt has at least one character (#1954)
    * validate prompt has at least one character
    
    * min lenght

--- nucliadb_models/nucliadb_models/search.py
+++ nucliadb_models/nucliadb_models/search.py
@@ -553,11 +553,6 @@ class SearchParamDefaults:
         title="Chat features",
         description="Features enabled for the chat endpoint. Semantic search is done if `vectors` is included. If `paragraphs` is included, the results will include matching paragraphs from the bm25 index. If `relations` is included, a graph of entities related to the answer is returned.",  # noqa
     )
-    prompt = ParamDefault(
-        default=None,
-        title="Prompt",
-        description="Input here your prompt with the words {context} and {question} in brackets where you want those fields to be placed, in case you want them in your prompt. Context will be the data returned by the retrieval step.",  # noqa
-    )
     suggest_features = ParamDefault(
         default=[
             SuggestOptions.PARAGRAPH,
@@ -889,7 +884,12 @@ class ChatRequest(BaseModel):
     resource_filters: List[
         str
     ] = SearchParamDefaults.resource_filters.to_pydantic_field()
-    prompt: Optional[str] = SearchParamDefaults.prompt.to_pydantic_field()
+    prompt: Optional[str] = Field(
+        default=None,
+        title="Prompt",
+        description="Input here your prompt with the words {context} and {question} in brackets where you want those fields to be placed, in case you want them in your prompt. Context will be the data returned by the retrieval step.",  # noqa
+        min_length=1,
+    )
     citations: bool = Field(
         default=False,
         description="Whether to include the citations for the answer in the response",
