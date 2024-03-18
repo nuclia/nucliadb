f2729113e591b8a143b8a4b6de6270b0fe4988f5
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-03-18 19:02:12 +0100

    Add migration to purge orphan kbslugs keys

---
+++ nucliadb/migrations/0017_purge_orphan_kbslugs.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Migration #17
+
+Due to a bug on backend services, some kbslugs were not properly deleted and got
+orphan. Let's delete them!
+
+"""
+
+from nucliadb.ingest.orm.knowledgebox import KB_SLUGS, KB_SLUGS_BASE, KnowledgeBox
+from nucliadb.migrator.context import ExecutionContext
+
+
+async def migrate(context: ExecutionContext) -> None:
+    async with context.kv_driver.transaction() as txn:
+        async for key in txn.keys(KB_SLUGS.format(slug=slug), count=-1):
+            slug = key.replace(KB_SLUGS_BASE, "")
+            kbid = await txn.get(key)
+            if kbid is None:
+                logger.error(f"KB with slug ({slug}) but without uuid?")
+                continue
+
+            if not KnowledgeBox.exist_kb(txn, kbid):
+                # log data too just in case
+                logger.info(
+                    "Removing orphan /kbslugs key",
+                    extra={"kbid": kbid, "kb_slug": slug, "maindb_key": key},
+                )
+                await txn.delete(key)
+
+
+async def migrate_kb(context: ExecutionContext, kbid: str) -> None:
+    ...
