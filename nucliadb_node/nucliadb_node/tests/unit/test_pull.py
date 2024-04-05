22c740424d6b2d0d805ecc1fd1fba59d760d21fa
Javier Torres <javier@javiertorres.eu>
2024-04-05 11:06:46 +0200

    lint

--- nucliadb_node/nucliadb_node/tests/unit/test_pull.py
+++ nucliadb_node/nucliadb_node/tests/unit/test_pull.py
@@ -17,9 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import asyncio
 from unittest import mock
-from unittest.mock import AsyncMock, MagicMock, Mock
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 from nats.aio.client import Msg
