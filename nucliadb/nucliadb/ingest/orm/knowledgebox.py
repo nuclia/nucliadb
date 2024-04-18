83ea45caecd09e12612211e39ca8f40d37d320cb
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-04-18 10:01:46 +0200

    Add global migration to purge orphan kbslugs keys from maindb (#1963)
    * Revert "Add logs (#1959)"
    
    This reverts commit 247f86ae067e740d130bb4c0ea09ad96629bde25.
    
    * Add migration to purge orphan kbslugs keys
    
    * Fix lints
    
    * Add test for migration 18
    
    * Common get_migration function for tests
    
    * Fix migration test for tikv
    
    * Changes after rebase
    
    * Lints after rebase and black upgrade
    
    * More lints again?
    
    * Fix migration test
    
    * Fix test again
    
    * Fix test

--- nucliadb/nucliadb/ingest/orm/knowledgebox.py
+++ nucliadb/nucliadb/ingest/orm/knowledgebox.py
@@ -92,7 +92,6 @@ class KnowledgeBox:
     async def delete_kb(cls, txn: Transaction, slug: str = "", kbid: str = ""):
         # Mark storage to be deleted
         # Mark keys to be deleted
-        logger.info(f"Deleting KB kbid={kbid} slug={slug}")
         if not kbid and not slug:
             raise AttributeError()
 
@@ -113,7 +112,6 @@ class KnowledgeBox:
         # Delete main anchor
         async with txn.driver.transaction() as subtxn:
             key_match = datamanagers.kb.KB_SLUGS.format(slug=slug)
-            logger.info(f"Deleting KB with slug: {slug}")
             await subtxn.delete(key_match)
 
             when = datetime.now().isoformat()
