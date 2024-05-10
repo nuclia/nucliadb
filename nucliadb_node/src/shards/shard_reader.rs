d1e350703bac345faf0884540745ddfec96b630b
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-05-10 16:31:06 +0200

    Use proto vectorset on search

--- nucliadb_node/src/shards/shard_reader.rs
+++ nucliadb_node/src/shards/shard_reader.rs
@@ -486,10 +486,21 @@ impl ShardReader {
             request.id = search_id.clone();
             let vectors_context = &index_queries.vectors_context;
             let task = move || {
-                read_rw_lock(&self.vector_readers)
-                    .get(DEFAULT_VECTORS_INDEX_NAME)
-                    .expect("Default vectors index should never be deleted (yet)")
-                    .search(&request, vectors_context)
+                let vectorset = &request.vector_set;
+                if vectorset.is_empty() {
+                    read_rw_lock(&self.vector_readers)
+                        .get(DEFAULT_VECTORS_INDEX_NAME)
+                        .expect("Default vectors index should never be deleted (yet)")
+                        .search(&request, vectors_context)
+                } else {
+                    let vector_readers = read_rw_lock(&self.vector_readers);
+                    let reader = vector_readers.get(vectorset);
+                    if let Some(reader) = reader {
+                        reader.search(&request, vectors_context)
+                    } else {
+                        Err(node_error!("Vectorset '{vectorset}' not found"))
+                    }
+                }
             };
             || run_with_telemetry(info_span!(parent: &span, "vector search"), task)
         });
