488c64d3b2774a4b320c8e80fab9df32d4265975
Javier Torres <javier@javiertorres.eu>
2024-05-14 12:21:35 +0200

    Avoid git dependency

--- nucliadb/nucliadb/tests/integration/search/test_filters_expression.py
+++ nucliadb/nucliadb/tests/integration/search/test_filters_expression.py
@@ -119,7 +119,7 @@ async def test_filtering_expression_validation(nucliadb_reader, nucliadb_writer)
         json={"query": "", "filters": [{"all": []}]},
     )
     assert resp.status_code == 422
-    assert resp.json()["detail"][-1]["msg"] == "ensure this value has at least 1 items"
+    assert "List should have at least 1 item" in resp.json()["detail"][-1]["msg"]
 
     # But we ignore None values
     resp = await nucliadb_reader.post(
