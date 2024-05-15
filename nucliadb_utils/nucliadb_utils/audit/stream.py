fdc134459ce7749f88e248b43da8f6882635e372
David Ruiz Falco <david.rf97@gmail.com>
2024-05-15 18:00:50 +0200

    format

--- nucliadb_utils/nucliadb_utils/audit/stream.py
+++ nucliadb_utils/nucliadb_utils/audit/stream.py
@@ -287,12 +287,14 @@ class StreamAuditStorage(AuditStorage):
             kb_id=kbid,
             kb_source=KBSource.HOSTED,
             # TODO unify AuditRequest client type and Nuclia Usage client type
-            searches=[Search(
-                client=ClientTypeKbUsage.Value(ClientType.Name(client_type)),  # type: ignore
-                type=SearchType.SEARCH,
-                tokens=2000,
-                num_searches=1,
-            )],
+            searches=[
+                Search(
+                    client=ClientTypeKbUsage.Value(ClientType.Name(client_type)),  # type: ignore
+                    type=SearchType.SEARCH,
+                    tokens=2000,
+                    num_searches=1,
+                )
+            ],
         )
 
     async def suggest(
@@ -321,12 +323,14 @@ class StreamAuditStorage(AuditStorage):
             kb_id=kbid,
             kb_source=KBSource.HOSTED,
             # TODO unify AuditRequest client type and Nuclia Usage client type
-            searches=[Search(
-                client=ClientTypeKbUsage.Value(ClientType.Name(client_type)),  # type: ignore
-                type=SearchType.SUGGEST,
-                tokens=0,
-                num_searches=1,
-            )],
+            searches=[
+                Search(
+                    client=ClientTypeKbUsage.Value(ClientType.Name(client_type)),  # type: ignore
+                    type=SearchType.SUGGEST,
+                    tokens=0,
+                    num_searches=1,
+                )
+            ],
         )
 
     async def chat(
