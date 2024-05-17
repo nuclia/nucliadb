98ee842f913c7c754c8a15832b2b3e4f95236bef
Ferran Llamas <llamas.arroniz@gmail.com>
2024-05-17 14:39:58 +0200

    try two methods

--- nucliadb/nucliadb/tests/integration/search/test_search.py
+++ nucliadb/nucliadb/tests/integration/search/test_search.py
@@ -1760,9 +1760,9 @@ async def test_catalog_date_range_filtering(
     body = resp.json()
     assert len(body["resources"]) == 1
 
-    resp = await nucliadb_reader.get(
+    resp = await nucliadb_reader.post(
         f"/kb/{knowledgebox}/catalog",
-        params={
+        json={
             "range_creation_end": one_hour_ago.isoformat(),
         },
     )
