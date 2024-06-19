32853e0c018d4fa0f4db02f6d2a2922ba3df2385
Ferran Llamas <llamas.arroniz@gmail.com>
2024-06-19 12:07:38 +0200

    Fix get_current_active_shard

--- nucliadb/src/nucliadb/ingest/orm/processor/__init__.py
+++ nucliadb/src/nucliadb/ingest/orm/processor/__init__.py
@@ -392,7 +392,8 @@ class Processor:
         if shard is None:
             # It's a new resource, get current active shard to place
             # new resource on
-            shard = await self.shard_manager.get_current_active_shard(txn, kbid)
+            async with datamanagers.with_ro_transaction() as ro_txn:
+                shard = await self.shard_manager.get_current_active_shard(ro_txn, kbid)
             if shard is None:
                 # no shard available, create a new one
                 shard = await self.shard_manager.create_shard_by_kbid(txn, kbid)
