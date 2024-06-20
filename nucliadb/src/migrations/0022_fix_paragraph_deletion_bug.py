d2d1f0f13e63dae8e0fe306c16cb707fabcb2558
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-06-20 17:37:21 +0200

    Add rollover migration to fix paragraph deletion bug (#2267)
    * Add rollover migration to fix paragraph deletion bug
    
    * Only for stage

---
+++ nucliadb/src/migrations/0022_fix_paragraph_deletion_bug.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Migration #22
+
+There was a bug while ingesting/indexing that made paragraphs not being properly
+removed in some cases. This rollover migration ensures data is consistently
+indexed.
+
+"""
+
+import logging
+import os
+
+from nucliadb.common.cluster.rollover import rollover_kb_shards
+from nucliadb.migrator.context import ExecutionContext
+
+logger = logging.getLogger(__name__)
+
+
+async def migrate(context: ExecutionContext) -> None: ...
+
+
+async def migrate_kb(context: ExecutionContext, kbid: str) -> None:
+    if os.environ.get("RUNNING_ENVIRONMENT", os.environ.get("ENVIRONMENT")) == "stage":
+        await rollover_kb_shards(context, kbid)
