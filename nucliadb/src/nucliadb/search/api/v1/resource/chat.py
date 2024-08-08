17fa8fef57fe807e7e8a15b9ebc63eca6c7d8c30
Ferran Llamas <llamas.arroniz@gmail.com>
2024-08-08 17:38:57 +0200

    Remove old chat endpoint

--- nucliadb/src/nucliadb/search/api/v1/resource/chat.py
+++ nucliadb/src/nucliadb/search/api/v1/resource/chat.py
@@ -17,26 +17,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from typing import Optional, Union
-
-from fastapi import Header, Request, Response
+from fastapi import Request
 from fastapi_versioning import version
-from starlette.responses import StreamingResponse
 
-from nucliadb.common import datamanagers
 from nucliadb.models.responses import HTTPClientError
-from nucliadb.search import predict
 from nucliadb.search.api.v1.router import KB_PREFIX, RESOURCE_SLUG_PREFIX, api
-from nucliadb.search.search.exceptions import (
-    IncompleteFindResultsError,
-    InvalidQueryError,
-)
 from nucliadb_models.resource import NucliaDBRoles
-from nucliadb_models.search import ChatRequest, NucliaDBClientType
 from nucliadb_utils.authentication import requires
-from nucliadb_utils.exceptions import LimitsExceededError
-
-from ..chat import create_chat_response
 
 
 @api.post(
@@ -47,6 +34,7 @@ from ..chat import create_chat_response
     tags=["Search"],
     response_model=None,
     deprecated=True,
+    include_in_schema=False,
 )
 @requires(NucliaDBRoles.READER)
 @version(1)
@@ -54,25 +42,10 @@ async def resource_chat_endpoint_by_uuid(
     request: Request,
     kbid: str,
     rid: str,
-    item: ChatRequest,
-    x_ndb_client: NucliaDBClientType = Header(NucliaDBClientType.API),
-    x_nucliadb_user: str = Header(""),
-    x_forwarded_for: str = Header(""),
-    x_synchronous: bool = Header(
-        False,
-        description="When set to true, outputs response as JSON in a non-streaming way. "
-        "This is slower and requires waiting for entire answer to be ready.",
-    ),
-) -> Union[StreamingResponse, HTTPClientError, Response]:
-    return await resource_chat_endpoint(
-        request,
-        kbid,
-        item,
-        x_ndb_client,
-        x_nucliadb_user,
-        x_forwarded_for,
-        x_synchronous,
-        resource_id=rid,
+) -> HTTPClientError:
+    return HTTPClientError(
+        status_code=404,
+        detail="This endpoint has been removed deprecated. Please use /ask instead.",
     )
 
 
@@ -84,6 +57,7 @@ async def resource_chat_endpoint_by_uuid(
     tags=["Search"],
     response_model=None,
     deprecated=True,
+    include_in_schema=False,
 )
 @requires(NucliaDBRoles.READER)
 @version(1)
@@ -91,83 +65,8 @@ async def resource_chat_endpoint_by_slug(
     request: Request,
     kbid: str,
     slug: str,
-    item: ChatRequest,
-    x_ndb_client: NucliaDBClientType = Header(NucliaDBClientType.API),
-    x_nucliadb_user: str = Header(""),
-    x_forwarded_for: str = Header(""),
-    x_synchronous: bool = Header(
-        False,
-        description="When set to true, outputs response as JSON in a non-streaming way. "
-        "This is slower and requires waiting for entire answer to be ready.",
-    ),
-) -> Union[StreamingResponse, HTTPClientError, Response]:
-    return await resource_chat_endpoint(
-        request,
-        kbid,
-        item,
-        x_ndb_client,
-        x_nucliadb_user,
-        x_forwarded_for,
-        x_synchronous,
-        resource_slug=slug,
+) -> HTTPClientError:
+    return HTTPClientError(
+        status_code=404,
+        detail="This endpoint has been removed deprecated. Please use /ask instead.",
     )
-
-
-async def resource_chat_endpoint(
-    request: Request,
-    kbid: str,
-    item: ChatRequest,
-    x_ndb_client: NucliaDBClientType,
-    x_nucliadb_user: str,
-    x_forwarded_for: str,
-    x_synchronous: bool,
-    resource_id: Optional[str] = None,
-    resource_slug: Optional[str] = None,
-) -> Union[StreamingResponse, HTTPClientError, Response]:
-    if resource_id is None:
-        if resource_slug is None:
-            raise ValueError("Either resource_id or resource_slug must be provided")
-
-        resource_id = await get_resource_uuid_by_slug(kbid, resource_slug)
-        if resource_id is None:
-            return HTTPClientError(status_code=404, detail="Resource not found")
-
-    try:
-        return await create_chat_response(
-            kbid,
-            item,
-            x_nucliadb_user,
-            x_ndb_client,
-            x_forwarded_for,
-            x_synchronous,
-            resource=resource_id,
-        )
-    except LimitsExceededError as exc:
-        return HTTPClientError(status_code=exc.status_code, detail=exc.detail)
-    except predict.ProxiedPredictAPIError as err:
-        return HTTPClientError(
-            status_code=err.status,
-            detail=err.detail,
-        )
-    except IncompleteFindResultsError:
-        return HTTPClientError(
-            status_code=529,
-            detail="Temporary error on information retrieval. Please try again.",
-        )
-    except predict.RephraseMissingContextError:
-        return HTTPClientError(
-            status_code=412,
-            detail="Unable to rephrase the query with the provided context.",
-        )
-    except predict.RephraseError as err:
-        return HTTPClientError(
-            status_code=529,
-            detail=f"Temporary error while rephrasing the query. Please try again later. Error: {err}",
-        )
-    except InvalidQueryError as exc:
-        return HTTPClientError(status_code=412, detail=str(exc))
-
-
-async def get_resource_uuid_by_slug(kbid: str, slug: str) -> Optional[str]:
-    async with datamanagers.with_ro_transaction() as txn:
-        return await datamanagers.resources.get_resource_uuid_from_slug(txn, kbid=kbid, slug=slug)
--- nucliadb/src/nucliadb/search/search/chat/query.py
+++ nucliadb/src/nucliadb/search/search/chat/query.py
@@ -17,15 +17,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import asyncio
-import time
-from dataclasses import dataclass
-from typing import AsyncGenerator, AsyncIterator, Optional
+from typing import Optional
 
 from nucliadb.search import logger
-from nucliadb.search.predict import AnswerStatusCode, RephraseMissingContextError
+from nucliadb.search.predict import AnswerStatusCode
 from nucliadb.search.requesters.utils import Method, node_query
-from nucliadb.search.search.chat.prompt import PromptContextBuilder
 from nucliadb.search.search.exceptions import IncompleteFindResultsError
 from nucliadb.search.search.find import find
 from nucliadb.search.search.merge import merge_relations_results
@@ -35,52 +31,25 @@ from nucliadb.search.utilities import get_predict
 from nucliadb_models.search import (
     Author,
     ChatContextMessage,
-    ChatModel,
     ChatOptions,
     ChatRequest,
     FindRequest,
     KnowledgeboxFindResults,
-    MinScore,
     NucliaDBClientType,
     PromptContext,
     PromptContextOrder,
     Relations,
     RephraseModel,
     SearchOptions,
-    UserPrompt,
-    parse_custom_prompt,
 )
 from nucliadb_protos import audit_pb2
 from nucliadb_protos.nodereader_pb2 import RelationSearchRequest, RelationSearchResponse
 from nucliadb_telemetry.errors import capture_exception
-from nucliadb_utils.helpers import async_gen_lookahead
 from nucliadb_utils.utilities import get_audit
 
 NOT_ENOUGH_CONTEXT_ANSWER = "Not enough data to answer this."
 AUDIT_TEXT_RESULT_SEP = " \n\n "
 START_OF_CITATIONS = b"_CIT_"
-
-
-class FoundStatusCode:
-    def __init__(self, default: AnswerStatusCode = AnswerStatusCode.SUCCESS):
-        self._value = AnswerStatusCode.SUCCESS
-
-    def set(self, value: AnswerStatusCode) -> None:
-        self._value = value
-
-    @property
-    def value(self) -> AnswerStatusCode:
-        return self._value
-
-
-@dataclass
-class ChatResult:
-    nuclia_learning_id: Optional[str]
-    answer_stream: AsyncIterator[bytes]
-    status_code: FoundStatusCode
-    find_results: KnowledgeboxFindResults
-    prompt_context: PromptContext
-    prompt_context_order: PromptContextOrder
 
 
 async def rephrase_query(
@@ -100,35 +69,6 @@ async def rephrase_query(
         generative_model=generative_model,
     )
     return await predict.rephrase_query(kbid, req)
-
-
-async def format_generated_answer(
-    answer_generator: AsyncGenerator[bytes, None], output_status_code: FoundStatusCode
-):
-    status_code: Optional[AnswerStatusCode] = None
-    is_last_chunk = False
-    async for answer_chunk, is_last_chunk in async_gen_lookahead(answer_generator):
-        if is_last_chunk:
-            try:
-                status_code = _parse_answer_status_code(answer_chunk)
-            except ValueError:
-                # TODO: remove this in the future, it's
-                # just for bw compatibility until predict
-                # is updated to the new protocol
-                status_code = AnswerStatusCode.SUCCESS
-                yield answer_chunk
-            else:
-                # TODO: this should be needed but, in case we receive the status
-                # code mixed with text, we strip it and return the text
-                if len(answer_chunk) != len(status_code.encode()):
-                    answer_chunk = answer_chunk.rstrip(status_code.encode())
-                    yield answer_chunk
-            break
-        yield answer_chunk
-    if not is_last_chunk:
-        logger.warning("BUG: /chat endpoint without last chunk")
-
-    output_status_code.set(status_code or AnswerStatusCode.SUCCESS)
 
 
 async def get_find_results(
@@ -209,190 +149,6 @@ async def get_relations_results(
         capture_exception(exc)
         logger.exception("Error getting relations results")
         return Relations(entities={})
-
-
-async def not_enough_context_generator():
-    await asyncio.sleep(0)
-    yield NOT_ENOUGH_CONTEXT_ANSWER.encode()
-    yield AnswerStatusCode.NO_CONTEXT.encode()
-
-
-async def chat(
-    kbid: str,
-    chat_request: ChatRequest,
-    user_id: str,
-    client_type: NucliaDBClientType,
-    origin: str,
-    resource: Optional[str] = None,
-) -> ChatResult:
-    metrics = RAGMetrics()
-    nuclia_learning_id: Optional[str] = None
-    chat_history = chat_request.context or []
-    user_context = chat_request.extra_context or []
-    user_query = chat_request.query
-    rephrased_query = None
-    prompt_context: PromptContext = {}
-    prompt_context_order: PromptContextOrder = {}
-
-    if len(chat_history) > 0 or len(user_context) > 0:
-        try:
-            with metrics.time("rephrase"):
-                rephrased_query = await rephrase_query(
-                    kbid,
-                    chat_history=chat_history,
-                    query=user_query,
-                    user_id=user_id,
-                    user_context=user_context,
-                    generative_model=chat_request.generative_model,
-                )
-        except RephraseMissingContextError:
-            logger.info("Failed to rephrase chat query, using original")
-
-    # Retrieval is not needed if we are chatting on a specific
-    # resource and the full_resource strategy is enabled
-    needs_retrieval = True
-    if resource is not None:
-        chat_request.resource_filters = [resource]
-        if any(strategy.name == "full_resource" for strategy in chat_request.rag_strategies):
-            needs_retrieval = False
-
-    if needs_retrieval:
-        with metrics.time("retrieval"):
-            find_results, query_parser = await get_find_results(
-                kbid=kbid,
-                query=rephrased_query or user_query,
-                chat_request=chat_request,
-                ndb_client=client_type,
-                user=user_id,
-                origin=origin,
-                metrics=metrics,
-            )
-        status_code = FoundStatusCode()
-        if len(find_results.resources) == 0:
-            # If no resources were found on the retrieval, we return
-            # a "Not enough context" answer and skip the llm query
-            answer_stream = format_generated_answer(not_enough_context_generator(), status_code)
-            return ChatResult(
-                nuclia_learning_id=nuclia_learning_id,
-                answer_stream=answer_stream,
-                status_code=status_code,
-                find_results=find_results,
-                prompt_context=prompt_context,
-                prompt_context_order=prompt_context_order,
-            )
-    else:
-        status_code = FoundStatusCode()
-        find_results = KnowledgeboxFindResults(resources={}, min_score=None)
-        query_parser = QueryParser(
-            kbid=kbid,
-            features=[],
-            query="",
-            filters=chat_request.filters,
-            page_number=0,
-            page_size=0,
-            min_score=MinScore(),
-        )
-
-    with metrics.time("context_building"):
-        query_parser.max_tokens = chat_request.max_tokens  # type: ignore
-        max_tokens_context = await query_parser.get_max_tokens_context()
-        prompt_context_builder = PromptContextBuilder(
-            kbid=kbid,
-            find_results=find_results,
-            resource=resource,
-            user_context=user_context,
-            strategies=chat_request.rag_strategies,
-            image_strategies=chat_request.rag_images_strategies,
-            max_context_characters=tokens_to_chars(max_tokens_context),
-            visual_llm=await query_parser.get_visual_llm_enabled(),
-        )
-        (
-            prompt_context,
-            prompt_context_order,
-            prompt_context_images,
-        ) = await prompt_context_builder.build()
-
-    custom_prompt = parse_custom_prompt(chat_request)
-    chat_model = ChatModel(
-        user_id=user_id,
-        system=custom_prompt.system,
-        user_prompt=UserPrompt(prompt=custom_prompt.user) if custom_prompt.user else None,
-        query_context=prompt_context,
-        query_context_order=prompt_context_order,
-        chat_history=chat_history,
-        question=user_query,
-        truncate=True,
-        citations=chat_request.citations,
-        generative_model=chat_request.generative_model,
-        max_tokens=query_parser.get_max_tokens_answer(),
-        query_context_images=prompt_context_images,
-        prefer_markdown=chat_request.prefer_markdown,
-    )
-    predict = get_predict()
-    generative_start = time.monotonic()
-    nuclia_learning_id, predict_generator = await predict.chat_query(kbid, chat_model)
-
-    async def _wrapped_stream():
-        # so we can audit after streamed out answer
-        text_answer = b""
-        async for chunk in format_generated_answer(predict_generator, status_code):
-            if text_answer == b"":
-                # first chunk
-                metrics.record_first_chunk_yielded()
-            text_answer += chunk
-            yield chunk
-        try:
-            rephrase_time = metrics.elapsed("rephrase")
-        except KeyError:
-            rephrase_time = None
-
-        maybe_audit_chat(
-            kbid=kbid,
-            user_id=user_id,
-            client_type=client_type,
-            origin=origin,
-            user_query=user_query,
-            rephrased_query=rephrased_query,
-            rephrase_time=rephrase_time,
-            generative_answer_time=time.monotonic() - generative_start,
-            generative_answer_first_chunk_time=metrics.first_chunk_yielded_at - metrics.global_start,
-            text_answer=text_answer,
-            status_code=status_code.value,
-            chat_history=chat_history,
-            query_context=prompt_context,
-            query_context_order=prompt_context_order,
-            learning_id=nuclia_learning_id,
-        )
-
-    answer_stream = _wrapped_stream()
-    return ChatResult(
-        nuclia_learning_id=nuclia_learning_id,
-        answer_stream=answer_stream,
-        status_code=status_code,
-        find_results=find_results,
-        prompt_context=prompt_context,
-        prompt_context_order=prompt_context_order,
-    )
-
-
-def _parse_answer_status_code(chunk: bytes) -> AnswerStatusCode:
-    """
-    Parses the status code from the last chunk of the answer.
-    """
-    try:
-        return AnswerStatusCode(chunk.decode())
-    except ValueError:
-        # In some cases, even if the status code was yield separately
-        # at the server side, the status code is appended to the previous chunk...
-        # It may be a bug in the aiohttp.StreamResponse implementation,
-        # but we haven't spotted it yet. For now, we just try to parse the status code
-        # from the tail of the chunk.
-        logger.debug(f"Error decoding status code from /chat's last chunk. Chunk: {chunk!r}")
-        if chunk == b"":
-            raise
-        if chunk.endswith(b"0"):
-            return AnswerStatusCode.SUCCESS
-        return AnswerStatusCode(chunk[-2:].decode())
 
 
 def maybe_audit_chat(
