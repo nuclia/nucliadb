8c42035efda0e0db385c8a7260447bc28a8919c9
Ferran Llamas <llamas.arroniz@gmail.com>
2024-08-14 17:09:15 +0200

    fix

--- nucliadb/src/nucliadb/search/search/find.py
+++ nucliadb/src/nucliadb/search/search/find.py
@@ -177,6 +177,7 @@ async def _index_node_retrieval(
                 "query": item.model_dump_json(),
                 "time": search_time,
                 "nodes": debug_nodes_info(queried_nodes),
+                "durations": metrics.steps(),
             },
         )
     elif search_time > settings.slow_find_log_threshold:
@@ -189,7 +190,7 @@ async def _index_node_retrieval(
                 "query": item.model_dump_json(),
                 "time": search_time,
                 "nodes": debug_nodes_info(queried_nodes),
-                "durations": metrics.durations(),
+                "durations": metrics.steps(),
             },
         )
 
