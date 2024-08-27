597a4d172ee7c36b8a1f16b3d3851e2d189e1fe1
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-08-27 17:40:00 +0200

    Consider vectorsets for rollover shards

--- nucliadb/src/nucliadb/common/cluster/rollover.py
+++ nucliadb/src/nucliadb/common/cluster/rollover.py
@@ -149,19 +149,33 @@ async def create_rollover_shards(
                 if node is None:
                     logger.error(f"Node {node_id} is not found or not available")
                     continue
-                is_matryoshka = len(kb_shards.model.matryoshka_dimensions) > 0
-                vector_index_config = nodewriter_pb2.VectorIndexConfig(
-                    similarity=kb_shards.similarity,
-                    vector_type=nodewriter_pb2.VectorType.DENSE_F32,
-                    vector_dimension=kb_shards.model.vector_dimension,
-                    normalize_vectors=is_matryoshka,
-                )
-                try:
-                    shard_created = await node.new_shard(
-                        kbid,
-                        release_channel=kb_shards.release_channel,
-                        vector_index_config=vector_index_config,
+
+                vectorsets = {
+                    vectorset_id: vectorset_config.vectorset_index_config
+                    async for vectorset_id, vectorset_config in datamanagers.vectorsets.iter(
+                        txn, kbid=kbid
                     )
+                }
+                try:
+                    if not vectorsets:
+                        is_matryoshka = len(kb_shards.model.matryoshka_dimensions) > 0
+                        vector_index_config = nodewriter_pb2.VectorIndexConfig(
+                            similarity=kb_shards.similarity,
+                            vector_type=nodewriter_pb2.VectorType.DENSE_F32,
+                            vector_dimension=kb_shards.model.vector_dimension,
+                            normalize_vectors=is_matryoshka,
+                        )
+                        shard_created = await node.new_shard(
+                            kbid,
+                            release_channel=kb_shards.release_channel,
+                            vector_index_config=vector_index_config,
+                        )
+                    else:
+                        shard_created = await node.new_shard_with_vectorsets(
+                            kbid,
+                            release_channel=kb_shards.release_channel,
+                            vectorsets_configs=vectorsets,
+                        )
                 except Exception as e:
                     errors.capture_exception(e)
                     logger.exception(f"Error creating new shard at {node}")
