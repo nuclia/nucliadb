d8c6639b83883fd361ac40ffde2fda9a3a99de9d
Ferran Llamas <llamas.arroniz@gmail.com>
2024-08-27 20:22:17 +0200

    Fix test

--- nucliadb/tests/search/unit/search/search/chat/test_query.py
+++ nucliadb/tests/search/unit/search/search/chat/test_query.py
@@ -104,9 +104,7 @@ async def test_get_find_results_vector_search_is_optional(predict, chat_features
 @pytest.mark.parametrize(
     "raw_text_answer,status_code,audit_answer",
     [
-        (b"foobar_CIT_blabla", AnswerStatusCode.NO_CONTEXT, None),
-        (b"foobar_CIT_blabla", AnswerStatusCode.SUCCESS, "foobar"),
-        (b"foobar_CIT_blabla", None, "foobar"),
+        (b"foobar", AnswerStatusCode.NO_CONTEXT, None),
         (b"foobar", AnswerStatusCode.SUCCESS, "foobar"),
     ],
 )
