d88172c378d6d24751f47e33a2d2b017a9f294fc
Ferran Llamas <llamas.arroniz@gmail.com>
2024-10-07 11:42:13 +0200

    Fix logic around image rag strategies

--- nucliadb/src/nucliadb/search/search/chat/prompt.py
+++ nucliadb/src/nucliadb/search/search/chat/prompt.py
@@ -782,33 +782,47 @@ class PromptContextBuilder:
         return context, context_order, context_images
 
     async def _build_context_images(self, context: CappedPromptContext) -> None:
-        page_count = 5
-        gather_pages = False
-        gather_tables = False
-        if self.image_strategies is not None:
-            for strategy in self.image_strategies:
-                if strategy.name == ImageRagStrategyName.PAGE_IMAGE:
-                    strategy = cast(PageImageStrategy, strategy)
-                    gather_pages = True
-                    if strategy.count is not None and strategy.count > 0:
-                        page_count = strategy.count
-                elif strategy.name == ImageRagStrategyName.TABLES:
-                    strategy = cast(TableImageStrategy, strategy)
-                    gather_tables = True
+        if self.image_strategies is None or len(self.image_strategies) == 0:
+            # Nothing to do
+            return
 
+        page_image_strategy: Optional[PageImageStrategy] = None
+        max_page_images = 5
+        table_image_strategy: Optional[TableImageStrategy] = None
+        for strategy in self.image_strategies:
+            if strategy.name == ImageRagStrategyName.PAGE_IMAGE:
+                page_image_strategy = cast(PageImageStrategy, strategy)
+                if page_image_strategy.count is not None:
+                    max_page_images = page_image_strategy.count
+            elif strategy.name == ImageRagStrategyName.TABLES:
+                table_image_strategy = cast(TableImageStrategy, strategy)
+
+        page_images_added = 0
         for paragraph in self.ordered_paragraphs:
-            if paragraph.page_with_visual and paragraph.position:
-                if gather_pages and paragraph.position.page_number and len(context.images) < page_count:
-                    field = "/".join(paragraph.id.split("/")[:3])
-                    page = paragraph.position.page_number
-                    page_id = f"{field}/{page}"
-                    if page_id not in context.images:
-                        context.images[page_id] = await get_page_image(self.kbid, paragraph.id, page)
-            # Only send tables if enabled by strategy, by default, send paragraph images
-            send_images = (gather_tables and paragraph.is_a_table) or not paragraph.is_a_table
-            if send_images and paragraph.reference and paragraph.reference != "":
-                image = paragraph.reference
-                context.images[paragraph.id] = await get_paragraph_image(self.kbid, paragraph.id, image)
+            pid = ParagraphId.from_string(paragraph.id)
+            paragraph_page_number = get_paragraph_page_number(paragraph)
+            if (
+                page_image_strategy is not None
+                and page_images_added < max_page_images
+                and paragraph_page_number is not None
+            ):
+                # page_image_id: rid/f/myfield/0
+                page_image_id = "/".join([pid.field_id.full(), paragraph_page_number])  # type: ignore
+                if page_image_id not in context.images:
+                    context.images[page_image_id] = await get_page_image(
+                        self.kbid, pid, paragraph_page_number
+                    )
+                    page_images_added += 1
+
+            if (
+                table_image_strategy is not None
+                and paragraph.is_a_table
+                and paragraph.reference is not None
+                and paragraph.reference != ""
+            ):
+                context.images[paragraph.id] = await get_paragraph_image(
+                    self.kbid, pid, paragraph.reference
+                )
 
     async def _build_context(self, context: CappedPromptContext) -> None:
         if self.strategies is None or len(self.strategies) == 0:
@@ -873,6 +887,14 @@ class PromptContextBuilder:
             )
         if metadata_extension:
             await extend_prompt_context_with_metadata(context, self.kbid, metadata_extension)
+
+
+def get_paragraph_page_number(paragraph: FindParagraph) -> Optional[int]:
+    if not paragraph.page_with_visual:
+        return None
+    if paragraph.position is None:
+        return None
+    return paragraph.position.page_number
 
 
 @dataclass
