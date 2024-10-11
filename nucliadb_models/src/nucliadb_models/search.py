a1917cb17b8a75a5241d9643c231f823ad7ca52a
Ferran Llamas <llamas.arroniz@gmail.com>
2024-10-11 15:52:02 +0200

    Limit query parameter

--- nucliadb_models/src/nucliadb_models/search.py
+++ nucliadb_models/src/nucliadb_models/search.py
@@ -413,7 +413,7 @@ class KnowledgeBoxCount(BaseModel):
 
 class SearchParamDefaults:
     query = ParamDefault(
-        default="", title="Query", description="The query to search for", max_items=10_000
+        default="", title="Query", description="The query to search for", max_items=20_000
     )
     suggest_query = ParamDefault(
         default=..., title="Query", description="The query to get suggestions for"
@@ -448,7 +448,7 @@ class SearchParamDefaults:
         default=...,
         title="Query",
         description="The query to get a generative answer for",
-        max_items=10_000,
+        max_items=20_000,
     )
     shards = ParamDefault(
         default=[],
