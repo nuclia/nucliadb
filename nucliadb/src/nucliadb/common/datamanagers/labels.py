99d66101fc8865556d303a613408e482d16b0817
Ferran Llamas <llamas.arroniz@gmail.com>
2024-10-22 08:03:41 +0200

    wip

--- nucliadb/src/nucliadb/common/datamanagers/labels.py
+++ nucliadb/src/nucliadb/common/datamanagers/labels.py
@@ -37,7 +37,9 @@ async def get_labels(txn: Transaction, *, kbid: str) -> kb_pb2.Labels:
     Get all labels for a knowledge box (from multiple labelsets)
     """
     labels = kb_pb2.Labels()
-    labelset_ids = await _get_labelset_ids_bw_compat(txn, kbid=kbid)
+    labelset_ids = await _get_labelset_ids(txn, kbid=kbid)
+    if labelset_ids is None:
+        return labels
     for labelset_id in labelset_ids:
         labelset = await txn.get(KB_LABELSET.format(kbid=kbid, id=labelset_id))
         if not labelset:
@@ -46,26 +48,6 @@ async def get_labels(txn: Transaction, *, kbid: str) -> kb_pb2.Labels:
         ls.ParseFromString(labelset)
         labels.labelset[labelset_id].CopyFrom(ls)
     return labels
-
-
-async def _get_labelset_ids_bw_compat(txn: Transaction, *, kbid: str) -> list[str]:
-    labelsets = await _get_labelset_ids(txn, kbid=kbid)
-    if labelsets is not None:
-        return labelsets
-    # TODO: Remove this after migration #11
-    return await _deprecated_scan_labelset_ids(txn, kbid=kbid)
-
-
-async def _deprecated_scan_labelset_ids(txn: Transaction, *, kbid: str) -> list[str]:
-    logger.warning(
-        "Scanning labelset ids. This is not optimal and should have been migrated.", extra={"kbid": kbid}
-    )
-    labelsets = []
-    labels_key = KB_LABELS.format(kbid=kbid)
-    async for key in txn.keys(labels_key, count=-1, include_start=False):
-        lsid = key.split("/")[-1]
-        labelsets.append(lsid)
-    return labelsets
 
 
 async def _get_labelset_ids(txn: Transaction, *, kbid: str) -> Optional[list[str]]:
@@ -80,9 +62,7 @@ async def _add_to_labelset_ids(txn: Transaction, *, kbid: str, labelsets: list[s
     previous = await _get_labelset_ids(txn, kbid=kbid)
     needs_set = False
     if previous is None:
-        # TODO: Remove this after migration #11
         needs_set = True
-        previous = await _deprecated_scan_labelset_ids(txn, kbid=kbid)
     for labelset in labelsets:
         if labelset not in previous:
             needs_set = True
@@ -92,12 +72,11 @@ async def _add_to_labelset_ids(txn: Transaction, *, kbid: str, labelsets: list[s
 
 
 async def _delete_from_labelset_ids(txn: Transaction, *, kbid: str, labelsets: list[str]) -> None:
-    needs_set = False
     previous = await _get_labelset_ids(txn, kbid=kbid)
     if previous is None:
-        # TODO: Remove this after migration #11
-        needs_set = True
-        previous = await _deprecated_scan_labelset_ids(txn, kbid=kbid)
+        # Nothing to delete
+        return
+    needs_set = False
     for labelset in labelsets:
         if labelset in previous:
             needs_set = True
