89f8585c6ba80bfb1760d379c08371269831c5db
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-11-13 13:50:12 +0100

    Fix test

--- nucliadb/tests/search/integration/api/v1/test_search.py
+++ nucliadb/tests/search/integration/api/v1/test_search.py
@@ -181,7 +181,7 @@ async def test_search_resource_all(
                     # 48-65 : Where is my beer?
 
                     # Q : Where is my wine?
-                    results = [(x.doc_id.id, x.score) for x in vectors.vector.documents]
+                    results = [(x.doc_id.id, x.score) for x in vectors.documents]
                     results.sort(reverse=True, key=lambda x: x[1])
                     assert results[0][0].endswith("48-65")
                     assert results[1][0].endswith("0-19")
