b914341b79c855e3ba02aca14ea0fce894b1090e
Ferran Llamas <ferran@nuclia.com>
2024-12-03 18:35:02 +0100

    Ingest pull consumers: pick up from where we left off (#2686)

--- nucliadb_utils/src/nucliadb_utils/const.py
+++ nucliadb_utils/src/nucliadb_utils/const.py
@@ -80,3 +80,4 @@ class Features:
     LOG_REQUEST_PAYLOADS = "nucliadb_log_request_payloads"
     IGNORE_EXTRACTED_IN_SEARCH = "nucliadb_ignore_extracted_in_search"
     NIDX_READS = "nucliadb_nidx_reads"
+    PULL_PROCESSED_CONSUMERS_DEPLOYED = "nucliadb_pull_processed_consumers_deployed"
