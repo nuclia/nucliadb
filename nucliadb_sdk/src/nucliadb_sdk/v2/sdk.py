142f7b7e97b6fc9ed80ea8a3691b94031df5ee1a
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-12-03 11:09:00 +0100

    Use exclude_unset and add test to validate

--- nucliadb_sdk/src/nucliadb_sdk/v2/sdk.py
+++ nucliadb_sdk/src/nucliadb_sdk/v2/sdk.py
@@ -182,7 +182,7 @@ def _parse_list_of_pydantic(
     output = []
     for item in data:
         if isinstance(item, BaseModel):
-            output.append(item.model_dump(by_alias=True, exclude_defaults=True))
+            output.append(item.model_dump(by_alias=True, exclude_unset=True))
         else:
             output.append(item)
     return orjson.dumps(output).decode("utf-8")
@@ -244,7 +244,7 @@ def _request_builder(
                     if not isinstance(content, request_type):  # type: ignore
                         raise TypeError(f"Expected {request_type}, got {type(content)}")
                     else:
-                        data = content.model_dump_json(by_alias=True, exclude_defaults=True)
+                        data = content.model_dump_json(by_alias=True, exclude_unset=True)
                 except TypeError:
                     if not isinstance(content, list):
                         raise
@@ -256,7 +256,7 @@ def _request_builder(
                     if key in request_type.model_fields:  # type: ignore
                         content_data[key] = kwargs.pop(key)
                 data = request_type.model_validate(content_data).model_dump_json(  # type: ignore
-                    by_alias=True, exclude_defaults=True
+                    by_alias=True, exclude_unset=True
                 )
         elif is_raw_request_content(content):
             raw_content = content
