7d9a64aa4f63857c63ab6c3c5a64c1242d92089c
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-12-16 12:39:30 +0100

    Don't serialize unset values on SDK requests (#2680)
    * Don't serialize default values on SDK requests
    
    * Use exclude_unset and add test to validate

---
+++ nucliadb_sdk/tests/test_request_serialization.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import json
+from unittest.mock import Mock, patch
+
+import nucliadb_sdk
+from nucliadb_models.search import FindRequest, KnowledgeboxFindResults, SearchOptions
+from nucliadb_sdk import Region
+
+
+def test_find_request_serialization() -> None:
+    sdk = nucliadb_sdk.NucliaDB(region=Region.ON_PREM, url="http://fake:8080")
+
+    with patch.object(
+        sdk,
+        "_request",
+        return_value=Mock(content=KnowledgeboxFindResults(resources={}).model_dump_json()),
+    ) as spy:
+        req = FindRequest(query="love", features=[SearchOptions.RELATIONS])
+
+        sdk.find(kbid="kbid", content=req)
+
+        sent = json.loads(spy.call_args.kwargs["data"])
+
+        assert sent == {"query": "love", "features": [SearchOptions.RELATIONS.value]}
+        assert sent == req.model_dump(exclude_unset=True)
