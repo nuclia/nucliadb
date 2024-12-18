c27fda239acb5b136a835bb83ac7e5e1a270cdbb
Joan Antoni RE <joanantoni.re16@gmail.com>
2024-12-18 15:40:58 +0100

    Avoid __init__ import hack and properly set fixtures in conftest.py

--- nucliadb/tests/ndbfixtures/__init__.py
+++ nucliadb/tests/ndbfixtures/__init__.py
@@ -18,29 +18,4 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""NucliaDB fixtures
-
-This module should never be imported directly, as it contains a dangerous mix of
-imports. It's done like this to avoid having to import all files in the conftest.
-
-"""
-
-# hacks and magic things with pytest to implement deploy mode parametrization
-
-from .magic import *  # noqa
-
-# components and deployment modes
-
-from .reader import *  # noqa
-from .writer import *  # noqa
-
-# subcomponents
-
-from .common import *  # type: ignore # noqa
-from .maindb import *  # noqa
-from .node import *  # noqa
-from .processing import *  # noqa
-
-# useful resources for tests (KBs...)
-
-from .resources import *  # noqa
+"""NucliaDB fixtures."""
