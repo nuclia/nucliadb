b07bc116f614c413e5ed12fb937e90e6552ea7a2
Javier Torres <javier@javiertorres.eu>
2024-12-24 15:00:23 +0100

    Proper restart of nidx utility between test runs

--- nucliadb/tests/ndbfixtures/maindb.py
+++ nucliadb/tests/ndbfixtures/maindb.py
@@ -27,6 +27,7 @@ from pytest_docker_fixtures import images
 
 from nucliadb.common.maindb.driver import Driver
 from nucliadb.common.maindb.pg import PGDriver
+from nucliadb.common.nidx import NIDX_ENABLED
 from nucliadb.ingest.settings import DriverConfig, DriverSettings
 from nucliadb.ingest.settings import settings as ingest_settings
 from nucliadb.migrator.migrator import run_pg_schema_migrations
@@ -72,6 +73,10 @@ async def cleanup_maindb(driver: Driver):
         for key in all_keys:
             await txn.delete(key)
         await txn.commit()
+
+        if NIDX_ENABLED:
+            async with txn.connection.cursor() as cur:
+                await cur.execute("TRUNCATE shards CASCADE")
 
 
 @pytest.fixture(scope="function")
