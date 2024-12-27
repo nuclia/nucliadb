f3557330463fc86e333b7fee80749f146f7f9104
Ferran Llamas <llamas.arroniz@gmail.com>
2024-12-27 10:15:12 +0100

    remove debug code

--- nucliadb_utils/src/nucliadb_utils/storages/storage.py
+++ nucliadb_utils/src/nucliadb_utils/storages/storage.py
@@ -305,7 +305,6 @@ class Storage(abc.ABC, metaclass=abc.ABCMeta):
             new_cf = CloudFile()
             new_cf.CopyFrom(file)
         else:
-            return CloudFile()
             raise InvalidCloudFile()
         return new_cf
 
