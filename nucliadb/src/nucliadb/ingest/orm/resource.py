0962437eed571c66cc0899794e50e7ef39b3fc82
Joan Antoni RE <joanantoni.re16@gmail.com>
2025-01-10 11:29:54 +0100

    Pass replace_field parameter

--- nucliadb/src/nucliadb/ingest/orm/resource.py
+++ nucliadb/src/nucliadb/ingest/orm/resource.py
@@ -226,6 +226,7 @@ class Resource:
                             page_positions=page_positions,
                             extracted_text=await field_obj.get_extracted_text(),
                             basic_user_field_metadata=user_field_metadata,
+                            replace_field=True,
                         )
 
         # Some basic fields are computed off field metadata.
@@ -336,6 +337,7 @@ class Resource:
                     page_positions=page_positions,
                     extracted_text=await field.get_extracted_text(),
                     basic_user_field_metadata=user_field_metadata,
+                    replace_field=reindex,
                 )
 
             if self.disable_vectors is False:
@@ -724,6 +726,7 @@ class Resource:
             page_positions=page_positions,
             extracted_text=extracted_text,
             basic_user_field_metadata=user_field_metadata,
+            replace_field=True,
         )
         loop = asyncio.get_running_loop()
         await loop.run_in_executor(_executor, apply_field_metadata)
