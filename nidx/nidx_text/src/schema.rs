85a4c8d72785bd490f07b4d2b7199fcf43f7036c
Javier Torres <javier@javiertorres.eu>
2025-01-21 12:38:15 +0100

    Text v2: faster prefiltering (#2791)

--- nidx/nidx_text/src/schema.rs
+++ nidx/nidx_text/src/schema.rs
@@ -18,6 +18,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
 
+use std::cmp::min;
+
 use tantivy::{
     schema::{FacetOptions, Field, NumericOptions, Schema, FAST, INDEXED, STORED, TEXT},
     DateOptions, DateTime,
@@ -42,6 +44,8 @@ pub struct TextSchema {
     // Security
     pub groups_public: Field,
     pub groups_with_access: Field,
+
+    pub encoded_field_id: Option<Field>,
 }
 
 pub fn timestamp_to_datetime_utc(timestamp: &nidx_protos::prost_types::Timestamp) -> DateTime {
@@ -49,7 +53,7 @@ pub fn timestamp_to_datetime_utc(timestamp: &nidx_protos::prost_types::Timestamp
 }
 
 impl TextSchema {
-    pub fn new() -> Self {
+    pub fn new(version: u64) -> Self {
         let mut sb = Schema::builder();
         let num_options: NumericOptions = NumericOptions::default().set_indexed().set_fast();
 
@@ -76,6 +80,15 @@ impl TextSchema {
         let groups_public = sb.add_u64_field("groups_public", num_options);
         let groups_with_access = sb.add_facet_field("groups_with_access", facet_options);
 
+        // v2: Field ID encoded as array of u64 for faster retrieval during prefilter
+        // Using a bytes field is slow due to tantivy's implementation being slow with many unique values.
+        // A better implementation is tracked in https://github.com/quickwit-oss/tantivy/issues/2090
+        let encoded_field_id = if version >= 2 {
+            Some(sb.add_u64_field("encoded_field_id", FAST))
+        } else {
+            None
+        };
+
         let schema = sb.build();
 
         TextSchema {
@@ -89,12 +102,71 @@ impl TextSchema {
             field,
             groups_public,
             groups_with_access,
+            encoded_field_id,
         }
     }
 }
 
-impl Default for TextSchema {
-    fn default() -> Self {
-        Self::new()
+/// Encodes a resource and field id as a series of u64
+/// This is stored in the fast field `encoded_field_id`
+/// We convert to bytes, batch them in sets of 8, and store
+/// as an array of u64
+pub fn encode_field_id(rid: uuid::Uuid, fid: &str) -> Vec<u64> {
+    let (r1, r2) = rid.as_u64_pair();
+    let mut out = vec![r1, r2];
+    let bb = fid.as_bytes();
+    let mut slice = bb;
+    while !slice.is_empty() {
+        let take = min(8, slice.len());
+        let mut data = [0; 8];
+        data[..take].copy_from_slice(&slice[..take]);
+        slice = &slice[take..];
+        out.push(u64::from_le_bytes(data));
+    }
+
+    out
+}
+
+/// Decodes a resource and field id from a series of u64
+/// This is retrieved from the fast field `encoded_field_id`
+/// and used for faster loading in the prefilter Collector
+pub fn decode_field_id(data: &[u64]) -> (uuid::Uuid, String) {
+    let rid = uuid::Uuid::from_u64_pair(data[0], data[1]);
+    let mut ubytes = Vec::with_capacity((data.len() - 2) * 8);
+    for word in &data[2..] {
+        let wb = word.to_le_bytes();
+        let mut i = 7;
+        while wb[i] == 0 {
+            i -= 1;
+        }
+        ubytes.extend_from_slice(&wb[0..=i]);
+    }
+    (rid, String::from_utf8(ubytes).unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{decode_field_id, encode_field_id};
+
+    #[test]
+    fn test_encode_decode_field_id() {
+        // Test with different lengths to ensure it works when crossing block length
+        let testcases = [
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title1"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title12"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title123"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title1234"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title12345"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title123456"),
+            ("03ce0c2cdcc0060069fb8f69c73fa8e2", "/a/title1234567"),
+        ];
+        for t in testcases {
+            let rid = uuid::Uuid::parse_str(t.0).unwrap();
+            let data = encode_field_id(rid, t.1);
+            let (decoded_rid, decoded_fid) = decode_field_id(&data);
+            assert_eq!(decoded_rid.simple().to_string().as_str(), t.0);
+            assert_eq!(decoded_fid, t.1);
+        }
     }
 }
