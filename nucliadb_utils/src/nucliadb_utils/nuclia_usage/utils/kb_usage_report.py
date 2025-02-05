73459d129f53c423047f74c14a92a187f57040a8
David Ruiz Falco <david.rf97@gmail.com>
2025-02-05 17:14:19 +0000

    rethink

--- nucliadb_utils/src/nucliadb_utils/nuclia_usage/utils/kb_usage_report.py
+++ nucliadb_utils/src/nucliadb_utils/nuclia_usage/utils/kb_usage_report.py
@@ -96,9 +96,7 @@ class KbUsageReportUtility:
         predicts: Iterable[Predict] = (),
         searches: Iterable[Search] = (),
         storage: Optional[Storage] = None,
-        trace_id: Optional[str] = None,
-        learning_id: Optional[str] = None,
-        resource_id: Optional[str] = None,
+        log_match_id: Optional[str] = None,
     ):
         usage = KbUsage()
         usage.service = service  # type: ignore
@@ -108,12 +106,8 @@ class KbUsageReportUtility:
         if kb_id is not None:
             usage.kb_id = kb_id
         usage.kb_source = kb_source  # type: ignore
-        if trace_id is not None:
-            usage.trace_id = trace_id
-        if learning_id is not None:
-            usage.learning_id = learning_id
-        if resource_id is not None:
-            usage.resource_id = resource_id
+        if log_match_id is not None:
+            usage.log_match_id = log_match_id
 
         usage.processes.extend(processes)
         usage.predicts.extend(predicts)
