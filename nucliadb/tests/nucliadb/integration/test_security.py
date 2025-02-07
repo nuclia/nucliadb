ec94e0be3cff7214c80d1ed2bb36d40430fad88f
Carles Bruguera <carlesba@gmail.com>
2025-02-07 10:56:39 +0100

    fix test

--- nucliadb/tests/nucliadb/integration/test_security.py
+++ nucliadb/tests/nucliadb/integration/test_security.py
@@ -215,9 +215,9 @@ async def _test_search_request_with_security(
         )
     elif search_endpoint == "ask_post":
         resp = await nucliadb_reader.post(
-            f"/kb/{kbid}/ask",
-            json=payload,
+            f"/kb/{kbid}/ask", json=payload, headers={"x_synchronous": True}
         )
+        print(resp.text)
     else:
         raise ValueError(f"Unknown search endpoint: {search_endpoint}")
 
