79f2a628710910cf36deff10cecd54597962fa3d
Carles Bruguera <carlesba@gmail.com>
2025-02-07 15:12:19 +0100

    debug

--- nucliadb/src/nucliadb/search/api/v1/ask.py
+++ nucliadb/src/nucliadb/search/api/v1/ask.py
@@ -68,9 +68,12 @@ async def ask_knowledgebox_endpoint(
     # security group of the payload
     if current_user.security_groups:
         if item.security is None:
+            print(f"item security={item.security}, header security={current_user.security_groups}")
             item.security = RequestSecurity(groups=current_user.security_groups)
         else:
+            print(f"item security={item.security}, header security={current_user.security_groups}")
             item.security.groups = current_user.security_groups
+    print(f"POST item security={item.security}")
     return await create_ask_response(
         kbid, item, x_nucliadb_user, x_ndb_client, x_forwarded_for, x_synchronous
     )
