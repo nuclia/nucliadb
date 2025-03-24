36e6a90f8eea31e377294e90c2f79da1bf6f58ab
Ferran Llamas <llamas.arroniz@gmail.com>
2025-03-24 13:37:11 +0100

    fix

--- nucliadb/src/nucliadb/ingest/orm/processor/processor.py
+++ nucliadb/src/nucliadb/ingest/orm/processor/processor.py
@@ -766,6 +766,13 @@ def needs_reindex(bm: writer_pb2.BrokerMessage) -> bool:
 
 
 def is_vectorset_migration_bm(bm: writer_pb2.BrokerMessage) -> bool:
+    """
+    This is a temporary solution to avoid duplicating paragraphs and text fields during vector migrations.
+    We need to reindex all the fields of a resource to avoid this issue.
+    TODO: Remove this when the index message generation logic has been decoupled into its own method.
+
+    Broker messages from semantic model migration task only contain the `field_vectors` field set.
+    """
     return (
         len(bm.field_vectors) > 0
         and not bm.HasField("basic")
