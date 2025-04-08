9bd5875519e00a849012d66bda4da09515f54ee2
Ferran Llamas <llamas.arroniz@gmail.com>
2025-04-08 17:57:51 +0200

    fix race condition in tests

--- nucliadb/tests/ingest/integration/orm/test_index_message.py
+++ nucliadb/tests/ingest/integration/orm/test_index_message.py
@@ -18,6 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
+from nucliadb.common.maindb.driver import Driver
 from nucliadb.ingest.orm.index_message import IndexMessageBuilder
 from nucliadb.ingest.orm.knowledgebox import KnowledgeBox
 from nucliadb_protos import noderesources_pb2
@@ -26,29 +27,29 @@ from tests.ingest.fixtures import create_resource
 
 
 async def test_generate_index_message_bw_compat(
-    storage, maindb_driver, cache, dummy_nidx_utility, knowledgebox_ingest: str
+    storage, maindb_driver: Driver, dummy_nidx_utility, knowledgebox_ingest: str
 ):
     full_resource = await create_resource(storage, maindb_driver, knowledgebox_ingest)
 
-    async with maindb_driver.transaction() as txn:
+    async with maindb_driver.transaction(read_only=True) as txn:
         kb_obj = KnowledgeBox(txn, storage, kbid=knowledgebox_ingest)
         resource = await kb_obj.get(full_resource.uuid)
         assert resource is not None
 
-    # Make sure that both the old and new way of generating index messages are equivalent
-    imv1 = (await resource.generate_index_message(reindex=True)).brain
-    imv2 = await IndexMessageBuilder(resource).full(reindex=True)
+        # Make sure that both the old and new way of generating index messages are equivalent
+        imv1 = (await resource.generate_index_message(reindex=True)).brain
+        imv2 = await IndexMessageBuilder(resource).full(reindex=True)
 
-    # Relation fields to remove is not fully implemented in v1, so we skip it from the comparison
-    imv1.ClearField("relation_fields_to_delete")
-    imv2.ClearField("relation_fields_to_delete")
+        # Relation fields to remove is not fully implemented in v1, so we skip it from the comparison
+        imv1.ClearField("relation_fields_to_delete")
+        imv2.ClearField("relation_fields_to_delete")
 
-    # field_relations needs special treatment because it is a map of repeated fields
-    assert flatten_field_relations(imv1) == flatten_field_relations(imv2)
-    imv1.ClearField("field_relations")
-    imv2.ClearField("field_relations")
+        # field_relations needs special treatment because it is a map of repeated fields
+        assert flatten_field_relations(imv1) == flatten_field_relations(imv2)
+        imv1.ClearField("field_relations")
+        imv2.ClearField("field_relations")
 
-    assert imv1 == imv2
+        assert imv1 == imv2
 
 
 def flatten_field_relations(r: noderesources_pb2.Resource):
@@ -76,34 +77,33 @@ def flatten_field_relations(r: noderesources_pb2.Resource):
 
 
 async def test_for_writer_bm_with_prefilter_update(
-    storage, maindb_driver, cache, dummy_nidx_utility, knowledgebox_ingest: str
+    storage, maindb_driver: Driver, dummy_nidx_utility, knowledgebox_ingest: str
 ):
     full_resource = await create_resource(storage, maindb_driver, knowledgebox_ingest)
 
-    async with maindb_driver.transaction() as txn:
+    async with maindb_driver.transaction(read_only=True) as txn:
         kb_obj = KnowledgeBox(txn, storage, kbid=knowledgebox_ingest)
         resource = await kb_obj.get(full_resource.uuid)
         assert resource is not None
+        writer_bm = BrokerMessage(
+            kbid=knowledgebox_ingest,
+            uuid=resource.uuid,
+            reindex=True,
+        )
 
-    writer_bm = BrokerMessage(
-        kbid=knowledgebox_ingest,
-        uuid=resource.uuid,
-        reindex=True,
-    )
+        im = await IndexMessageBuilder(resource).for_writer_bm([writer_bm], resource_created=False)
 
-    im = await IndexMessageBuilder(resource).for_writer_bm([writer_bm], resource_created=False)
+        # title, summary, file, link, conv and text
+        total_fields = 6
 
-    # title, summary, file, link, conv and text
-    total_fields = 6
+        # Only the texts and relations index is reindexed
+        assert len(im.texts) == total_fields
+        assert len(im.texts_to_delete) == total_fields
+        assert len(im.field_relations) == 1  # a/metadata
+        assert "a/metadata" in im.field_relations
+        assert im.relation_fields_to_delete == ["a/metadata"]
 
-    # Only the texts and relations index is reindexed
-    assert len(im.texts) == total_fields
-    assert len(im.texts_to_delete) == total_fields
-    assert len(im.field_relations) == 1  # a/metadata
-    assert "a/metadata" in im.field_relations
-    assert im.relation_fields_to_delete == ["a/metadata"]
-
-    # Others are not
-    assert len(im.paragraphs) == 0
-    assert len(im.paragraphs_to_delete) == 0
-    assert len(im.vector_prefixes_to_delete) == 0
+        # Others are not
+        assert len(im.paragraphs) == 0
+        assert len(im.paragraphs_to_delete) == 0
+        assert len(im.vector_prefixes_to_delete) == 0
