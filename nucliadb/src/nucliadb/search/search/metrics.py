9b41253b2d1c7c40b98739e9eb8353ea8ea3102c
Joan Antoni RE <joanantoni.re16@gmail.com>
2025-04-16 11:36:11 +0200

    Add metrics for query parsing

--- nucliadb/src/nucliadb/search/search/metrics.py
+++ nucliadb/src/nucliadb/search/search/metrics.py
@@ -26,6 +26,7 @@ from nucliadb_telemetry import metrics
 merge_observer = metrics.Observer("merge_results", labels={"type": ""})
 node_features = metrics.Counter("nucliadb_node_features", labels={"type": ""})
 query_parse_dependency_observer = metrics.Observer("query_parse_dependency", labels={"type": ""})
+query_parser_observer = metrics.Observer("nucliadb_query_parser", labels={"type": ""})
 
 buckets = [
     0.005,
