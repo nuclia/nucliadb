514526bea72febb788862877884b46ac49f67fe2
Ferran Llamas <llamas.arroniz@gmail.com>
2025-04-24 17:09:26 +0200

    simpler

--- nucliadb/tests/nucliadb/integration/test_vectorsets_api.py
+++ nucliadb/tests/nucliadb/integration/test_vectorsets_api.py
@@ -591,7 +591,6 @@ async def test_vectorset_migration_split_field(
         ev.vectors.split_vectors[split].vectors.append(vector)
         bm2.field_vectors.append(ev)
 
-    breakpoint()
     await inject_message(nucliadb_ingest_grpc, bm2)
 
     await wait_for_sync()
