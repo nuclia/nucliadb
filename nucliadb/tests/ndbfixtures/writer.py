b1c02573482e382196a946609dcf3309eff8e215
Ferran Llamas <llamas.arroniz@gmail.com>
2025-04-25 14:14:31 +0200

    fix tests

--- nucliadb/tests/ndbfixtures/writer.py
+++ nucliadb/tests/ndbfixtures/writer.py
@@ -104,7 +104,9 @@ async def writer_api_server(
 
 @pytest.fixture(scope="function")
 def disabled_back_pressure():
-    with patch("nucliadb.common.materializer.is_back_pressure_enabled", return_value=False) as mocked:
+    with patch(
+        "nucliadb.common.back_pressure.materializer.is_back_pressure_enabled", return_value=False
+    ) as mocked:
         yield mocked
 
 
