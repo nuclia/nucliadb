f6575c12c489f1b176ae13e3d30189c70050dbd8
Ferran Llamas <ferran@nuclia.com>
2025-05-23 13:28:20 +0200

    Debugging (#3136)

--- nucliadb/src/nucliadb/ingest/fields/base.py
+++ nucliadb/src/nucliadb/ingest/fields/base.py
@@ -20,6 +20,7 @@
 from __future__ import annotations
 
 import enum
+import logging
 from datetime import datetime
 from typing import TYPE_CHECKING, Any, Generic, Optional, Type, TypeVar
 
@@ -43,8 +44,12 @@ from nucliadb_protos.resources_pb2 import (
 )
 from nucliadb_protos.utils_pb2 import ExtractedText, VectorObject
 from nucliadb_protos.writer_pb2 import Error, FieldStatus
+from nucliadb_utils import const
 from nucliadb_utils.storages.exceptions import CouldNotCopyNotFound
 from nucliadb_utils.storages.storage import Storage, StorageField
+from nucliadb_utils.utilities import has_feature
+
+logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:  # pragma: no cover
     from nucliadb.ingest.orm.resource import Resource
@@ -206,6 +211,21 @@ class Field(Generic[PbType]):
     ) -> None:
         # Try delete vectors
         sf = self._get_extracted_vectors_storage_field(vectorset, storage_key_kind)
+
+        if has_feature(const.Features.DEBUG_MISSING_VECTORS):
+            # This is a very chatty log. It is just a temporary hint while debugging an issue.
+            logger.info(
+                "Deleting vectors from storage",
+                extra={
+                    "kbid": self.kbid,
+                    "rid": self.resource.uuid,
+                    "field": f"{self.type}/{self.id}",
+                    "vectorset": vectorset,
+                    "storage_key_kind": storage_key_kind,
+                    "key": sf.key,
+                    "bucket": sf.bucket,
+                },
+            )
         try:
             await self.storage.delete_upload(sf.key, sf.bucket)
         except KeyError:
