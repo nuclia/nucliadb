be2b9ac160dc5cb064118778c5046e394b14e1c3
Javier Torres <javier@javiertorres.eu>
2025-06-18 14:20:51 +0200

    Add serialization test

--- nucliadb_models/tests/test_common.py
+++ nucliadb_models/tests/test_common.py
@@ -29,3 +29,13 @@ def test_file_model_validator():
     # payload is mandatory
     with pytest.raises(ValidationError):
         common.File(filename="myfile")
+
+
+def test_cloud_link_serialization():
+    link = common.CloudLink(
+        uri="/kbs/1b372a0b-5ec1-4a9a-ba71-76147ca6b265/r/2c4f1db44735409eb881d2b308bce18b/f/f/text/extracted",
+    )
+    assert (
+        link.model_dump()["uri"]
+        == "/kb/1b372a0b-5ec1-4a9a-ba71-76147ca6b265/resource/2c4f1db44735409eb881d2b308bce18b/file/text/download/field/extracted"
+    )
