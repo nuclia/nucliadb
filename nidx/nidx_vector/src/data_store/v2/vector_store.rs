57e9a5787b1c4e61f19936e3402fcb6ddf48ae14
Javier Torres <javier@nuclia.com>
2025-07-11 14:29:32 +0200

    tmp

--- nidx/nidx_vector/src/data_store/v2/vector_store.rs
+++ nidx/nidx_vector/src/data_store/v2/vector_store.rs
@@ -19,6 +19,23 @@
 //
 
 use memmap2::Mmap;
+use std::{fs::File, io::Write as _, path::Path};
+
+use crate::{config::VectorType, data_store::DataStore, data_types::usize_utils::U32_LEN};
+
+const FILENAME: &str = "vectors.bin";
+
+struct StoreV2Vector<'a>(&'a [u8]);
+
+impl StoreV2Vector<'_> {
+    fn vector(&self) -> &[u8] {
+        &self.0[0..self.0.len() - U32_LEN]
+    }
+
+    // fn paragraph(&self, store: impl DataStore) -> u32 {
+    //     0
+    // }
+}
 
 /// Storage for vectors of fixed size
 /// For each vector, we store the vector and a pointer to the paragraph it appears on
@@ -26,4 +43,57 @@ use memmap2::Mmap;
 /// (encoded_vector: [u8], paragraph_addr: u32)
 struct VectorStore {
     data: Mmap,
+    vector_len: usize,
+}
+
+impl VectorStore {
+    pub fn open(path: &Path, vector_type: VectorType) -> std::io::Result<Self> {
+        let data = unsafe { Mmap::map(&File::open(path.join(FILENAME))?)? };
+
+        // TODO: Maybe different flags for read (random) and merge (sequential / willneed)
+        #[cfg(not(target_os = "windows"))]
+        {
+            data.advise(memmap2::Advice::Random)?;
+        }
+
+        Ok(Self {
+            data,
+            vector_len: vector_type.dimension().unwrap() + U32_LEN,
+        })
+    }
+
+    pub fn get_vector(&self, vector_addr: u32) -> StoreV2Vector {
+        let start = vector_addr as usize * self.vector_len;
+        StoreV2Vector(&self.data[start..start + self.vector_len])
+    }
+}
+
+pub struct VectorStoreWriter {
+    output: File,
+    addr: u32,
+}
+
+impl VectorStoreWriter {
+    pub fn new(path: &Path) -> std::io::Result<Self> {
+        Ok(Self {
+            output: File::create(path.join(FILENAME))?,
+            addr: 0,
+        })
+    }
+
+    pub fn write(&mut self, paragraph_id: u32, vectors: &[&[u8]]) -> std::io::Result<(u32, u32)> {
+        let first_addr = self.addr;
+        for v in vectors {
+            self.output.write_all(v)?;
+            self.output.write_all(paragraph_id.to_le_bytes().as_slice())?;
+            // TODO: Alignment
+        }
+        self.addr += vectors.len() as u32;
+        let last_addr = self.addr - 1;
+        Ok((first_addr, last_addr))
+    }
+
+    pub fn close(self) -> std::io::Result<()> {
+        self.output.sync_all()
+    }
 }
