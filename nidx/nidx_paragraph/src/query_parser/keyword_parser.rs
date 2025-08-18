ee0327d781e36ef8aafe975dfd1252a022966a9c
Joan Antoni RE <joanantoni.re16@gmail.com>
2025-08-18 10:17:31 +0200

    Custom keyword parser
    Reverts revert of pull request #3240
    
    This reverts commit 5d7d123338977084ac2038e3c1df5d62077266d4.

---
+++ nidx/nidx_paragraph/src/query_parser/keyword_parser.rs
@@ -0,0 +1,212 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+use tantivy::Term;
+use tantivy::query::{AllQuery, BooleanQuery, EmptyQuery, Occur, PhraseQuery, Query, TermQuery};
+use tantivy::schema::IndexRecordOption;
+use tracing::error;
+
+use crate::ParagraphSchema;
+
+use super::tokenizer::Token;
+
+/// Convert a tokenized query into a tantivy keyword query
+///
+/// Empty queries will match everything.
+pub fn parse_keyword_query<'a>(query: &'a [Token<'a>], schema: &ParagraphSchema) -> Box<dyn Query> {
+    let mut subqueries = vec![];
+    for item in query {
+        match item {
+            Token::Literal(literal) => {
+                let q = parse_literal(schema, literal);
+                subqueries.push((Occur::Should, q));
+            }
+            Token::Quoted(quoted) => {
+                let q = parse_quoted(schema, quoted);
+                subqueries.push((Occur::Should, q));
+            }
+            Token::Excluded(excluded) => {
+                let q = parse_excluded(schema, excluded);
+                subqueries.push((Occur::Should, q));
+            }
+        }
+    }
+
+    if subqueries.is_empty() {
+        Box::new(AllQuery)
+    } else if subqueries.len() == 1 {
+        subqueries.pop().unwrap().1
+    } else {
+        Box::new(BooleanQuery::new(subqueries))
+    }
+}
+
+#[inline]
+pub fn parse_literal(schema: &ParagraphSchema, literal: &str) -> Box<dyn Query> {
+    Box::new(TermQuery::new(
+        Term::from_field_text(schema.text, literal),
+        IndexRecordOption::Basic,
+    ))
+}
+
+pub fn parse_quoted(schema: &ParagraphSchema, quoted: &str) -> Box<dyn Query> {
+    let mut terms: Vec<Term> = quoted
+        .split_whitespace()
+        .map(|word| Term::from_field_text(schema.text, word))
+        .collect();
+
+    #[allow(clippy::comparison_chain)]
+    if terms.len() == 1 {
+        // phrase queries must have more than one term, so we use a term query
+        let term = terms.remove(0); // safe because terms.len() == 1
+        Box::new(TermQuery::new(term, IndexRecordOption::Basic))
+    } else if terms.len() > 1 {
+        Box::new(PhraseQuery::new(terms))
+    } else {
+        debug_assert!(
+            false,
+            "Quoted content should have been validated to not only contain whitespaces"
+        );
+        // we return a fallback to protect us from tokenizer errors, but this branch should never
+        // happen
+        error!("Keyword tokenizer build a query with a only whitespaces Quoted token!");
+        Box::new(EmptyQuery)
+    }
+}
+
+#[inline]
+pub fn parse_excluded(schema: &ParagraphSchema, excluded: &str) -> Box<dyn Query> {
+    Box::new(BooleanQuery::new(vec![
+        (Occur::Must, Box::new(AllQuery)),
+        (
+            Occur::MustNot,
+            Box::new(TermQuery::new(
+                Term::from_field_text(schema.text, excluded),
+                IndexRecordOption::Basic,
+            )),
+        ),
+    ]))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_query_is_all_query() {
+        let schema = ParagraphSchema::new();
+        let query = parse_keyword_query(&[], &schema);
+        assert!(query.is::<AllQuery>());
+    }
+
+    #[test]
+    fn test_one_clause_simplification() {
+        let schema = ParagraphSchema::new();
+
+        let query = parse_keyword_query(&[Token::Literal("nucliadb".into())], &schema);
+        let term = extract_term_from(&query);
+        assert_eq!(*term, Term::from_field_text(schema.text, "nucliadb"));
+    }
+
+    #[test]
+    fn test_tantivy_query_conversion() {
+        let schema = ParagraphSchema::new();
+
+        // nucliadb -is a "RAG database" with "superpowers"
+        let query = vec![
+            Token::Literal("nucliadb".into()),
+            Token::Excluded("is".into()),
+            Token::Literal("a".into()),
+            Token::Quoted("RAG database".into()),
+            Token::Literal("with".into()),
+            Token::Quoted("superpowers".into()),
+        ];
+        let r = downcast_boolean_query(parse_keyword_query(&query, &schema));
+        let clauses = r.clauses();
+        assert_eq!(clauses.len(), 6);
+
+        // term: nucliadb
+        assert_eq!(clauses[0].0, Occur::Should);
+        let term = extract_term_from(&clauses[0].1);
+        assert_eq!(*term, Term::from_field_text(schema.text, "nucliadb"));
+
+        // excluded term: is
+        assert_eq!(clauses[1].0, Occur::Should);
+        let subquery = clauses[1]
+            .1
+            .downcast_ref::<BooleanQuery>()
+            .expect("BooleanQuery expected");
+        let subclauses = subquery.clauses();
+        assert_eq!(subclauses.len(), 2);
+        assert_eq!(subclauses[0].0, Occur::Must);
+        assert!(subclauses[0].1.downcast_ref::<AllQuery>().is_some());
+        assert_eq!(subclauses[1].0, Occur::MustNot);
+        let term = extract_term_from(&subclauses[1].1);
+        assert_eq!(*term, Term::from_field_text(schema.text, "is"));
+
+        // term: a
+        assert_eq!(clauses[2].0, Occur::Should);
+        let term = extract_term_from(&clauses[2].1);
+        assert_eq!(*term, Term::from_field_text(schema.text, "a"));
+
+        // exact: RAG database
+        assert_eq!(clauses[3].0, Occur::Should);
+        let terms = extract_phrase_terms_from(&clauses[3].1);
+        assert_eq!(
+            *terms,
+            vec![
+                Term::from_field_text(schema.text, "RAG"),
+                Term::from_field_text(schema.text, "database"),
+            ]
+        );
+
+        // term: with
+        assert_eq!(clauses[4].0, Occur::Should);
+        let term = extract_term_from(&clauses[4].1);
+        assert_eq!(*term, Term::from_field_text(schema.text, "with"));
+
+        // exact: superpowers.
+        // An exact match of 1 word is converted into a term query
+        assert_eq!(clauses[5].0, Occur::Should);
+        let term = extract_term_from(&clauses[5].1);
+        assert_eq!(*term, Term::from_field_text(schema.text, "superpowers"));
+    }
+
+    fn downcast_boolean_query(q: Box<dyn Query>) -> Box<BooleanQuery> {
+        let q = q.downcast::<BooleanQuery>();
+        assert!(q.is_ok(), "BooleanQuery expected");
+        q.unwrap()
+    }
+
+    #[allow(clippy::borrowed_box)]
+    fn extract_term_from(query: &Box<dyn Query>) -> &Term {
+        let q = query.downcast_ref::<TermQuery>();
+        assert!(q.is_some(), "TermQuery expected");
+        let q = q.unwrap();
+        q.term()
+    }
+
+    #[allow(clippy::borrowed_box)]
+    fn extract_phrase_terms_from(query: &Box<dyn Query>) -> Vec<Term> {
+        let q = query.downcast_ref::<PhraseQuery>();
+        assert!(q.is_some(), "PhraseQuery expected");
+        let q = q.unwrap();
+        q.phrase_terms()
+    }
+}
---
+++ nidx/nidx_paragraph/src/query_parser/tokenizer.rs
@@ -0,0 +1,454 @@
+// Copyright (C) 2021 Bosutech XXI S.L.
+//
+// nucliadb is offered under the AGPL v3.0 and as commercial software.
+// For commercial licensing, contact us at info@nuclia.com.
+//
+// AGPL:
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+use std::borrow::Cow;
+
+use itertools::Itertools;
+use nom::branch::alt;
+use nom::bytes::complete::{is_a, is_not, take_while1};
+use nom::character::complete::{char, multispace0};
+use nom::combinator::{eof, map, opt};
+use nom::multi::fold_many0;
+use nom::sequence::{delimited, preceded};
+use nom::{Finish, IResult};
+use tantivy::tokenizer::{LowerCaser, SimpleTokenizer, TextAnalyzer};
+use tracing::warn;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Token<'a> {
+    Literal(Cow<'a, str>),
+    Quoted(Cow<'a, str>),
+    Excluded(Cow<'a, str>),
+}
+
+/// Tokenize a user query and return a list of lexical tokens.
+///
+/// Currently, our query language only supports:
+/// - literal terms (words)
+/// - excluded terms (using a prefix -)
+/// - exact match terms (using double quotes)
+///
+/// This is a lenient parser implementation, unclosed quotes will be removed
+/// from the tokenized result and no error will be returned.
+///
+/// Note this tokenization also applies the default tantivy tokenizer.
+/// Punctuation and other special characters will be removed from the query.
+///
+pub fn tokenize_query_infallible(input: &str) -> Vec<Token<'_>> {
+    let r = nidx_grammar_tokenize(input);
+
+    // safe to use finish. As we are using a complete parser, it'll never panic for Incomplete
+    match r.finish() {
+        Ok((rest, tokens)) => {
+            debug_assert!(rest.is_empty(), "parser should be complete and parse all input");
+
+            // after our own grammar tokenization, we pass the tantivy one to apply the same changes
+            // to the query than the ones the index applies to the text
+            tantivy_retokenize(tokens)
+        }
+        Err(error) => {
+            warn!("Parsing error for query '{}'. {:?}", input, error);
+            // Fallback to tantivy's tokenizer and parse the whole query as
+            // literals. This makes tokenization infallible at expenses of
+            // losing our grammar if the parser is incorrect.
+            tantivy_retokenize(vec![Token::Literal(Cow::Borrowed(input))])
+        }
+    }
+}
+
+fn nidx_grammar_tokenize(input: &str) -> IResult<&str, Vec<Token>, nom::error::Error<&str>> {
+    let is_literal_char = |c: char| -> bool {
+        c.is_alphanumeric()
+            || (
+                // a ~is_punctuation excluding quotes
+                !c.is_whitespace() && !c.is_control() && c != '"'
+            )
+    };
+
+    // detect alphanumeric words like: abc, 123, a1b2, a-b.3c, etc.
+    let literal = delimited(multispace0, take_while1(is_literal_char), multispace0);
+
+    // detect a quoted string: "xxx"
+    let quoted = delimited(
+        multispace0,
+        delimited(char('"'), is_not(r#"""#), char('"')),
+        multispace0,
+    );
+
+    // detect a alphanumeric string prefixed with a -: -abc123
+    let excluded = delimited(
+        multispace0,
+        preceded(char('-'), take_while1(is_literal_char)),
+        multispace0,
+    );
+
+    let unclosed_quote = is_a(r#"""#);
+
+    map(
+        delimited(
+            multispace0,
+            opt(fold_many0(
+                alt((
+                    map(quoted, |t: &str| {
+                        // our parser rule can match empty quotes, we get rid of them
+                        if t.trim().is_empty() {
+                            return None;
+                        }
+                        Some(Token::Quoted(Cow::Borrowed(t)))
+                    }),
+                    map(unclosed_quote, |_| None),
+                    map(excluded, |t| Some(Token::Excluded(Cow::Borrowed(t)))),
+                    map(literal, |t| Some(Token::Literal(Cow::Borrowed(t)))),
+                )),
+                Vec::new,
+                |mut acc, item| {
+                    if let Some(item) = item {
+                        acc.push(item);
+                    }
+                    acc
+                },
+            )),
+            eof,
+        ),
+        |t| {
+            // Return an empty list of tokens if query is empty (or only contains spaces)
+            t.unwrap_or_default()
+        },
+    )(input)
+}
+
+/// Parse a stream of tokens and retokenize them using the tantivy tokenizer.
+///
+/// This is done after our own grammar tokenization. We want to pass tantivy's
+/// tokenizer to remove puntuation and apply the same process the index is
+/// applying to the `text` field.
+///
+/// NOTE this depends on how the `text` field is being indexed and tokenized.
+/// Any change there should be reflected here too.
+fn tantivy_retokenize(tokens: Vec<Token>) -> Vec<Token> {
+    let mut tantivy_tokenizer = TextAnalyzer::builder(SimpleTokenizer::default())
+        .filter(LowerCaser)
+        .build();
+
+    tokens
+        .into_iter()
+        .flat_map(|token| match token {
+            Token::Literal(Cow::Borrowed(lit)) => {
+                let mut stream = tantivy_tokenizer.token_stream(lit);
+                let mut tokens = vec![];
+                while stream.advance() {
+                    let token = stream.token();
+                    tokens.push(Token::Literal(Cow::Owned(token.text.clone())));
+                }
+                tokens
+            }
+            Token::Quoted(Cow::Borrowed(quoted)) => {
+                let mut stream = tantivy_tokenizer.token_stream(quoted);
+                let mut tokens = vec![];
+                while stream.advance() {
+                    let token = stream.token();
+                    tokens.push(token.text.clone())
+                }
+                vec![Token::Quoted(Cow::Owned(tokens.into_iter().join(" ")))]
+            }
+            Token::Excluded(Cow::Borrowed(excluded)) => {
+                let mut stream = tantivy_tokenizer.token_stream(excluded);
+                let mut tokens = vec![];
+                while stream.advance() {
+                    let token = stream.token();
+                    tokens.push(Token::Excluded(Cow::Owned(token.text.clone())));
+                }
+                tokens
+            }
+            Token::Literal(Cow::Owned(_)) | Token::Quoted(Cow::Owned(_)) | Token::Excluded(Cow::Owned(_)) => {
+                unreachable!("nom parser only returns borrowed strings")
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use Token::*;
+
+    #[test]
+    fn test_empty_query() {
+        let expected = vec![];
+
+        let query = "";
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // whitespaces only
+        let query = "    ";
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // dashes and whitespaces only
+        let query = "  - - -   - - -  ";
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // quoted whitespaces
+        let query = r#""  ""#;
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // only special characters (punctuation is removed)
+        let query = "!@#~&/()=?";
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_simple_query() {
+        let query = "This is a simple query";
+        let expected = vec![
+            Literal(Cow::Borrowed("this")),
+            Literal(Cow::Borrowed("is")),
+            Literal(Cow::Borrowed("a")),
+            Literal(Cow::Borrowed("simple")),
+            Literal(Cow::Borrowed("query")),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_special_characters() {
+        let query = "This-is?a_less+simple*query";
+        let expected = vec![
+            Literal("this".into()),
+            Literal("is".into()),
+            Literal("a".into()),
+            Literal("less".into()),
+            Literal("simple".into()),
+            Literal("query".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_excluded_words() {
+        let query = "This is an -excluded word";
+        let expected = vec![
+            Literal("this".into()),
+            Literal("is".into()),
+            Literal("an".into()),
+            Excluded("excluded".into()),
+            Literal("word".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = "-Everything -is -excluded";
+        let expected = vec![
+            Excluded("everything".into()),
+            Excluded("is".into()),
+            Excluded("excluded".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // We don't handle double exclude, it is considered as exclude `-Not` and the second dash is
+        // removed by the tantivy tokenizer
+        let query = "--Not ---everything -is -excluded";
+        let expected = vec![
+            Excluded("not".into()),
+            Excluded("everything".into()),
+            Excluded("is".into()),
+            Excluded("excluded".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // hyphenated words. Due to tantivy tokenization, we remove the minus (-) and split the term
+        // in two literals
+        let query = "hyphenated-word";
+        let expected = vec![Literal("hyphenated".into()), Literal("word".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_quoted_strings() {
+        let query = r#""quoted""#;
+        let expected = vec![Quoted("quoted".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = r#"This is "really important""#;
+        let expected = vec![
+            Literal("this".into()),
+            Literal("is".into()),
+            Quoted("really important".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = r#"half "quoted string"#;
+        let expected = vec![
+            Literal("half".into()),
+            Literal("quoted".into()),
+            Literal("string".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = r#"half" quoted string"#;
+        let expected = vec![
+            Literal("half".into()),
+            Literal("quoted".into()),
+            Literal("string".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = r#"half q"uoted string"#;
+        let expected = vec![
+            Literal("half".into()),
+            Literal("q".into()),
+            Literal("uoted".into()),
+            Literal("string".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_excluded_inside_quotes() {
+        // quotes take preference over excluding words
+        let query = r#"This is "really -important""#;
+        let expected = vec![
+            Literal("this".into()),
+            Literal("is".into()),
+            // here we have removed the minus (-) due to tantivy tokenization
+            Quoted("really important".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // without separation between excluded and quotes
+        let query = r#""quoted"-term"#;
+        let expected = vec![Quoted("quoted".into()), Excluded("term".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        let query = r#"-term"quoted""#;
+        let expected = vec![Excluded("term".into()), Quoted("quoted".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_unicode_characters_and_language_support() {
+        let query = r#"résumé "São Paulo" -tésting"#;
+        let expected = vec![
+            Literal("résumé".into()),
+            Quoted("são paulo".into()),
+            Excluded("tésting".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // japanese chars are not splitted, this may lead to wrong results. We may want to support
+        // other tokenizers to consider different languages
+        let query = "クジラはすごい！";
+        let expected = vec![Literal("クジラはすごい".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // As arabic has spaces, at least we split them
+        let query = r#"الحيتان رائعة"#;
+        let expected = vec![Literal("الحيتان".into()), Literal("رائعة".into())];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_complex_combinations() {
+        let query = r#"This is "really" "important stuff" -except for this"#;
+        let expected = vec![
+            Literal("this".into()),
+            Literal("is".into()),
+            Quoted("really".into()),
+            Quoted("important stuff".into()),
+            Excluded("except".into()),
+            Literal("for".into()),
+            Literal("this".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // mixed whitespaces in different positions
+        let query = r#"   -first "second    part"   third -fourth   "#;
+        let expected = vec![
+            Excluded("first".into()),
+            Quoted("second part".into()),
+            Literal("third".into()),
+            Excluded("fourth".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // The following are more (potentially) controversial combination that may lead to
+        // unexpected results. Any of these is subject to change in the future if we implement more
+        // complex parsing (e.g. escaping support)
+
+        // quoted string with escaped quotes
+        let query = r#"Text with "escaped \"quote\" inside""#;
+        let expected = vec![
+            Literal("text".into()),
+            Literal("with".into()),
+            // handling escaped quotes would quote these 3 terms together
+            Quoted("escaped".into()),
+            Literal("quote".into()),
+            Quoted("inside".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+
+        // Mix punctuation with dashes and quotes. In this case, "WORLD"!-minus this is considered a
+        // literal because tantivy tokenization is done *after* our grammar tokenization. So we
+        // detect the `!-minus` and tantivy removed both punctuation chars
+        let query = r#"Hello, "WORLD"!-minus -this!"#;
+        let expected = vec![
+            Literal("hello".into()),
+            Quoted("world".into()),
+            Literal("minus".into()),
+            Excluded("this".into()),
+        ];
+        let tokens = tokenize_query_infallible(query);
+        assert_eq!(tokens, expected);
+        assert_eq!(
+            nidx_grammar_tokenize(r#""WORLD"!-minus"#),
+            Ok(("", vec![Quoted("WORLD".into()), Literal("!-minus".into())]))
+        );
+    }
+
+    #[test]
+    fn test_tantivy_retokenization() {
+        let query = vec![Literal("Title".into()), Literal("No-DaShEs".into())];
+        let expected = vec![Literal("title".into()), Literal("no".into()), Literal("dashes".into())];
+        let retokenized = tantivy_retokenize(query);
+        assert_eq!(retokenized, expected)
+    }
+}
