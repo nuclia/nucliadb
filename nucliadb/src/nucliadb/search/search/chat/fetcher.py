5f5374475c256a3b55ced5e80917f4f6eb16d3d4
Ferran Llamas <ferran@nuclia.com>
2025-11-17 18:48:17 +0100

    fetcher and parser for ask

---
+++ nucliadb/src/nucliadb/search/search/chat/fetcher.py
@@ -0,0 +1,380 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import asyncio
+from typing import Optional
+
+from async_lru import alru_cache
+
+from nucliadb.common import datamanagers
+from nucliadb.common.exceptions import InvalidQueryError
+from nucliadb.common.maindb.utils import get_driver
+from nucliadb.search import logger
+from nucliadb.search.predict import (
+    SendToPredictError,
+    convert_relations,
+)
+from nucliadb.search.predict_models import QueryModel
+from nucliadb.search.search.query_parser.fetcher import Fetcher, FetcherCache, is_cached
+from nucliadb.search.utilities import get_predict
+from nucliadb_models.internal.predict import QueryInfo
+from nucliadb_models.search import (
+    Image,
+    MaxTokens,
+)
+from nucliadb_protos import knowledgebox_pb2, utils_pb2
+
+
+class RAOFetcher(Fetcher):
+    """Queries are getting more and more complex and different phases of the
+    query depend on different data, not only from the user but from other parts
+    of the system.
+
+    This class is an encapsulation of data gathering across different parts of
+    the system. Given the user query input, it aims to be as efficient as
+    possible removing redundant expensive calls to other parts of the system. An
+    instance of a fetcher caches it's results and it's thought to be used in the
+    context of a single request. DO NOT use this as a global object!
+    """
+
+    def __init__(
+        self,
+        kbid: str,
+        *,
+        query: str,
+        user_vector: Optional[list[float]],
+        vectorset: Optional[str],
+        rephrase: bool,
+        rephrase_prompt: Optional[str],
+        generative_model: Optional[str],
+        query_image: Optional[Image],
+    ):
+        self.kbid = kbid
+        self.query = query
+        self.user_vector = user_vector
+        self.user_vectorset = vectorset
+        self.user_vectorset_validated = False
+        self.rephrase = rephrase
+        self.rephrase_prompt = rephrase_prompt
+        self.generative_model = generative_model
+        self.query_image = query_image
+
+        self.cache = FetcherCache()
+        self.locks: dict[str, asyncio.Lock] = {}
+
+    # Semantic search
+
+    async def get_matryoshka_dimension(self) -> Optional[int]:
+        vectorset = await self.get_vectorset()
+        return await get_matryoshka_dimension_cached(self.kbid, vectorset)
+
+    async def _get_user_vectorset(self) -> Optional[str]:
+        """Returns the user's requested vectorset and validates if it does exist
+        in the KB.
+
+        """
+        async with self.locks.setdefault("user_vectorset", asyncio.Lock()):
+            if not self.user_vectorset_validated:
+                if self.user_vectorset is not None:
+                    await self.validate_vectorset(self.kbid, self.user_vectorset)
+            self.user_vectorset_validated = True
+            return self.user_vectorset
+
+    async def get_vectorset(self) -> str:
+        """Get the vectorset to be used in the search. If not specified, by the
+        user, Predict API or the own uses KB will provide a default.
+
+        """
+        async with self.locks.setdefault("vectorset", asyncio.Lock()):
+            if is_cached(self.cache.vectorset):
+                return self.cache.vectorset
+
+            user_vectorset = await self._get_user_vectorset()
+            if user_vectorset:
+                # user explicitly asked for a vectorset
+                self.cache.vectorset = user_vectorset
+                return user_vectorset
+
+            # when it's not provided, we get the default from Predict API
+            query_info = await self._predict_query_endpoint()
+            if query_info is None:
+                vectorset = None
+            else:
+                if query_info.sentence is None:
+                    logger.error(
+                        "Asking for a vectorset but /query didn't return one", extra={"kbid": self.kbid}
+                    )
+                    vectorset = None
+                else:
+                    # vectors field is enforced by the data model to have at least one key
+                    for vectorset in query_info.sentence.vectors.keys():
+                        vectorset = vectorset
+                        break
+
+            if vectorset is None:
+                # in case predict don't answer which vectorset to use, fallback to
+                # the first vectorset of the KB
+                async with datamanagers.with_ro_transaction() as txn:
+                    async for vectorset, _ in datamanagers.vectorsets.iter(txn, kbid=self.kbid):
+                        break
+                assert vectorset is not None, "All KBs must have at least one vectorset in maindb"
+
+            self.cache.vectorset = vectorset
+            return vectorset
+
+    async def get_query_vector(self) -> Optional[list[float]]:
+        if self.user_vector is not None:
+            query_vector = self.user_vector
+        else:
+            query_info = await self._predict_query_endpoint()
+            if query_info is None or query_info.sentence is None:
+                return None
+
+            vectorset = await self.get_vectorset()
+            if vectorset not in query_info.sentence.vectors:
+                logger.warning(
+                    "Predict is not responding with a valid query nucliadb vectorset",
+                    extra={
+                        "kbid": self.kbid,
+                        "vectorset": vectorset,
+                        "predict_vectorsets": ",".join(query_info.sentence.vectors.keys()),
+                    },
+                )
+                return None
+
+            query_vector = query_info.sentence.vectors[vectorset]
+
+        matryoshka_dimension = await self.get_matryoshka_dimension()
+        if matryoshka_dimension is not None:
+            if self.user_vector is not None and len(query_vector) < matryoshka_dimension:
+                raise InvalidQueryError(
+                    "vector",
+                    f"Invalid vector length, please check valid embedding size for {vectorset} model",
+                )
+
+            # KB using a matryoshka embeddings model, cut the query vector
+            # accordingly
+            query_vector = query_vector[:matryoshka_dimension]
+
+        return query_vector
+
+    async def get_rephrased_query(self) -> Optional[str]:
+        query_info = await self._predict_query_endpoint()
+        if query_info is None:
+            return None
+        return query_info.rephrased_query
+
+    def get_cached_rephrased_query(self) -> Optional[str]:
+        if not is_cached(self.cache.predict_query_info):
+            return None
+        if self.cache.predict_query_info is None:
+            return None
+        return self.cache.predict_query_info.rephrased_query
+
+    async def get_semantic_min_score(self) -> Optional[float]:
+        query_info = await self._predict_query_endpoint()
+        if query_info is None:
+            return None
+
+        vectorset = await self.get_vectorset()
+        min_score = query_info.semantic_thresholds.get(vectorset, None)
+        return min_score
+
+    # Labels
+
+    async def get_classification_labels(self) -> knowledgebox_pb2.Labels:
+        async with self.locks.setdefault("classification_labels", asyncio.Lock()):
+            if is_cached(self.cache.labels):
+                return self.cache.labels
+
+            labels = await get_classification_labels(self.kbid)
+            self.cache.labels = labels
+            return labels
+
+    # Entities
+
+    async def get_detected_entities(self) -> list[utils_pb2.RelationNode]:
+        async with self.locks.setdefault("detected_entities", asyncio.Lock()):
+            if is_cached(self.cache.detected_entities):
+                return self.cache.detected_entities
+
+            # Optimization to avoid calling predict twice
+            if is_cached(self.cache.predict_query_info):
+                # /query supersets detect entities, so we already have them
+                query_info = self.cache.predict_query_info
+                if query_info is not None and query_info.entities is not None:
+                    detected_entities = convert_relations(query_info.entities.model_dump())
+                else:
+                    detected_entities = []
+            else:
+                # No call to /query has been done, we'll use detect entities
+                # endpoint instead (as it's faster)
+                detected_entities = await self._predict_detect_entities()
+
+            self.cache.detected_entities = detected_entities
+            return detected_entities
+
+    # Synonyms
+
+    async def get_synonyms(self) -> Optional[knowledgebox_pb2.Synonyms]:
+        async with self.locks.setdefault("synonyms", asyncio.Lock()):
+            if is_cached(self.cache.synonyms):
+                return self.cache.synonyms
+
+            synonyms = await get_kb_synonyms(self.kbid)
+            self.cache.synonyms = synonyms
+            return synonyms
+
+    # Generative
+
+    async def get_visual_llm_enabled(self) -> bool:
+        query_info = await self._predict_query_endpoint()
+        if query_info is None:
+            raise SendToPredictError("Error while using predict's query endpoint")
+
+        return query_info.visual_llm
+
+    async def get_max_context_tokens(self, max_tokens: Optional[MaxTokens]) -> int:
+        query_info = await self._predict_query_endpoint()
+        if query_info is None:
+            raise SendToPredictError("Error while using predict's query endpoint")
+
+        model_max = query_info.max_context
+        if max_tokens is not None and max_tokens.context is not None:
+            if max_tokens.context > model_max:
+                raise InvalidQueryError(
+                    "max_tokens.context",
+                    f"Max context tokens is higher than the model's limit of {model_max}",
+                )
+            return max_tokens.context
+        return model_max
+
+    def get_max_answer_tokens(self, max_tokens: Optional[MaxTokens]) -> Optional[int]:
+        if max_tokens is not None and max_tokens.answer is not None:
+            return max_tokens.answer
+        return None
+
+    # Predict API
+
+    async def _predict_query_endpoint(self) -> Optional[QueryInfo]:
+        async with self.locks.setdefault("predict_query_endpoint", asyncio.Lock()):
+            if is_cached(self.cache.predict_query_info):
+                return self.cache.predict_query_info
+
+            # we can't call get_vectorset, as it would do a recirsive loop between
+            # functions, so we'll manually parse it
+            vectorset = await self._get_user_vectorset()
+            try:
+                query_info = await query_information(
+                    self.kbid,
+                    self.query,
+                    vectorset,
+                    self.generative_model,
+                    self.rephrase,
+                    self.rephrase_prompt,
+                    self.query_image,
+                )
+            except (SendToPredictError, TimeoutError):
+                query_info = None
+
+            self.cache.predict_query_info = query_info
+            return query_info
+
+    async def _predict_detect_entities(self) -> list[utils_pb2.RelationNode]:
+        try:
+            detected_entities = await detect_entities(self.kbid, self.query)
+        except (SendToPredictError, TimeoutError) as ex:
+            logger.warning(f"Errors on Predict API detecting entities: {ex}", extra={"kbid": self.kbid})
+            detected_entities = []
+
+        return detected_entities
+
+    async def validate_vectorset(self, kbid: str, vectorset: str):
+        async with datamanagers.with_ro_transaction() as txn:
+            if not await datamanagers.vectorsets.exists(txn, kbid=kbid, vectorset_id=vectorset):
+                raise InvalidQueryError(
+                    "vectorset", f"Vectorset {vectorset} doesn't exist in you Knowledge Box"
+                )
+
+
+async def query_information(
+    kbid: str,
+    query: str,
+    semantic_model: Optional[str],
+    generative_model: Optional[str] = None,
+    rephrase: bool = False,
+    rephrase_prompt: Optional[str] = None,
+    query_image: Optional[Image] = None,
+) -> QueryInfo:
+    # NOTE: When moving /ask to RAO, this will need to change to whatever client/utility is used
+    # to call NUA predict (internally or externally in the case of onprem).
+    predict = get_predict()
+    item = QueryModel(
+        text=query,
+        semantic_models=[semantic_model] if semantic_model else None,
+        generative_model=generative_model,
+        rephrase=rephrase,
+        rephrase_prompt=rephrase_prompt,
+        query_image=query_image,
+    )
+    return await predict.query(kbid, item)
+
+
+async def detect_entities(kbid: str, query: str) -> list[utils_pb2.RelationNode]:
+    # NOTE: When moving /ask to RAO, this will need to change to whatever client/utility is used
+    # to call NUA predict (internally or externally in the case of onprem).
+    predict = get_predict()
+    return await predict.detect_entities(kbid, query)
+
+
+@alru_cache(maxsize=None)
+async def get_matryoshka_dimension_cached(kbid: str, vectorset: str) -> Optional[int]:
+    # This can be safely cached as the matryoshka dimension is not expected to change
+    return await get_matryoshka_dimension(kbid, vectorset)
+
+
+async def get_matryoshka_dimension(kbid: str, vectorset: Optional[str]) -> Optional[int]:
+    # NOTE: When moving /ask to RAO, this will need to change to some http endpoint where we can
+    # get the vectorset configuration.
+    async with get_driver().ro_transaction() as txn:
+        matryoshka_dimension = None
+        if not vectorset:
+            # XXX this should be migrated once we remove the "default" vectorset
+            # concept
+            matryoshka_dimension = await datamanagers.kb.get_matryoshka_vector_dimension(txn, kbid=kbid)
+        else:
+            vectorset_config = await datamanagers.vectorsets.get(txn, kbid=kbid, vectorset_id=vectorset)
+            if vectorset_config is not None and vectorset_config.vectorset_index_config.vector_dimension:
+                matryoshka_dimension = vectorset_config.vectorset_index_config.vector_dimension
+
+        return matryoshka_dimension
+
+
+async def get_classification_labels(kbid: str) -> knowledgebox_pb2.Labels:
+    # NOTE: When moving /ask to RAO, this will need to change to some http endpoint where we can
+    # get the classification labels of the kb.
+    async with get_driver().ro_transaction() as txn:
+        return await datamanagers.labels.get_labels(txn, kbid=kbid)
+
+
+async def get_kb_synonyms(kbid: str) -> Optional[knowledgebox_pb2.Synonyms]:
+    # NOTE: When moving /ask to RAO, this will need to change to some http endpoint where we can
+    # get the kb synonyms.
+    async with get_driver().ro_transaction() as txn:
+        return await datamanagers.synonyms.get(txn, kbid=kbid)
---
+++ nucliadb/src/nucliadb/search/search/chat/parser.py
@@ -0,0 +1,286 @@
+# Copyright (C) 2021 Bosutech XXI S.L.
+#
+# nucliadb is offered under the AGPL v3.0 and as commercial software.
+# For commercial licensing, contact us at info@nuclia.com.
+#
+# AGPL:
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+from typing import Optional
+
+from nidx_protos import nodereader_pb2
+from pydantic import ValidationError
+
+from nucliadb.common.exceptions import InvalidQueryError
+from nucliadb.common.filter_expression import parse_expression
+from nucliadb.common.models_utils.from_proto import RelationNodeTypeMap
+from nucliadb.search.search.query import expand_entities
+from nucliadb.search.search.query_parser.exceptions import InternalParserError
+from nucliadb.search.search.query_parser.fetcher import Fetcher
+from nucliadb.search.search.query_parser.models import (
+    Filters,
+    GraphQuery,
+    NoopReranker,
+    PredictReranker,
+    Query,
+    RankFusion,
+    ReciprocalRankFusion,
+    RelationQuery,
+    Reranker,
+    UnitRetrieval,
+)
+from nucliadb.search.search.query_parser.old_filters import OldFilterParams, parse_old_filters
+from nucliadb.search.search.query_parser.parsers.common import (
+    parse_keyword_query,
+    parse_semantic_query,
+    should_disable_vector_search,
+)
+from nucliadb.search.search.utils import filter_hidden_resources
+from nucliadb_models import search as search_models
+from nucliadb_models.filters import FilterExpression
+from nucliadb_models.search import (
+    FindRequest,
+)
+from nucliadb_protos import utils_pb2
+
+
+class RAOQueryParser:
+    def __init__(self, kbid: str, item: FindRequest, fetcher: Fetcher):
+        self.kbid = kbid
+        self.item = item
+        self.fetcher = fetcher
+
+        # cached data while parsing
+        self._query: Optional[Query] = None
+        self._top_k: Optional[int] = None
+
+    async def parse(self) -> UnitRetrieval:
+        self._validate_request()
+
+        self._top_k = self.item.top_k
+
+        # parse search types (features)
+
+        self._query = Query()
+
+        if search_models.FindOptions.KEYWORD in self.item.features:
+            self._query.keyword = await parse_keyword_query(self.item, fetcher=self.fetcher)
+
+        if search_models.FindOptions.SEMANTIC in self.item.features:
+            self._query.semantic = await parse_semantic_query(self.item, fetcher=self.fetcher)
+
+        if search_models.FindOptions.RELATIONS in self.item.features:
+            self._query.relation = await self._parse_relation_query()
+
+        if search_models.FindOptions.GRAPH in self.item.features:
+            self._query.graph = await self._parse_graph_query()
+
+        filters = await self._parse_filters()
+
+        try:
+            rank_fusion = self._parse_rank_fusion()
+        except ValidationError as exc:
+            raise InternalParserError(f"Parsing error in rank fusion: {str(exc)}") from exc
+        try:
+            reranker = self._parse_reranker()
+        except ValidationError as exc:
+            raise InternalParserError(f"Parsing error in reranker: {str(exc)}") from exc
+
+        # Adjust retrieval windows. Our current implementation assume:
+        # `top_k <= reranker.window <= rank_fusion.window`
+        # and as rank fusion is done before reranking, we must ensure rank
+        # fusion window is at least, the reranker window
+        if isinstance(reranker, PredictReranker):
+            rank_fusion.window = max(rank_fusion.window, reranker.window)
+
+        retrieval = UnitRetrieval(
+            query=self._query,
+            top_k=self._top_k,
+            filters=filters,
+            rank_fusion=rank_fusion,
+            reranker=reranker,
+        )
+        return retrieval
+
+    def _validate_request(self):
+        # synonyms are not compatible with vector/graph search
+        if (
+            self.item.with_synonyms
+            and self.item.query
+            and (
+                search_models.FindOptions.SEMANTIC in self.item.features
+                or search_models.FindOptions.RELATIONS in self.item.features
+                or search_models.FindOptions.GRAPH in self.item.features
+            )
+        ):
+            raise InvalidQueryError(
+                "synonyms",
+                "Search with custom synonyms is only supported on paragraph and document search",
+            )
+
+        if search_models.FindOptions.SEMANTIC in self.item.features:
+            if should_disable_vector_search(self.item):
+                self.item.features.remove(search_models.FindOptions.SEMANTIC)
+
+        if self.item.graph_query and search_models.FindOptions.GRAPH not in self.item.features:
+            raise InvalidQueryError("graph_query", "Using a graph query requires enabling graph feature")
+
+    async def _parse_relation_query(self) -> RelationQuery:
+        detected_entities = await self._get_detected_entities()
+
+        deleted_entity_groups = await self.fetcher.get_deleted_entity_groups()
+
+        meta_cache = await self.fetcher.get_entities_meta_cache()
+        deleted_entities = meta_cache.deleted_entities
+
+        return RelationQuery(
+            entry_points=detected_entities,
+            deleted_entity_groups=deleted_entity_groups,
+            deleted_entities=deleted_entities,
+        )
+
+    async def _parse_graph_query(self) -> GraphQuery:
+        if self.item.graph_query is None:
+            raise InvalidQueryError(
+                "graph_query", "Graph query must be provided when using graph search"
+            )
+        return GraphQuery(query=self.item.graph_query)
+
+    async def _get_detected_entities(self) -> list[utils_pb2.RelationNode]:
+        """Get entities from request, either automatically detected or
+        explicitly set by the user."""
+
+        if self.item.query_entities:
+            detected_entities = []
+            for entity in self.item.query_entities:
+                relation_node = utils_pb2.RelationNode()
+                relation_node.value = entity.name
+                if entity.type is not None:
+                    relation_node.ntype = RelationNodeTypeMap[entity.type]
+                if entity.subtype is not None:
+                    relation_node.subtype = entity.subtype
+                detected_entities.append(relation_node)
+        else:
+            detected_entities = await self.fetcher.get_detected_entities()
+
+        meta_cache = await self.fetcher.get_entities_meta_cache()
+        detected_entities = expand_entities(meta_cache, detected_entities)
+
+        return detected_entities
+
+    async def _parse_filters(self) -> Filters:
+        assert self._query is not None, "query must be parsed before filters"
+
+        has_old_filters = (
+            len(self.item.filters) > 0
+            or len(self.item.resource_filters) > 0
+            or len(self.item.fields) > 0
+            or len(self.item.keyword_filters) > 0
+            or self.item.range_creation_start is not None
+            or self.item.range_creation_end is not None
+            or self.item.range_modification_start is not None
+            or self.item.range_modification_end is not None
+        )
+        if self.item.filter_expression is not None and has_old_filters:
+            raise InvalidQueryError("filter_expression", "Cannot mix old filters with filter_expression")
+
+        field_expr = None
+        paragraph_expr = None
+        filter_operator = nodereader_pb2.FilterOperator.AND
+
+        if has_old_filters:
+            old_filters = OldFilterParams(
+                label_filters=self.item.filters,
+                keyword_filters=self.item.keyword_filters,
+                range_creation_start=self.item.range_creation_start,
+                range_creation_end=self.item.range_creation_end,
+                range_modification_start=self.item.range_modification_start,
+                range_modification_end=self.item.range_modification_end,
+                fields=self.item.fields,
+                key_filters=self.item.resource_filters,
+            )
+            field_expr, paragraph_expr = await parse_old_filters(old_filters, self.fetcher)
+
+        if self.item.filter_expression is not None:
+            if self.item.filter_expression.field:
+                field_expr = await parse_expression(self.item.filter_expression.field, self.kbid)
+            if self.item.filter_expression.paragraph:
+                paragraph_expr = await parse_expression(self.item.filter_expression.paragraph, self.kbid)
+            if self.item.filter_expression.operator == FilterExpression.Operator.OR:
+                filter_operator = nodereader_pb2.FilterOperator.OR
+            else:
+                filter_operator = nodereader_pb2.FilterOperator.AND
+
+        hidden = await filter_hidden_resources(self.kbid, self.item.show_hidden)
+
+        return Filters(
+            facets=[],
+            field_expression=field_expr,
+            paragraph_expression=paragraph_expr,
+            filter_expression_operator=filter_operator,
+            security=self.item.security,
+            hidden=hidden,
+            with_duplicates=self.item.with_duplicates,
+        )
+
+    def _parse_rank_fusion(self) -> RankFusion:
+        rank_fusion: RankFusion
+
+        top_k = self.item.top_k
+        window = min(top_k, 500)
+
+        if isinstance(self.item.rank_fusion, search_models.RankFusionName):
+            if self.item.rank_fusion == search_models.RankFusionName.RECIPROCAL_RANK_FUSION:
+                rank_fusion = ReciprocalRankFusion(window=window)
+            else:
+                raise InternalParserError(f"Unknown rank fusion algorithm: {self.item.rank_fusion}")
+
+        elif isinstance(self.item.rank_fusion, search_models.ReciprocalRankFusion):
+            user_window = self.item.rank_fusion.window
+            rank_fusion = ReciprocalRankFusion(
+                k=self.item.rank_fusion.k,
+                boosting=self.item.rank_fusion.boosting,
+                window=min(max(user_window or 0, top_k), 500),
+            )
+
+        else:
+            raise InternalParserError(f"Unknown rank fusion {self.item.rank_fusion}")
+
+        return rank_fusion
+
+    def _parse_reranker(self) -> Reranker:
+        reranking: Reranker
+
+        top_k = self.item.top_k
+
+        if isinstance(self.item.reranker, search_models.RerankerName):
+            if self.item.reranker == search_models.RerankerName.NOOP:
+                reranking = NoopReranker()
+
+            elif self.item.reranker == search_models.RerankerName.PREDICT_RERANKER:
+                # for predict rearnker, by default, we want a x2 factor with a
+                # top of 200 results
+                reranking = PredictReranker(window=min(top_k * 2, 200))
+
+            else:
+                raise InternalParserError(f"Unknown reranker algorithm: {self.item.reranker}")
+
+        elif isinstance(self.item.reranker, search_models.PredictReranker):
+            user_window = self.item.reranker.window
+            reranking = PredictReranker(window=min(max(user_window or 0, top_k), 200))
+
+        else:
+            raise InternalParserError(f"Unknown reranker {self.item.reranker}")
+
+        return reranking
