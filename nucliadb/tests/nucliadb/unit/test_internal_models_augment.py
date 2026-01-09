a5c37f9159a20ef75e6df37bd581fb93257a4fdd
Joan Antoni RE <joanantoni.re16@gmail.com>
2026-01-09 16:20:24 +0100

    Hacky test to validate prop deduplicator assumptions

--- nucliadb/tests/nucliadb/unit/test_internal_models_augment.py
+++ nucliadb/tests/nucliadb/unit/test_internal_models_augment.py
@@ -19,8 +19,13 @@
 #
 from uuid import uuid4
 
+from typing_extensions import assert_never
+
 from nucliadb.common.ids import FieldId, ParagraphId
 from nucliadb.models.internal import augment
+from nucliadb.search.augmentor.fields import dedup_field_select
+from nucliadb.search.augmentor.paragraphs import dedup_paragraph_select
+from nucliadb.search.augmentor.resources import dedup_resource_select
 
 
 def test_augment():
@@ -235,3 +240,132 @@ def paragraph_from_id(id: str) -> augment.Paragraph:
         id=ParagraphId.from_string(id),
         metadata=None,
     )
+
+
+def test_paragraph_prop_deduplicator_assumptions() -> None:
+    """In order to deduplicate props, augmentor assumes some have no other field
+    than it's own `prop` literal. However, this might change.
+
+    This test ensures the assumptions taken by the deduplicators are correct.
+
+    """
+    prop: augment.ParagraphProp
+    paragraph_props: list[tuple[augment.ParagraphProp, set[str] | None]] = [
+        (augment.ParagraphText(), None),
+        (augment.ParagraphPosition(), None),
+        (augment.ParagraphImage(), None),
+    ]
+    prop = augment.ParagraphTable()
+    paragraph_props.append(
+        (
+            prop,
+            {f"{prop.prefer_page_preview =}".split()[0].removeprefix("prop.")},
+        )
+    )
+    prop = augment.ParagraphPage()
+    paragraph_props.append(
+        (
+            prop,
+            {f"{prop.preview =}".split()[0].removeprefix("prop.")},
+        )
+    )
+    prop = augment.RelatedParagraphs(neighbours_before=0, neighbours_after=0)
+    paragraph_props.append(
+        (
+            prop,
+            {
+                f"{prop.neighbours_before =}".split()[0].removeprefix("prop."),
+                f"{prop.neighbours_after =}".split()[0].removeprefix("prop."),
+            },
+        )
+    )
+
+    for prop, field_names in paragraph_props:
+        if (
+            isinstance(prop, augment.ParagraphText)
+            or isinstance(prop, augment.ParagraphPosition)
+            or isinstance(prop, augment.ParagraphImage)
+            or isinstance(prop, augment.ParagraphTable)
+            or isinstance(prop, augment.ParagraphPage)
+            or isinstance(prop, augment.RelatedParagraphs)
+        ):
+            if not field_names:
+                assert prop.__class__.model_fields.keys() == {"prop"}
+            else:
+                assert prop.__class__.model_fields.keys() == {"prop"} | field_names
+        else:
+            assert_never(prop)
+
+    props = [prop for prop, _ in paragraph_props]
+    assert dedup_paragraph_select([*props, *props]) == props
+
+
+def test_field_prop_deduplicator_assumptions() -> None:
+    field_props: list[augment.FieldProp | augment.FileProp | augment.ConversationProp] = [
+        augment.FieldText(),
+        augment.FieldValue(),
+        augment.FieldClassificationLabels(),
+        augment.FieldEntities(),
+        augment.FileThumbnail(),
+        augment.ConversationAnswerOrAfter(),
+        augment.ConversationText(selector=augment.MessageSelector()),
+        augment.ConversationAttachments(selector=augment.MessageSelector()),
+    ]
+
+    for prop in field_props:
+        if isinstance(prop, augment.ConversationText) or isinstance(
+            prop, augment.ConversationAttachments
+        ):
+            assert prop.__class__.model_fields.keys() == {
+                "prop",
+                f"{prop.selector =}".split()[0].removeprefix("prop."),
+            }
+
+        elif (
+            isinstance(prop, augment.FieldText)
+            or isinstance(prop, augment.FieldValue)
+            or isinstance(prop, augment.FieldClassificationLabels)
+            or isinstance(prop, augment.FieldEntities)
+            or isinstance(prop, augment.FileThumbnail)
+            or isinstance(prop, augment.ConversationAnswerOrAfter)
+        ):
+            assert prop.__class__.model_fields.keys() == {"prop"}
+
+        else:
+            assert_never(prop)
+
+    # conversation text and attachments are not deduplicated
+    mergeable_props = field_props[:-2]
+    assert dedup_field_select([*mergeable_props, *mergeable_props]) == mergeable_props
+    unmergeable_props = field_props[-2:]
+    assert dedup_field_select([*unmergeable_props, *unmergeable_props]) == [
+        *unmergeable_props,
+        *unmergeable_props,
+    ]
+
+
+def test_resource_prop_deduplicator_assumptions() -> None:
+    resource_props: list[augment.ResourceProp] = [
+        augment.ResourceTitle(),
+        augment.ResourceSummary(),
+        augment.ResourceOrigin(),
+        augment.ResourceExtra(),
+        augment.ResourceSecurity(),
+        augment.ResourceClassificationLabels(),
+    ]
+
+    for prop in resource_props:
+        if (
+            isinstance(prop, augment.ResourceTitle)
+            or isinstance(prop, augment.ResourceSummary)
+            or isinstance(prop, augment.ResourceOrigin)
+            or isinstance(prop, augment.ResourceExtra)
+            or isinstance(prop, augment.ResourceSecurity)
+            or isinstance(prop, augment.ResourceClassificationLabels)
+        ):
+            assert prop.__class__.model_fields.keys() == {"prop"}
+
+        else:
+            assert_never(prop)
+
+    assert dedup_resource_select([*resource_props, *resource_props]) == resource_props
