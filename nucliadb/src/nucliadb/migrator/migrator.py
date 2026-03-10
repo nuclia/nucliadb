7cb32e52d137aeb418e28ea7544137e771761fae
Ferran Llamas <ferran@nuclia.com>
2026-03-10 09:09:03 +0100

    migration logs

--- nucliadb/src/nucliadb/migrator/migrator.py
+++ nucliadb/src/nucliadb/migrator/migrator.py
@@ -208,6 +208,7 @@ async def run_rollovers(context: ExecutionContext) -> None:
 async def run_pg_schema_migrations(driver: PGDriver):
     migrations = get_pg_migrations()
 
+    logger.info(f"Running PG schema migrations.")
     # The migration uses two transactions. The former is only used to get a lock (pg_advisory_lock)
     # without having to worry about correctly unlocking it (postgres unlocks it when the transaction ends)
     async with driver.rw_transaction() as tx_lock, tx_lock.connection.cursor() as cur_lock:  # type: ignore[attr-defined]
@@ -224,6 +225,7 @@ async def run_pg_schema_migrations(driver: PGDriver):
             if version in migrated:
                 continue
 
+            logger.info(f"Running PG schema migration {version}: {migration.__name__}")
             # Gets a new transaction for each migration, so if they get interrupted we at least
             # save the state of the last finished transaction
             async with driver.rw_transaction() as tx, tx.connection.cursor() as cur:  # type: ignore[attr-defined]
