37769e81743fcf0707af434459846b2fbab01169
Joan Antoni RE <joanantoni.re16@gmail.com>
2026-03-25 11:32:02 +0100

    Test POST /suggest

--- nucliadb_models/src/nucliadb_models/suggest.py
+++ nucliadb_models/src/nucliadb_models/suggest.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 from pydantic import BaseModel
 from pydantic.json_schema import SkipJsonSchema
 
