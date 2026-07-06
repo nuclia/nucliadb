96fc796f7aa2f37388c84e777a7d73daf5709b14
Ferran Llamas <ferran@nuclia.com>
2026-07-06 09:52:21 +0200

    Fix

--- nucliadb/src/nucliadb/ingest/orm/resource.py
+++ nucliadb/src/nucliadb/ingest/orm/resource.py
@@ -288,7 +288,7 @@ class Resource:
 
     async def field_exists(self, type: FieldType.ValueType, field: str) -> bool:
         """Return whether this resource has this field or not."""
-        all_fields_ids = await self.get_fields_ids(force=False)
+        all_fields_ids = await self.get_fields_ids(force=True)
         for field_type, field_id in all_fields_ids:
             if field_type == type and field_id == field:
                 return True
