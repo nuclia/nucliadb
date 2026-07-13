ddeaba3afa8c9cf82be534ea096b1feeafa17844
Ferran Llamas <ferran@nuclia.com>
2026-07-13 07:18:37 +0200

    use exc_info instead

--- nucliadb/src/nucliadb/common/back_pressure/materializer.py
+++ nucliadb/src/nucliadb/common/back_pressure/materializer.py
@@ -109,10 +109,11 @@ class BackPressureMaterializer:
             try:
                 with back_pressure_observer({"type": "get_processing_pending"}):
                     pending = await self._get_processing_pending(kbid)
-            except (TimeoutError, OSError) as exc:  # pragma: no cover
+            except (TimeoutError, OSError):  # pragma: no cover
                 logger.warning(
                     "Transient error getting pending messages to process. Back pressure on processing for KB can't be applied.",
-                    extra={"kbid": kbid, "error": str(exc)},
+                    extra={"kbid": kbid},
+                    exc_info=True,
                 )
                 return 0
             except Exception:  # pragma: no cover
@@ -162,10 +163,10 @@ class BackPressureMaterializer:
                     with back_pressure_observer({"type": "get_ingest_pending"}):
                         status = await self.processing_http_client.pull_status()
                         self.ingest_pending = status.pending
-                except (TimeoutError, OSError) as exc:  # pragma: no cover
+                except (TimeoutError, OSError):  # pragma: no cover
                     logger.warning(
                         "Transient error getting pending messages to ingest",
-                        extra={"error": str(exc)},
+                        exc_info=True,
                     )
                 except Exception:  # pragma: no cover
                     logger.exception(
